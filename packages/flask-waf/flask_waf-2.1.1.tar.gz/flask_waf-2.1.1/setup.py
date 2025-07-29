b05fe9ee418ad232a6638f2e04cadfa30f425da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:43 +0000

    Add package flask-waf/2.1.1/flask_waf-2.1.1.tar.gz

---
+++ packages/flask-waf/flask_waf-2.1.1.tar.gz/flask_waf-2.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask_waf',
+    version='2.1.1',
+    packages=find_packages(),
+    author='Ishan Oshada',
+    author_email='ic31908@gmail.com',
+    description='Flask-WAF is an advanced Web Application Firewall (WAF) extension for Flask applications. It provides comprehensive protection against various web application threats, enhancing the security of your Flask-based web applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ishanoshada/Flask-Waf',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
