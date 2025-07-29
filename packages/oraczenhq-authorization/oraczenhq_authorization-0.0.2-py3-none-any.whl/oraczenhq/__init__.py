b7d5fbffdb57299ac37fbef439ae133a25b1564d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:12 +0000

    Add package oraczenhq-authorization/0.0.2/oraczenhq_authorization-0.0.2-py3-none-any.whl

---
+++ packages/oraczenhq-authorization/oraczenhq_authorization-0.0.2-py3-none-any.whl/oraczenhq/__init__.py
@@ -0,0 +1,11 @@
+
+from .authorization.authorization import OpenFgaConfig
+from .authorization.authorization import OpenFgaService
+from .authorization.authorization import OpenFgaServiceSingleton
+
+
+__all__ = [
+    "OpenFgaConfig",
+    "OpenFgaService",
+    "OpenFgaServiceSingleton"
+]
