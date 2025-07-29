8ec2881b5df607b823c82de5a1b44191aec6fc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:21 +0000

    Add package OpenGeode-Stochastic/1.1.0/opengeode_stochastic-1.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/OpenGeode-Stochastic/opengeode_stochastic-1.1.0-cp310-cp310-win_amd64.whl/opengeode_stochastic/geometry.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+
+from .bin.opengeode_stochastic_py_geometry import *
+
+StochasticGeometryLibrary.initialize()
