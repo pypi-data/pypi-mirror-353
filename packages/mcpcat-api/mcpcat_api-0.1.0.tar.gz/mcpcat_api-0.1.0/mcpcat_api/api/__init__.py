10eb6d9a64821175c4b7f7ee2e9b44ade06909cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:39 +0000

    Add package mcpcat-api/0.1.0/mcpcat_api-0.1.0.tar.gz

---
+++ packages/mcpcat-api/mcpcat_api-0.1.0.tar.gz/mcpcat_api-0.1.0/mcpcat_api/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from mcpcat_api.api.api_documentation_api import APIDocumentationApi
+from mcpcat_api.api.events_api import EventsApi
+
---
+++ packages/mcpcat-api/mcpcat_api-0.1.0.tar.gz/mcpcat_api-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mcpcat_api",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "urllib3 >= 1.25.3",
+        "python-dateutil",
+        "pydantic>=1.10.5,<2",
+    ],
+)
