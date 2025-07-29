0a6a8a87673e2b059ef748ff75661e3348aff318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:35 +0000

    Add package format-tree/0.1.1/format_tree-0.1.1.tar.gz

---
+++ packages/format-tree/format_tree-0.1.1.tar.gz/format_tree-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="format_tree",
+    version="0.1.1",
+    description="A utility to plot decision trees with formatted node information.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Kathy G",
+    author_email="kguo715@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scikit-learn"
+    ],
+    python_requires=">=3.6",
+    url="https://pypi.org/project/format_tree/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
