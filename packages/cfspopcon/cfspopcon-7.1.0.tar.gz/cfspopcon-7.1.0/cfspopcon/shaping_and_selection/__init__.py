beca9406dbaf11b3c9d346b23aee902709af947a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:52 +0000

    Add package cfspopcon/7.1.0/cfspopcon-7.1.0.tar.gz

---
+++ packages/cfspopcon/cfspopcon-7.1.0.tar.gz/cfspopcon-7.1.0/cfspopcon/shaping_and_selection/__init__.py
@@ -0,0 +1,27 @@
+"""Functions to post-process a POPCON analysis, such as finding the coordinates matching a particular condition."""
+
+from .line_selection import (
+    find_coords_of_contour,
+    interpolate_onto_line,
+)
+from .point_selection import (
+    build_mask_from_dict,
+    find_coords_of_maximum,
+    find_coords_of_minimum,
+)
+from .transform_coords import (
+    build_transform_function_from_dict,
+    interpolate_array_onto_new_coords,
+    order_dimensions,
+)
+
+__all__ = [
+    "build_mask_from_dict",
+    "build_transform_function_from_dict",
+    "find_coords_of_contour",
+    "find_coords_of_maximum",
+    "find_coords_of_minimum",
+    "interpolate_array_onto_new_coords",
+    "interpolate_onto_line",
+    "order_dimensions",
+]
---
+++ packages/cfspopcon/cfspopcon-7.1.0.tar.gz/cfspopcon-7.1.0/cfspopcon/shaping_and_selection/point_selection.py
@@ -0,0 +1,56 @@
+"""Routines to find the coordinates of the minimum or maximum value of a field."""
+
+from collections.abc import Sequence
+from typing import Optional
+
+import numpy as np
+import xarray as xr
+from xarray.core.coordinates import DataArrayCoordinates
+
+from ..unit_handling import Quantity
+
+
+def find_coords_of_minimum(array: xr.DataArray, keep_dims: Sequence[str] = [], mask: Optional[xr.DataArray] = None) -> DataArrayCoordinates:
+    """Find the coordinates the minimum value of array.
+
+    These coordinates can be used to find the value of other arrays at the same point.
+
+    For example
+        >>> import xarray as xr
+        >>> import numpy as np
+        >>> from cfspopcon.point_selection import find_coords_of_minimum
+        >>> x = xr.DataArray(np.linspace(0, 1, num=10), dims="x")
+        >>> y = xr.DataArray(np.linspace(-1, 1, num=20), dims="y")
+        >>> z = xr.DataArray(np.abs(x + y), coords=dict(x=x, y=y))
+        >>> line = z.isel(find_coords_of_minimum(z, keep_dims="y"))
+    """
+    large = Quantity(np.inf, array.pint.units)
+
+    if mask is None:
+        masked_array = array.fillna(large)
+    else:
+        masked_array = array.where(mask).fillna(large)
+
+    along_dims = [dim for dim in masked_array.dims if dim not in keep_dims]
+    point_coords = masked_array.argmin(dim=along_dims)
+
+    return point_coords  # type:ignore[return-value]
+
+
+def find_coords_of_maximum(array: xr.DataArray, keep_dims: Sequence[str] = [], mask: Optional[xr.DataArray] = None) -> DataArrayCoordinates:
+    """Find the coordinates of the maximum value of array."""
+    return find_coords_of_minimum(-array, keep_dims=keep_dims, mask=mask)
+
+
+def build_mask_from_dict(dataset: xr.Dataset, plot_params: dict) -> xr.DataArray:
+    """Build a mask field which hides inaccessible parts of the operational space."""
+    mask = xr.DataArray(True)
+
+    for mask_key, mask_range in plot_params.get("where", dict()).items():
+        mask_field = dataset[mask_key]
+        mask_min = Quantity(mask_range.get("min", -np.inf), mask_range.get("units", ""))
+        mask_max = Quantity(mask_range.get("max", +np.inf), mask_range.get("units", ""))
+
+        mask = mask & ((mask_field > mask_min) & (mask_field < mask_max))
+
+    return mask
