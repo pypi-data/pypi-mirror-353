5911926a53577d440b31f165e94c7d9530c62d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:23 +0000

    Add package deadline/0.50.0/deadline-0.50.0-py3-none-any.whl

---
+++ packages/deadline/deadline-0.50.0-py3-none-any.whl/deadline/client/exceptions.py
@@ -0,0 +1,25 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+"""
+Exceptions for the AWS Deadline Cloud Client Library.
+"""
+
+
+class DeadlineOperationError(Exception):
+    """Error whose message gets printed verbatim by the cli handler"""
+
+
+class CreateJobWaiterCanceled(Exception):
+    """Error for when the waiter on CreateJob is interrupted"""
+
+
+class UserInitiatedCancel(Exception):
+    """Error for when the user requests cancelation"""
+
+
+class NonValidInputError(Exception):
+    """Error for when the user input is nonvalid"""
+
+
+class ManifestOutdatedError(Exception):
+    """Error for when local files are different from version captured in manifest"""
