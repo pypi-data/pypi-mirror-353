a94c30d5f3797a002a75190458a8b4d394a46979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:31 +0000

    Add package shutill/0.2/shutill-0.2.tar.gz

---
+++ packages/shutill/shutill-0.2.tar.gz/shutill-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shutill",
+    version="0.2",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "shutill=shutill.__main__:open_calc",
+        ],
+    },
+    description="Open Windows Calculator from Python",
+    author="Bat-sec",
+    author_email="bat-sec1337@outlook.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.6",
+)
