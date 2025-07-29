fda8a8e7f55fe87352868caadbe45f5cfb8afbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:30 +0000

    Add package promptbuilder/0.4.6/promptbuilder-0.4.6.tar.gz

---
+++ packages/promptbuilder/promptbuilder-0.4.6.tar.gz/promptbuilder-0.4.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="promptbuilder",
+    version="0.4.6",
+    packages=find_packages(),
+    install_requires=[
+        "pydantic",
+        "aisuite-async",
+    ],
+    author="Kapulkin Stanislav",
+    author_email="kapulkin@gmail.com",
+    description="Library for building prompts for LLMs",
+    long_description=open("Readme.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kapulkin/promptbuilder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
