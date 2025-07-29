68379031b709e3d6468c0639b01cbb0041458f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:34 +0000

    Add package tacklebox-cli/0.2.5/tacklebox_cli-0.2.5-py3-none-any.whl

---
+++ packages/tacklebox-cli/tacklebox_cli-0.2.5-py3-none-any.whl/tacklebox/sync.py
@@ -0,0 +1,23 @@
+from asyncio import run
+from functools import wraps
+from typing import TypeVar, Any, Callable, ParamSpec
+from collections.abc import Coroutine
+
+Params = ParamSpec("Params")
+Response = TypeVar("Response")
+
+
+def sync(
+    func: Callable[Params, Coroutine[Any, Any, Response]],
+) -> Callable[Params, Response]:  # pyright: ignore[reportExplicitAny]
+    """
+    Decorator that takes an async function `func(...) -> Coroutine[Any, Any, Response]`
+    and turns it into a synchronous function `() -> Response` by running
+    `asyncio.run(...)` under the hood.
+    """
+
+    @wraps(wrapped=func)
+    def wrapper(*args: Params.args, **kwargs: Params.kwargs) -> Response:
+        return run(func(*args, **kwargs))
+
+    return wrapper
