60fecaeee246e2a1a2f96e4b2b2bd05ab81ecd21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:38 +0000

    Add package ommlds/0.0.0.dev331/ommlds-0.0.0.dev331-py3-none-any.whl

---
+++ packages/ommlds/ommlds-0.0.0.dev331-py3-none-any.whl/ommlds/minichain/chat/streaming.py
@@ -0,0 +1,24 @@
+import typing as ta
+
+from ..registry import register_type
+from ..resources import ResourceManaged
+from ..services import Response
+from ..services import Service
+from .choices import AiChoices
+from .services import ChatRequest
+from .services import ChatResponseOutputs
+
+
+##
+
+
+ChatStreamResponse: ta.TypeAlias = Response[ResourceManaged[ta.Iterator[AiChoices]], ChatResponseOutputs]
+
+
+##
+
+
+# @omlish-manifest ommlds.minichain.registry.RegistryTypeManifest
+ChatStreamService: ta.TypeAlias = Service[ChatRequest, ChatStreamResponse]
+
+register_type(ChatStreamService, module=__name__)
