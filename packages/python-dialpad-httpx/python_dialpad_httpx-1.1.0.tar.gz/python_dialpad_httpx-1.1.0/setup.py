f32049ad2419d0f09e74671dfeef15f71ae54d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:22 +0000

    Add package python-dialpad-httpx/1.1.0/python_dialpad_httpx-1.1.0.tar.gz

---
+++ packages/python-dialpad-httpx/python_dialpad_httpx-1.1.0.tar.gz/python_dialpad_httpx-1.1.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md') as f:
+    return f.read()
+
+
+setup(
+  name='python-dialpad-httpx',
+  version='1.1.0',
+  description='A python wrapper for the Dialpad REST API',
+  long_description=readme(),
+  long_description_content_type="text/markdown",
+  author='Jake Nielsen',
+  author_email='jnielsen@dialpad.com',
+  license='MIT',
+  url='https://github.com/dialpad/dialpad-python-sdk',
+  install_requires=[
+    'cached-property',
+    'certifi',
+    'chardet',
+    'idna',
+    'httpx',
+    'urllib3'
+  ],
+  include_package_data=True,
+  packages=find_packages()
+)
