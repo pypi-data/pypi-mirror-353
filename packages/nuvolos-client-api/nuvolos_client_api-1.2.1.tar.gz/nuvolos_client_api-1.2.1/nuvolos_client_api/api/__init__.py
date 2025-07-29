797e3da35d8c34e369ec8fa749e9796b376ee1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:09:05 +0000

    Add package nuvolos-client-api/1.2.1/nuvolos_client_api-1.2.1.tar.gz

---
+++ packages/nuvolos-client-api/nuvolos_client_api-1.2.1.tar.gz/nuvolos_client_api-1.2.1/nuvolos_client_api/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from nuvolos_client_api.api.apps_v1_api import AppsV1Api
+from nuvolos_client_api.api.instances_v1_api import InstancesV1Api
+from nuvolos_client_api.api.organizations_v1_api import OrganizationsV1Api
+from nuvolos_client_api.api.snapshots_v1_api import SnapshotsV1Api
+from nuvolos_client_api.api.spaces_v1_api import SpacesV1Api
+from nuvolos_client_api.api.tasks_v1_api import TasksV1Api
+from nuvolos_client_api.api.workloads_v1_api import WorkloadsV1Api
+
