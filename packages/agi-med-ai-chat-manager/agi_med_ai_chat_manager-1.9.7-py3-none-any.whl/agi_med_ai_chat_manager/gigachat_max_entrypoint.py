c45735f17bf910cd0cec06fc9f717719b5df0ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:18 +0000

    Add package agi-med-ai-chat-manager/1.9.7/agi_med_ai_chat_manager-1.9.7-py3-none-any.whl

---
+++ packages/agi-med-ai-chat-manager/agi_med_ai_chat_manager-1.9.7-py3-none-any.whl/agi_med_ai_chat_manager/gigachat_max_entrypoint.py
@@ -0,0 +1,15 @@
+from .gigachat_entrypoint import GigaChatEntryPoint
+
+
+class GigaMaxEntryPoint(GigaChatEntryPoint):
+    def __init__(
+        self, client_id: str, client_secret: str, model_id: str = "GigaChat-Max", warmup: bool = False
+    ) -> None:
+        super().__init__(client_id=client_id, client_secret=client_secret, model_id=model_id, warmup=warmup)
+
+
+class GigaMax2EntryPoint(GigaChatEntryPoint):
+    def __init__(
+        self, client_id: str, client_secret: str, model_id: str = "GigaChat-2-Max", warmup: bool = False
+    ) -> None:
+        super().__init__(client_id=client_id, client_secret=client_secret, model_id=model_id, warmup=warmup)
