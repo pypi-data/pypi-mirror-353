2d07d8f1c642bd3ddcbf88c55b2b87334523e9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:26 +0000

    Add package lipyphilic/0.12.1/lipyphilic-0.12.1-cp312-cp312-win_amd64.whl

---
+++ packages/lipyphilic/lipyphilic-0.12.1-cp312-cp312-win_amd64.whl/lipyphilic/__init__.py
@@ -0,0 +1,22 @@
+from ._lipyferric import __version__
+
+__version__ = version = __version__.replace("-", ".")  # Rust uses X.Y-dev0 but Python X.Y.dev0
+__version_tuple__ = version_tuple = tuple(version.split("."))
+
+from lipyphilic import (
+    analysis,
+    plotting,
+    transformations,
+)
+from lipyphilic.leaflets.assign_leaflets import (
+    AssignCurvedLeaflets,
+    AssignLeaflets,
+)
+
+__all__ = [
+    "AssignCurvedLeaflets",
+    "AssignLeaflets",
+    "analysis",
+    "plotting",
+    "transformations",
+]
