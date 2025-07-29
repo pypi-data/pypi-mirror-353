fd348ba2abf84244791fc35e671233c30e33df04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:04 +0000

    Add package splunksecrets/1.1.0/splunksecrets-1.1.0.tar.gz

---
+++ packages/splunksecrets/splunksecrets-1.1.0.tar.gz/splunksecrets-1.1.0/README.rst
@@ -0,0 +1,279 @@
+splunksecrets - Encrypt and Decrypt Splunk Passwords
+====================================================
+
+|Build Status|
+
+``splunksecrets`` is a tool for working with Splunk secrets offline. It
+currently supports encryption and decryption of passwords, but in the
+future will support offline recursive conversion of a Splunk
+installation from one splunk.secret file to another (e.g. for
+synchronizing splunk.secret across your entire distributed
+infrastructure).
+
+Installation
+------------
+
+You can install ``splunksecrets`` using ``uv``:
+
+::
+
+   uv tool install splunksecrets
+
+
+Or with pip:
+
+::
+
+   python -m pip install splunksecrets
+
+
+Usage
+-----
+
+As of version 1.0.0, splunksecrets is broken into subcommands for each
+operaton
+
+::
+
+  Usage: splunksecrets [OPTIONS] COMMAND [ARGS]...
+
+  Options:
+    --help  Show this message and exit.
+
+  Commands:
+    dbconnect-decrypt           Decrypt password used for dbconnect identity
+    dbconnect-encrypt           Encrypt password used for dbconnect identity
+    dbconnect-legacy-encrypt    Encrypt password used for dbconnect identity using legacy algorithm
+    phantom-decrypt             Decrypt password used for Phantom asset
+    phantom-encrypt             Encrypt password used for Phantom asset
+    splunk-decrypt              Decrypt password using Splunk 7.2 algorithm
+    splunk-encrypt              Encrypt password using Splunk 7.2 algorithm
+    splunk-hash-passwd          Generate password hash for use in...
+    splunk-legacy-decrypt       Decrypt password using legacy Splunk algorithm...
+    splunk-legacy-encrypt       Encrypt password using legacy Splunk algorithm...
+
+You can find details of the usage of each subcommand by running the command
+followed by ``--help``.
+
+- **dbconnect-encrypt** and **dbconnect-decrypt** are for interacting with
+  credentials stored in ``identities.conf`` in splunk_app_db_connect
+- **phantom-encrypt** and **phantom-decrypt** are for interacting with
+  credentials stored in the Phantom database
+- **splunk-encrypt** and **splunk-decrypt** are for interacting with
+  credentials from Splunk 7.2 or newer
+- **splunk-legacy-encrypt** and **splunk-legacy-decrypt** are for interacting
+  with credentials from Splunk versions below 7.2
+- **splunk-hash-passwd** is for generating Splunk local user password hashes
+  for use in ``$SPLUNK_HOME/etc/passwd``
+
+Encryption Schemes
+------------------
+
+Splunk passwd hashes
+~~~~~~~~~~~~~~~~~~~~
+
+Splunk password hashes are not, strictly speaking, encrypted. They're
+hashed using the standard Unix ``crypt`` function. The ``$6$`` indicates
+that SHA-512 hashing algorithm is used. Details on SHA-crypt can be
+found `here <https://akkadia.org/drepper/SHA-crypt.txt>`__.
+
+Splunk pre-7.2
+~~~~~~~~~~~~~~
+
+Splunk prior to 7.2 used RC4 encryption for secrets, indicated in
+configuration files by ``$1$`` in the encrypted password. The plaintext
+password is XOR'ed with a static salt (``DEFAULTSA``) and then RC4
+encrypted using the first 16-bytes of ``splunk.secret`` as the key. The
+resulting ciphertext is base64-encoded and prepended with ``$1$`` to
+produce the encrypted password seen in the configuration files.
+
+Splunk 7.2
+~~~~~~~~~~
+
+Starting in Splunk 7.2, AES256-GCM is used for encryption of secrets,
+indicated in configuration files by ``$7$`` in the encrypted password.
+The ``PBKDF2`` algorithm is used to derive an encryption key from all
+254 bytes of ``splunk.secret`` (the newline character is stripped from
+the end of the file), using a static salt of ``disk-encryption`` and a
+single iteration. This 256-bit key is then used as the encryption key
+for AES256-GCM, with a 16-byte randomly generated initialization vector.
+The encryption produces both the ciphertext as well as a "tag" that is
+used as part of integrity verification. The iv, ciphertext, and tag (in
+that order) are concatenated, base64-encoded, and prepended with ``$7$``
+to produce the encrypted password seen in the configuration files. If
+the key is less than 254-bytes it is padded with null bytes.
+
+Phantom
+~~~~~~~
+
+Phantom stores encrypted credentials in it's internal database as part
+of the JSON blob in the ``configuration`` column of the asset table.
+These credentials are encrypted using AES256-CBC and are stored
+base64-encoded. A SHA256 hash is generated using the public modulus of
+the Phantom RSA private key (located at
+``/opt/phantom/keystore/private_key.pem``) concatenated with the
+Django ``SECRET_KEY`` (found in
+``/opt/phantom/www/phantom_ui/secret_key.py``) - this hash is used as
+the key for the AES encryption. A SHA1 hash is generated using the
+``id`` of the asset from the ``asset`` table - this hash is used as
+the initialization vector (``iv``) for the AES encryption. The password
+to be encrypted is padded using PKCS7 padding prior to encryption.
+
+DBConnect pre ~3.12
+~~~~~~~~~
+
+DBConnect used to encrypt passwords using the OpenSSL ``aes-256-cbc`` command.
+This changed somewhere around 3.12 though I haven't bothered to find the exact
+version where the change was made. 
+This encryption is fairly straightforward, and is documented in
+``dbxcrypto.py`` included with DBConnect. The key and IV are derived from
+the secret (stored in ``$SPLUNK_HOMEetc/apps/splunk_app_db_connect/certs/identity.dat``)
+and the salt (randomly generated by OpenSSL). The resulting ciphertext is
+concatenated together with the salt (``Salted__`` + salt + ciphertext) and
+encoded using Base64. The password to be encrypted is padded using PKCS7
+padding prior to encryption.
+
+DBConnect post ~3.12
+~~~~~~~~~~~~~~~~~~~~
+
+DBConnect now encrypts passwords using AES GCM with no padding.
+A random 16-byte salt is generated as well as a random 12-byte IV. The key is derived
+from the salt and the secret (stored in ``$SPLUNK_HOMEetc/apps/splunk_app_db_connect/certs/identity.dat``)
+using Scrypt. The final output is ``salt`` + ``iv`` + ``ciphertext``.
+
+Known Issues
+------------
+
+- If the splunk secret and an encrypted password don't match, one of
+  two things will happen - either you'll get gibberish output, or a
+  Python traceback. At some point, we'll hopefully have better error
+  checking for this.
+- DBConnect will occassionally store the ending newline from OpenSSL output in
+  identities.conf, represented as a single ``\`` at the end of a line. This
+  must be removed prior to passing encrypted passwords to ``splunksecrets``
+
+Version History
+---------------
+Version 1.1.0 (2025-06-05)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Added support for encryption/decryption of DBConnect credentials using newer algorithm
+- Bumps minimum Python version to 3.9
+- Refactored code into multiple files
+- Switched from setup.py to pyproject.toml
+- Removed ``six`` dependency used for Python 2 compatibility
+
+Version 1.0.1
+~~~~~~~~~~~~~~
+- Short secrets are padded in the same way Splunk itself does it. Thanks to
+Matt Anderson for this contribution.
+
+Version 1.0.0 (2021-06-07)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Updated CLI command docstrings for proper help output
+- Add initial support for encryption/decrypt of Splunk DBConnect credentials
+
+Version 1.0.0rc1 (2021-05-19)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- New CLI interface that breaks each operation into its own subcommand
+- CLI interface supports passing path to key files (splunk.secret,
+  private_key.pem, and secret_key.py) via arguments, or file contents
+  via environment variables
+- Add initial support for encryption/decryption of Phantom asset
+  credentials (thanks swoops)
+
+Version 0.5.1 (2022-06-29)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Allow passing the splunk secret itself as an environment variable
+    ~ provided by chutzimir
+
+
+Version 0.5.0 (2020-06-23)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Add support for passing a path to splunk.secret, the splunk.secret
+  contents, and the password/password hash via environment variables
+  (thanks hanswurscht)
+- Added error output when trying to decrypt user passwords
+
+Version 0.4.3 (2020-02-17)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Correct an issue when splunk secret is too long. Splunk handles this
+  by truncating splunk.secret to 254 bytes, so we now do the same
+  (thanks nbertram)
+- Added error checking in case, somehow, splunk secret is too short.
+
+Version 0.4.2 (2019-09-29)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Add handling for incorrectly padded (but otherwise correct) password
+  strings as a convenience (thanks rafrey-his)
+
+Version 0.4.1 (2019-09-11)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Fix an issue in Python 3 when using ``--splunk-secret-text`` (thanks
+  Joeboyc2)
+
+Version 0.4.0 (2019-03-25)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Add ``--hash-passwd`` flag for generating Splunk password hashes
+
+Version 0.3.1 (2019-02-06)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Fix bug when a character in the password was the same as the salt
+  character
+- Add ``--splunk-secret-text`` and ``--password`` arguments for
+  non-interactive use (thanks nadidsky)
+
+Version 0.3.0 (2019-01-26)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Add ``--nosalt`` flag for pre-7.2 passwords that are not salted, such
+  as ``sslPassword`` (thanks duckfez)
+
+Version 0.2.1 (2018-10-27)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Cosmetic release to update metadata on PyPI
+
+Version 0.2.0 (2018-10-24)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Add support for Splunk 7.2
+
+Version 0.1.0 (2018-10-08)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Initial release
+- Support Splunk < 7.2
+
+License Info
+------------
+
+The MIT License (MIT)
+
+Copyright (c) 2025 Hurricane Labs LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.. |Build Status| image:: https://github.com/HurricaneLabs/splunksecrets/actions/workflows/tests.yml/badge.svg
+   :target: https://github.com/HurricaneLabs/splunksecrets/actions/workflows/tests.yml
---
+++ packages/splunksecrets/splunksecrets-1.1.0.tar.gz/splunksecrets-1.1.0/tests/test_splunksecrets.py
@@ -0,0 +1,314 @@
+import base64
+import os
+import random
+import unittest
+from unittest.mock import patch
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.rsa import generate_private_key
+
+import splunksecrets
+
+
+splunk_secret = b"JX7cQAnH6Nznmild8MvfN8/BLQnGr8C3UYg3mqvc3ArFkaxj4gUt1RUCaRBD/r0CNn8xOA2oKX8/0uyyChyGRiFKhp6h2FA+ydNIRnN46N8rZov8QGkchmebZa5GAM5U50GbCCgzJFObPyWi5yT8CrSCYmv9cpRtpKyiX+wkhJwltoJzAxWbBERiLp+oXZnN3lsRn6YkljmYBqN9tZLTVVpsLvqvkezPgpv727Fd//5dRoWsWBv2zRp0mwDv3t\n"
+
+
+phantom_private_key = b"""
+-----BEGIN RSA PRIVATE KEY-----
+MIIJKAIBAAKCAgEA0bmUNdFG/zToW1Ii/WROkJ3of7g1aHBqM++eQqqKVO/NY/Uz
+kp32q7lLwYPsIVaxrq2EGHHIj9Ls1Isp6EhCQLCVzFVOCkUMjqoNumtVZEwnELMV
+/Ya5oDc3aVI+Mw5JGSbuR2TisXF5yrOIrBk1SYn55crkTs6N48nTBcZfWcTchhNe
+75Gl3zM+ETLVv1eVpOC0wbd/6WDAKXxG4IfT+W2POwOOt2Ozwr9dyLbKou5yGklM
+leW7uBRCmg/cNryJ54XnOmpiAt6gJfLii6dPiMdJJq/upLCJan9LpGgIwLva0I4J
+RMGBgcLvNn+tiU4+NLrtep9mZ30pRJulLf1LdKYKfAReCgFUbUUpUVo/RTW49CLm
+My56bwudswcBLlXX0/nyIcG0D6/Sop+nSE/tIGoFZm8nK6xG9dEpInig/A3MenHq
+ZM2oslEzISdSJFGWWbHWzOc/eZQhRfLDDYlFChGyxEWis4PStr7iUyoshsbDsgpo
+/57IM96kSRoUxNu96yRDOWJDxym0C4TzZ3blcBpcRI8PmimTRaax7DXlC3AeraD7
+Qgo64meixWPe4d4WilUntLQx0MXit+JXVQOkXuqT+cFujnQ6BRGjqqzDimpAbUVC
+5+CnidkkCkLgKVyJwnEjnrMh2gyOXQOTNXO2sr86x2n4qgswHzOOdEqP8mMCAwEA
+AQKCAgAn5UincDc0RylIbfiQAWvcoN1gpncqdfAODzAo+G42OCP3NubnpjsNccNS
+fbkGoopMc1+kksiXa4V37T08nbpUugLVCwk8dOQto3XXF6H34XCxAZBWWTGoo7hb
+xbRlW/tV2I0NcciZ/r8vazW9n1H+ukuahh0mTK67PWJyP8lVt1hH/RlbM3a9Xe8Z
+sHTu4EdsRP69tx2TFL+ZZLXK8qvFUC0mCcg1ZLcGfgbmLrefhQKZs4XpIfmyy8/b
+pNa8ZuNI8rUI7MHWT+lhLrIf5OUBXP+yZ1dbZuXNJ4gJL20y1MmMUOi8dp8fQyjk
+Uid97i9xMnoIkJgXabzW+1DRzdRzdP80BvmT5Rl1mrybOdiaa/OvCQRRh/AKvzEs
+rarGxkG8MqPBtMJOtI+1sSJVQ8rNyJk9mJt/oTaANMkeKUVPqNI+xIxKwUEDu6s1
+092NQ3XmmdK6/HjDcQm1o41QY0PMr7uPnUayilSlZSS2RY7K91FZrMR2lxSKVSEv
+QNc3tEw8oEqk1lcrGn0RLTR15ISsIJmOpU+WQ4VWxHVr5CRiE3utF81Wa93YVu0m
+eUZTEdyo9Coa6eiaAAQHX8XXKq3y2USOgGW4BGRA3n+vRpSJXG+AMTsccSVo/68q
+L/Pc9JoPd8+mPtEBIZcM9WBNEk+xQSZbxfQoI0+guhr3YOnukQKCAQEA8nUr8NLr
+4ItVGKPLbMzuSHFbumW9xtX5Wy/foQ2K3PTaV+9oO/LHDmP9y6bPAKCLlj1O2eMU
+kHhF9xk4WKPEs5MVJf/TKyVnp5Np5448v6rVkEmkmkFOKS5RAPY7rb6woPe8bqnN
+g6qAzxsiSECWt0JUkijtGtM8KqYZQJ1YQnxonu8HvT1p4vpa8u5c6hE0V785Cm+/
+EsN32h13moddwV7aBRNoNxIIDKF+YsFwpuhxU+ykP0oqbwd47INT5YRWU8Gcwgw6
+6YEAODJN6vqwW9cFm9C5XxkmlP5xqN3ld6Oo419HLe+FTqg+mVsXzuE6K/eTg4Xr
+3s4v1g8NQv9ceQKCAQEA3XBfCsiPCCMfW1sUZ7X5R4u/p71p1r3DrF1mrEBi6XcZ
+fodJVWBKhb5aOwV/Hk2Yo2xM2hg+rxi5hH1NJ0ojiMG10MA4m5j53fI7jKh33Rek
+lK4OoPugCR734CRBcvX0LNwlKnp+XScWQbUXfyaX3SYhu7kNc/ACp5RjBSyWG7YA
+ggRvuEksbrOzW4JBvxsQLlenpHKbbkuuTcb7phmFWy4f4j89aVyi0K0uek/jQjun
+bwajiv02akf8s1A80gsjsJq4ZXSC2bZYGaRVNyl3aRf5UQo+hHmf7B0D87jGcOvZ
+FDj5m5gkBb6PBAkf1BaDAoZZ5xDQNp0IHUsiCX0WuwKCAQEAuqZhUV/Onmok9HcD
+35pTxgkeaHxygGOxNvW/3qgiQr9sZ02ynJPlkbqAYwrjSMRPMTW5QhuCdUVh9zu/
+GN3aS7PrD/nFgu2kwmjFmrDxK0xEZHOM+ANWPHWffnIQt0yZhRGySi8GO1pDFyz4
+U08Ft8akfHBtN71uEBcWUZvPmj9T+yZgetqQbV/2LBY8CSV9eh+HtfUYz0UTOyJ8
+NMYI8xgmFso97EdBUxfvx+7K3nMK62S9fMuHpznFaa4gJAyguJHQL8Jih+f7V0fC
+nCapJcp+Utl4GzGtdf37WdiZEmU3WvK6RS6jHU6AAmcSCP1yXu6U7Sdn0XpBcTTd
+LLqRYQKCAQAmveRC9LfhipOP3i2Bv5qvY8nSRRdhVK4JWAYAmPs1MXsqYez4nPIs
+2BLdRKbq3FSaMvZ3Kcq9w6uhfHKlLxlsccenkBnXTzpYFGtKvqbJ7bsDsDgq/hsP
+vXVMp8szm8humM1/0xKMpawFLvO/cTccn/FC8Ktj31f3jcSNQTG6A2uvU6tgKJ03
+eQUmO3HZR5jbIsbXxJ1g/KiwPuIEO27Tdwxo/vt1lZl+vO3mxFdUXS98A0NVq9t7
+WMDjdmRNs1k5KricN3g1MuvTAKdQEVcs8d7SyOILN2Q9YQ4x89/0Ha9LFHTHPiT/
+REmtQRNqfc3KAUt1W8Gx7GNhDKYV1ubFAoIBAHo2LNyQJdd05YWYD9I+hNwEnmMN
+1QQRwRXEiqdUC0Qsi+xE7CQgDjfL558JDOFqBxbpB8/nK1COu55+Qsz1Qad1qRhH
+cmgdYmm9WrfUdq6iHxGkcCqBHn2exywm/m6e7lQcOB5NfzJTRQ/b5tMtxIZtiHSJ
+JYffRggbRPmc7ZzCW1YmprbLDi7BjWqfMf6XM9kJzls1BfyD56V9dBNoZ1bjx686
+gKgdY8DnLzg387kXGgawyCzPTgZK9xMKOS7Fgq2EzJevcFL2UJmBWNMTZcpVdz6w
+ennUbapylEo4uLqjPlS64jYwbMIDMsn8UAkQT8Sx2ShwYwSdHsb2aE1tdj4=
+-----END RSA PRIVATE KEY-----
+""".strip()
+phantom_secret = b"bhxiegi%@^76toslu+7olp2fx)taw%vcqoxlhlr27^ri3i41vt"
+
+dbconnect_secret = b"WeUSgc35gxALVesK01Z91MASQNl4E1NvYfmc5zC7KXI="
+
+
+class TestSplunkSecrets(unittest.TestCase):
+    def test_to_bytes(self):
+        self.assertEqual(
+            splunksecrets.to_bytes(1234, 16, "big"),
+            b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\xd2",
+        )
+
+    def test_encrypt(self):
+        ciphertext = splunksecrets.encrypt(splunk_secret, "temp1234")
+        self.assertEqual(ciphertext, "$1$n6g0W7F51ZAK")
+
+    def test_encrypt_nosalt(self):
+        ciphertext = splunksecrets.encrypt(splunk_secret, "temp1234", nosalt=True)
+        self.assertEqual(ciphertext, "$1$2+1yGuQ1gcMK")
+
+    def test_encrypt_raises_value_error_short_secret(self):
+        with self.assertRaisesRegex(
+            ValueError, "^secret too short, need 16 bytes, got 15$"
+        ):
+            splunk_secret = base64.b64encode(os.urandom(255))[:15]
+            splunksecrets.encrypt(splunk_secret, "temp1234")
+
+    def test_encrypt_new(self):
+        ciphertext = splunksecrets.encrypt_new(
+            splunk_secret, "temp1234", iv=b"i5dKMGaSIRNpJty4"
+        )
+        self.assertEqual(
+            ciphertext, "$7$aTVkS01HYVNJUk5wSnR5NKR+EdOfT4t84WSiXvPFHGHsfHtbgPIL3g=="
+        )
+
+    def test_encrypt_new_str_secret(self):
+        ciphertext = splunksecrets.encrypt_new(
+            "abc123",  # secret as a string (not bytes)
+            "strings are fine",
+            iv=b"zIDM0YmIgDQ2gMzk",
+        )
+        self.assertEqual(
+            ciphertext,
+            "$7$eklETTBZbUlnRFEyZ016a+2HMVEtbCAJkNb7RkVHdqZwZkVJyZ+HmTWlYJedFdR4",
+        )
+
+    def test_encrypt_new_pads_short_secret(self):
+        ciphertext = splunksecrets.encrypt_new(
+            splunk_secret[:30], "short123", iv=b"4KK0Ra8LWBKxUFQ8"
+        )
+        self.assertEqual(
+            ciphertext, "$7$NEtLMFJhOExXQkt4VUZROK9vm0tDLbJn2jxESMRbs7MTdiHuTtBz8g=="
+        )
+
+    def test_encrypt_character_matches_salt1(self):
+        ciphertext = splunksecrets.encrypt(splunk_secret, "A" * 8)
+        self.assertEqual(ciphertext, "$1$qowYK8EKp+UK")
+
+    def test_encrypt_character_matches_salt2(self):
+        ciphertext = splunksecrets.encrypt(splunk_secret, "DEFAULTSA" * 8)
+        self.assertEqual(
+            ciphertext,
+            "$1$681ZK4BL5qRLsmMRT6EotpYVgOge69IZZhhxq0P+2ZBCaRTkci1IwiwRG9Ty2bHaSoG1p9QSXWIYA7mrYsyFqfWYqlvg+oQ+sg==",
+        )  # noqa: E501
+
+    def test_encrypt_phantom(self):
+        ciphertext = splunksecrets.encrypt_phantom(
+            phantom_private_key, phantom_secret, "temp1234", 1234
+        )
+        self.assertEqual(ciphertext, "9Uzb3CO0PFFRUk0upbNvrA==")
+
+    def test_decrypt(self):
+        plaintext = splunksecrets.decrypt(splunk_secret, "$1$n6g0W7F51ZAK")
+        self.assertEqual(plaintext, "temp1234")
+
+    def test_decrypt_raises_value_error_short_secret1(self):
+        with self.assertRaisesRegex(
+            ValueError, "^secret too short, need 16 bytes, got 15$"
+        ):
+            splunk_secret = base64.b64encode(os.urandom(255))[:15]
+            splunksecrets.decrypt(splunk_secret, "$1$n6g0W7F51ZAK")
+
+    def test_decrypt_pads_short_secret2(self):
+        plaintext = splunksecrets.decrypt(
+            splunk_secret[:30],
+            "$7$NEtLMFJhOExXQkt4VUZROK9vm0tDLbJn2jxESMRbs7MTdiHuTtBz8g==",
+        )
+        self.assertEqual(plaintext, "short123")
+
+    def test_decrypt_nosalt(self):
+        plaintext = splunksecrets.decrypt(splunk_secret, "$1$2+1yGuQ1gcMK", nosalt=True)
+        self.assertEqual(plaintext, "temp1234")
+
+    def test_decrypt_new(self):
+        plaintext = splunksecrets.decrypt(
+            splunk_secret, "$7$aTVkS01HYVNJUk5wSnR5NKR+EdOfT4t84WSiXvPFHGHsfHtbgPIL3g=="
+        )
+        self.assertEqual(plaintext, "temp1234")
+
+    def test_decrypt_character_matches_salt1(self):
+        plaintext = splunksecrets.decrypt(splunk_secret, "$1$qowYK8EKp+UK")
+        self.assertEqual(plaintext, "A" * 8)
+
+    def test_decrypt_character_matches_salt2(self):
+        plaintext = splunksecrets.decrypt(
+            splunk_secret,
+            "$1$681ZK4BL5qRLsmMRT6EotpYVgOge69IZZhhxq0P+2ZBCaRTkci1IwiwRG9Ty2bHaSoG1p9QSXWIYA7mrYsyFqfWYqlvg+oQ+sg==",  # noqa: E501
+        )
+        self.assertEqual(plaintext, "DEFAULTSA" * 8)
+
+    def test_decrypt_unpadded_base64(self):
+        plaintext = splunksecrets.decrypt(splunk_secret, "$1$iqw4ag")
+        self.assertEqual(plaintext, "aaa")
+
+    def test_decrypt_phantom(self):
+        plaintext = splunksecrets.decrypt_phantom(
+            phantom_private_key, phantom_secret, "9Uzb3CO0PFFRUk0upbNvrA==", 1234
+        )
+        self.assertEqual(plaintext, "temp1234")
+
+    def test_end_to_end(self):
+        splunk_secret = base64.b64encode(os.urandom(255))[:254]
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt(splunk_secret, plaintext1)
+        plaintext2 = splunksecrets.decrypt(splunk_secret, ciphertext)
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_end_to_end_nosalt(self):
+        splunk_secret = base64.b64encode(os.urandom(255))[:254]
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt(splunk_secret, plaintext1, nosalt=True)
+        plaintext2 = splunksecrets.decrypt(splunk_secret, ciphertext, nosalt=True)
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_end_to_end_new(self):
+        splunk_secret = base64.b64encode(os.urandom(255))[:254]
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt_new(splunk_secret, plaintext1)
+        plaintext2 = splunksecrets.decrypt(splunk_secret, ciphertext)
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_end_to_end_character_matches_salt(self):
+        splunk_secret = base64.b64encode(os.urandom(255))[:254]
+        plaintext1 = "".join([random.choice("DEFAULTSA") for _ in range(24)])
+        ciphertext = splunksecrets.encrypt(splunk_secret, plaintext1)
+        plaintext2 = splunksecrets.decrypt(splunk_secret, ciphertext)
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_encrypt_new_and_decrypt_use_only_first_254(self):
+        splunk_secret1 = base64.b64encode(os.urandom(512))[:300]
+        splunk_secret2 = splunk_secret1[:254]
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt_new(splunk_secret1, plaintext1)
+        plaintext2 = splunksecrets.decrypt(splunk_secret2, ciphertext)
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_end_to_end_phantom(self):
+        __phantom_private_key = generate_private_key(
+            public_exponent=65537, key_size=2048
+        )
+        __phantom_private_key = __phantom_private_key.private_bytes(
+            serialization.Encoding.PEM,
+            serialization.PrivateFormat.TraditionalOpenSSL,
+            serialization.NoEncryption(),
+        )
+        chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+
+        __phantom_secret = "".join(random.choice(chars) for _ in range(50))
+
+        asset_id = random.randint(0, 999999)
+
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt_phantom(
+            __phantom_private_key, __phantom_secret, plaintext1, asset_id
+        )
+        plaintext2 = splunksecrets.decrypt_phantom(
+            __phantom_private_key, __phantom_secret, ciphertext, asset_id
+        )
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_get_key_and_iv_1(self):
+        key, iv = splunksecrets.get_key_and_iv(dbconnect_secret, b" " * 8)
+        self.assertEqual(
+            key,
+            b"\xd5\x7f\x00\xab\xb2\xaa\xbf\x9a\xab\x00\x9bH\x08\x14+\xd0\xf4d\xeb\xfa\xfc8\xa1J\x92v\xed\xed\x90X\xb4\x9c",
+        )
+        self.assertEqual(iv, b"\x1d\xd4\x97A\x06t\xc0\x9bv\xe78o\x84CwF")
+
+    def test_get_key_and_iv_2(self):
+        key, iv = splunksecrets.get_key_and_iv(dbconnect_secret, b"0" * 8)
+        self.assertEqual(
+            key,
+            b"Br\xc9\x08\xecL\x1b\t\x95\x12\xbc\x8c\xa4\xd8\xaf\x9f\x97w\x8dy\x8bS\xd2riJ\x07Ls\x04\x98\x9a",
+        )
+        self.assertEqual(iv, b'\xe9{w^\x8e{s\x81\xe3*\x02\x04\xf8j"\xcb')
+
+    def test_encrypt_dbconnect_legacy(self):
+        ciphertext = splunksecrets.encrypt_dbconnect(
+            dbconnect_secret, "temp1234", salt=b"saup)j99", legacy=True
+        )
+        self.assertEqual(ciphertext, "U2FsdGVkX19zYXVwKWo5ORLxPm8l7hVgaxH/DGPlq3c=")
+
+    def test_encrypt_dbconnect(self):
+        # Mock os.urandom so that IV is always the same
+        with patch("os.urandom", side_effect=lambda size: b"\x01" * size):
+            ciphertext = splunksecrets.encrypt_dbconnect(
+                dbconnect_secret, "temp1234", salt=b"sixteenbytesalt.", legacy=False
+            )
+
+        self.assertEqual(
+            ciphertext,
+            "c2l4dGVlbmJ5dGVzYWx0LgEBAQEBAQEBAQEBAV8SSOUDQd1NzcdSyxk+cU9xxBJNN8bJWg==",
+        )
+
+    def test_decrypt_dbconnect_legacy(self):
+        plaintext = splunksecrets.decrypt_dbconnect(
+            dbconnect_secret, b"U2FsdGVkX19zYXVwKWo5ORLxPm8l7hVgaxH/DGPlq3c="
+        )
+        self.assertEqual(plaintext, "temp1234")
+
+    def test_decrypt_dbconnect(self):
+        plaintext = splunksecrets.decrypt_dbconnect(
+            dbconnect_secret,
+            b"c2l4dGVlbmJ5dGVzYWx0LgEBAQEBAQEBAQEBAV8SSOUDQd1NzcdSyxk+cU9xxBJNN8bJWg==",
+        )
+        self.assertEqual(plaintext, "temp1234")
+
+    def test_end_to_end_dbconnect_legacy(self):
+        __dbconnect_secret = base64.b64encode(os.urandom(32))
+
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt_dbconnect(
+            __dbconnect_secret, plaintext1, legacy=True
+        )
+        plaintext2 = splunksecrets.decrypt_dbconnect(__dbconnect_secret, ciphertext)
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_end_to_end_dbconnect(self):
+        __dbconnect_secret = base64.b64encode(os.urandom(32))
+
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt_dbconnect(
+            __dbconnect_secret, plaintext1, legacy=False
+        )
+        plaintext2 = splunksecrets.decrypt_dbconnect(__dbconnect_secret, ciphertext)
+        self.assertEqual(plaintext2, plaintext1)
