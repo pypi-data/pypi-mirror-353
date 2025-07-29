bcdd3bae824c9e4ce46c5f6f6d1b0bafbe05f2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:29 +0000

    Add package aicourse-dist-prob-asreeman/1.2/aicourse_dist_prob_asreeman-1.2.tar.gz

---
+++ packages/aicourse-dist-prob-asreeman/aicourse_dist_prob_asreeman-1.2.tar.gz/aicourse_dist_prob_asreeman-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="aicourse_dist_prob_asreeman",
+    version="1.2",
+    description="Gaussian and Binomial Distributions",
+    packages=["aicourse_dist_prob_asreeman"],
+    author="Arjun Sree Manoj",
+    author_email="rjsree66@gmail.com",
+    zip_safe=False,
+)
