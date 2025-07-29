56c93c4232287c524eb2c9e4d390a8cde04b6a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:03 +0000

    Add package mtn-momo-sdk/0.1.1/mtn_momo_sdk-0.1.1.tar.gz

---
+++ packages/mtn-momo-sdk/mtn_momo_sdk-0.1.1.tar.gz/mtn_momo_sdk-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mtn-momo-sdk',
+    version='0.1.1',
+    description='MTN MoMo API Collection SDK for Python',
+    author='Bruce Bagarukayo',
+    author_email='bbagarukayo5@gmail.com',
+    url='https://github.com/brucekyl/momo-sdk',
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
