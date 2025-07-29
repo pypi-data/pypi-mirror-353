f42116b7d3722dcb8ed15deecbf30806bc8c5af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:36 +0000

    Add package Arrayer/0.0.5/arrayer-0.0.5-py3-none-any.whl

---
+++ packages/Arrayer/arrayer-0.0.5-py3-none-any.whl/arrayer/__init__.py
@@ -0,0 +1,6 @@
+"""Arrayer: Array-based operations and algorithms."""
+
+from arrayer import dtype, matrix, pcloud, tensor
+from arrayer.pca import pca
+
+__all__ = ["dtype", "matrix", "pca", "pcloud", "tensor"]
