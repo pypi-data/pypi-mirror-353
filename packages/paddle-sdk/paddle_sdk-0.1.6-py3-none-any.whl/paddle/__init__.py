5503cb4a21afac9a85c8405d5a212f88219d8333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:01 +0000

    Add package paddle-sdk/0.1.6/paddle_sdk-0.1.6-py3-none-any.whl

---
+++ packages/paddle-sdk/paddle_sdk-0.1.6-py3-none-any.whl/paddle/__init__.py
@@ -0,0 +1,13 @@
+import warnings
+
+warnings.warn(
+    "This package 'paddle-sdk' is deprecated and replaced by 'paddle.py'. Please use 'paddle.py' instead: https://pypi.org/project/paddle.py/",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+__version__ = "0.1.6"
+__all__ = ["Client", "Environment"]
+
+from .client import Client
+from .environment import Environment
