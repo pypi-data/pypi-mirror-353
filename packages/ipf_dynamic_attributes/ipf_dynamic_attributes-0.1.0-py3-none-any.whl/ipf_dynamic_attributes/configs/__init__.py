3ea8cf7d13a8638fad3932a7beb5c8890530c88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:40 +0000

    Add package ipf_dynamic_attributes/0.1.0/ipf_dynamic_attributes-0.1.0-py3-none-any.whl

---
+++ packages/ipf_dynamic_attributes/ipf_dynamic_attributes-0.1.0-py3-none-any.whl/ipf_dynamic_attributes/configs/__init__.py
@@ -0,0 +1,11 @@
+from .rules import Rule, ConfigRule
+from .sync import Config
+from .values import AttributeValue, ConfigValue
+
+__all__ = [
+    "Config",
+    "Rule",
+    "ConfigRule",
+    "AttributeValue",
+    "ConfigValue",
+]
