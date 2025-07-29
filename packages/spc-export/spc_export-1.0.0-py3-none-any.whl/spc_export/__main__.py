252082c242ed81c446d655e80251a6f051b33ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:49 +0000

    Add package spc-export/1.0.0/spc_export-1.0.0-py3-none-any.whl

---
+++ packages/spc-export/spc_export-1.0.0-py3-none-any.whl/spc_export/__main__.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+import spc
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Utility for parsing spc files.")
+    parser.add_argument("input_file", help="Path to the input file.")
+    parser.add_argument("output_file",nargs="?", default=None, help="Path to the output file.")
+
+    args = parser.parse_args()
+
+    # Validate input file
+    if not os.path.isfile(args.input_file):
+        print(f"Error: Input file '{args.input_file}' does not exist.")
+        return
+
+    record = spc.SPC_Record(args.input_file)
+    record.write_zip_file()
+
+if __name__ == "__main__":
+    main()
+    
