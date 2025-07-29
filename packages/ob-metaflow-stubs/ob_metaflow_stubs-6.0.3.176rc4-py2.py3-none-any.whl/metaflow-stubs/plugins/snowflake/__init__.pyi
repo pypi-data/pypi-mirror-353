8495fda3c6163fa3203087e29bf55139b8298b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:35 +0000

    Add package ob-metaflow-stubs/6.0.3.176rc4/ob_metaflow_stubs-6.0.3.176rc4-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.176rc4-py2.py3-none-any.whl/metaflow-stubs/plugins/snowflake/__init__.pyi
@@ -0,0 +1,14 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-04T19:55:23.711743                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ...mf_extensions.outerbounds.plugins.snowflake import snowflake as snowflake
+from ...mf_extensions.outerbounds.plugins.snowflake.snowflake import connect as connect
+from ...mf_extensions.outerbounds.plugins.snowflake.snowflake import get_snowflake_token as get_snowflake_token
+from ...mf_extensions.outerbounds.plugins.snowflake.snowflake import Snowflake as Snowflake
+
