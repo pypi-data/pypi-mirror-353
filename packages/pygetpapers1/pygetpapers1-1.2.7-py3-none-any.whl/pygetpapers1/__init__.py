da7d85f31ff3350b525d6e93eea0f3ce3de9f310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:35 +0000

    Add package pygetpapers1/1.2.7/pygetpapers1-1.2.7-py3-none-any.whl

---
+++ packages/pygetpapers1/pygetpapers1-1.2.7-py3-none-any.whl/pygetpapers1/__init__.py
@@ -0,0 +1,13 @@
+"""
+pygetpapers1 module
+"""
+from pygetpapers1.download_tools import DownloadTools
+from pygetpapers1.repository.europe_pmc import EuropePmc
+from pygetpapers1.repository.crossref import CrossRef
+from pygetpapers1.repository.arxiv import Arxiv
+from pygetpapers1.repository.rxiv import Rxiv
+from pygetpapers1.repository.rxivist import Rxivist
+from pygetpapers1.pygetpapers1 import pygetpapers1
+
+__author__ = "Ayush Garg"
+__email__ = "ayush@science.org.in"
