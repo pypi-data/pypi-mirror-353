84654b259963d06c59d5e9a17b1cd6fd6a314e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:25 +0000

    Add package almaflow/0.1.7/almaflow-0.1.7-py3-none-any.whl

---
+++ packages/almaflow/almaflow-0.1.7-py3-none-any.whl/almaflow/_state.py
@@ -0,0 +1,15 @@
+import typing
+
+import pydantic
+
+__all__ = [
+    "observable_state",
+]
+
+
+class observable_state(pydantic.BaseModel):
+    _uri_: typing.ClassVar[str]
+
+    def __init_subclass__(klass) -> None:
+        klass._uri_ = f"{klass.__module__}.{klass.__name__}"
+        super().__init_subclass__()
