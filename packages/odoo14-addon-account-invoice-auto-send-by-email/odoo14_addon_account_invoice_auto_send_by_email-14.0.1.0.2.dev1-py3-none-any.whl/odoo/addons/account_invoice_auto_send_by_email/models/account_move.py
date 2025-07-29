514cd13f750d1ffa0c3cc459b21ec534757fb314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:38 +0000

    Add package odoo14-addon-account-invoice-auto-send-by-email/14.0.1.0.2.dev1/odoo14_addon_account_invoice_auto_send_by_email-14.0.1.0.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-account-invoice-auto-send-by-email/odoo14_addon_account_invoice_auto_send_by_email-14.0.1.0.2.dev1-py3-none-any.whl/odoo/addons/account_invoice_auto_send_by_email/models/account_move.py
@@ -0,0 +1,45 @@
+# Copyright 2023 Camptocamp SA
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import _, api, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def cron_send_email_invoice(self):
+        invoices = self.search(self._email_invoice_to_send_domain())
+        for invoice in invoices:
+            description = "Send invoice {} by email".format(invoice.name)
+            invoice.with_delay(description=description)._execute_invoice_sent_wizard()
+
+    def _prepare_invoice_sent_wizard_vals(self):
+        return {"is_email": True, "composition_mode": "mass_mail"}
+
+    def _execute_invoice_sent_wizard(self, options=None):
+        self.ensure_one()
+        if self.is_move_sent:
+            return _("This invoice has already been sent.")
+        if self.transmit_method_code != "mail":
+            return _("This invoice should not send by mail")
+        res = self.action_invoice_sent()
+        wiz_ctx = res["context"] or {}
+        wiz_ctx["active_model"] = self._name
+        wiz_ctx["active_ids"] = self.ids
+        wiz = (
+            self.env["account.invoice.send"]
+            .with_context(**wiz_ctx)
+            .create(self._prepare_invoice_sent_wizard_vals())
+        )
+        return wiz.send_and_print_action()
+
+    @api.model
+    def _email_invoice_to_send_domain(self):
+        return [
+            ("move_type", "in", ("out_invoice", "out_refund")),
+            ("state", "=", "posted"),
+            ("is_move_sent", "=", False),
+            ("transmit_method_code", "=", "mail"),
+            ("payment_state", "=", "not_paid"),
+        ]
