62972f5894d9416893faa6d4d075513db5208b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:58 +0000

    Add package ffwrapy/1.0.1/ffwrapy-1.0.1.tar.gz

---
+++ packages/ffwrapy/ffwrapy-1.0.1.tar.gz/ffwrapy-1.0.1/src/ffwrapy/__init__.py
@@ -0,0 +1,6 @@
+# ffwrapy - A fast, flexible Python wrapper for FFmpeg, FFprobe, and FFplay — fetch metadata, encode, split, and run custom commands easily with real-time progress tracking.
+# Copyright 2025 Bhagya Jyoti
+# Licensed under the Apache License, Version 2.0
+
+
+from .ffmedia import FFMedia
