6e568f1a31dae7667f6d4cb49f50891e6ef10097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:56 +0000

    Add package metaperceptron/2.2.0/metaperceptron-2.2.0-py3-none-any.whl

---
+++ packages/metaperceptron/metaperceptron-2.2.0-py3-none-any.whl/metaperceptron/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 10/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "2.2.0"
+
+from metaperceptron.helpers.preprocessor import Data, DataTransformer
+from metaperceptron.core.gradient_mlp import MlpClassifier, MlpRegressor
+from metaperceptron.core.metaheuristic_mlp import MhaMlpClassifier, MhaMlpRegressor
+from metaperceptron.core.comparator import MhaMlpComparator
+from metaperceptron.core.tuner import MhaMlpTuner
