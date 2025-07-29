c63fe52f9e91616a0168802d714cdd4424e1e7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:41 +0000

    Add package moldenViz/0.2.1/moldenviz-0.2.1.tar.gz

---
+++ packages/moldenViz/moldenviz-0.2.1.tar.gz/moldenviz-0.2.1/src/moldenViz/examples/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2025-present Faria22 <fariafelipe22@hotmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .get_example_files import acrolein, benzene, co, co2, furan, h2o, o2, prismane, pyridine
+
+__all__ = ['acrolein', 'benzene', 'co', 'co2', 'furan', 'h2o', 'o2', 'prismane', 'pyridine']
