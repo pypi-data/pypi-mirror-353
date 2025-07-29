199a795b3383a9e9ed6eadc04cef9e458e434b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:48 +0000

    Add package simple_openid_connect/2.0.0/simple_openid_connect-2.0.0.tar.gz

---
+++ packages/simple_openid_connect/simple_openid_connect-2.0.0.tar.gz/simple_openid_connect-2.0.0/DEVELOPMENT.md
@@ -0,0 +1,82 @@
+# Development Notes and Guides
+
+This file is intended to make it easier for new developers to get along with the code structure
+and style of this project.
+
+## Code Organisation
+
+```text
+py_simple_openid_connect/
+├ src/simple_openid_connect/  (where all the main python code is located)
+│  ├ data.py                  (message definitions for all OIDC interactions)
+│  ├ client.py                (entry point for the high level client api)
+│  ├ <others>                 (implementations for specific OIDC features)
+│  ├ flows/                   (implementations of different authorization flows)
+│  └ integrations/            (additional integrations into external software)
+├ docs/                       (manually written documentation & sphinx config for auto-generated python docs)
+└ tests/                      (test related code)
+   ├ django_test_project/     (a simple django project using the django integration)
+   ├ interactive_tests/       (tests which require user interaction to execute)
+   └ <others>                 (pytest based test suit)
+```
+
+## How to set up a dev environment
+
+### With nix
+
+If you use nix, this project provides a `flake.nix` file that defines a development shell with all dependencies installed.
+It can be entered by typing `nix develop`.
+Additionally, a [nix-direnv](https://github.com/nix-community/nix-direnv) configuration file is also present which also sets up a python environment suitable for development.
+
+### Manually
+
+Ensure you have the following system dependencies installed:
+- `python~=3.9`
+- a python virtual environment manager. This document assumes [uv](https://github.com/astral-sh/uv).
+
+Afterwards, follow the below commands to set up your development environment:
+
+```shell
+# create a virtual python environment
+uv venv
+# install this project + its dev dependencies into the virtual environment
+uv pip install -e .[django,djangorestframework] -r requirements.dev.txt
+# activate the venv python interpreter for use (use the correct activation script for your shell though)
+source .venv/bin/activate
+```
+
+## How to run the Tests
+
+```shell
+pytest
+```
+
+## How to build the Documentation
+
+Assuming that the virtual environment is already activated, the following commands can be executed to build a local
+version of the projects documentation.
+
+```shell
+cd docs
+make html
+```
+
+Afterwards, the documentation is available as html files under `docs/_build/html`.
+To view it, either open the files directly in your browser or use your favourite local http server to serve the content
+(e.g. by running `python -m http.server -d docs/_build/html 8080`).
+
+Sometimes, a clean build is required to update the documentation extracted from the source code.
+This can be done by running `make clean`.
+
+## How to release
+
+In order to release a new version, the following steps are necessary.
+Please keep in mind when picking a new version number that [Semantic Versioning](https://semver.org/) should be followed.
+
+1. Bump version in [\_\_init\_\_.py](./src/simple_openid_connect/__init__.py).
+2. Add entries regarding what changed to [CHANGELOG.md](./CHANGELOG.md).
+3. Commit both changes using `git commit -m "bump version to vX.Y.Z"`
+4. Tag the commit using `git tag -a -s vX.Y.Z`.
+
+   The tag name should be the same as the version number and the tag notes should contain a copy of the changelog entry of the new version.
+5. Publish to pypi using `flit publish`
---
+++ packages/simple_openid_connect/simple_openid_connect-2.0.0.tar.gz/simple_openid_connect-2.0.0/src/simple_openid_connect/integrations/django/views.py
@@ -0,0 +1,206 @@
+"""
+View functions which handle openid authentication and their related callbacks
+"""
+
+import logging
+import secrets
+from datetime import datetime, timedelta, timezone
+from http import HTTPStatus
+from typing import Mapping
+
+from django.conf import settings
+from django.contrib.auth import login, logout
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseRedirect,
+)
+from django.shortcuts import resolve_url
+from django.template.response import TemplateResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.cache import cache_control
+
+from simple_openid_connect.data import (
+    FrontChannelLogoutNotification,
+    IdToken,
+    RpInitiatedLogoutRequest,
+    TokenSuccessResponse,
+)
+from simple_openid_connect.integrations.django.apps import OpenidAppConfig
+from simple_openid_connect.integrations.django.models import OpenidSession
+
+logger = logging.getLogger(__name__)
+
+
+class InvalidAuthStateError(Exception):
+    """
+    Exception that is thrown when the LoginCallbackView is served and the user-agent has no authentication procedure currently in progress
+    """
+
+    def __init__(self) -> None:
+        super().__init__(
+            self,
+            "User-Agent has no authentication procedures in progress so the login-callback will not be processed",
+        )
+
+
+class InvalidNonceError(Exception):
+    """
+    Exception that is thrown when an authentication response contains an invalid or no nonce value
+    """
+
+    def __init__(self) -> None:
+        super().__init__(
+            self, "Authentication response contained an invalid or no nonce value"
+        )
+
+
+class InitLoginView(View):
+    """
+    The view which handles initiating a login.
+
+    It essentially redirects the user agent to the Openid provider.
+    """
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        logout(request)
+        if "next" in request.GET.keys():
+            request.session["login_redirect_url"] = request.GET["next"]
+
+        # save the login state into the session to prevent CSRF attacks
+        # ref: https://simple-openid-connect.readthedocs.io/en/stable/nonce_and_state.html
+        state = secrets.token_urlsafe(32)
+        request.session["openid_auth_state"] = state
+
+        # save the time at which authentication was started
+        request.session["openid_auth_start_time"] = datetime.now(
+            tz=timezone.utc
+        ).timestamp()
+
+        # prevent replay attacks by generating and specifying a nonce
+        # ref: https://simple-openid-connect.readthedocs.io/en/stable/nonce_and_state.html
+        nonce = secrets.token_urlsafe(48)
+        request.session["openid_auth_nonce"] = nonce
+
+        # redirect the user-agent to the oidc provider
+        client = OpenidAppConfig.get_instance().get_client(request)
+        redirect = client.authorization_code_flow.start_authentication(
+            state=state, nonce=nonce
+        )
+        return HttpResponseRedirect(redirect)
+
+
+class LoginCallbackView(View):
+    """
+    The view which handles login callbacks.
+
+    It handles an authentication response from the Openid provider that is encoded in the current url by either logging
+    the user in or rendering the error.
+
+    Error rendering can be customized by overwriting the template *simple_openid_connect/login_failed.html* which
+    receives the context `token_response` of type :class:`TokenErrorResponse <simple_openid_connect.data.TokenErrorResponse>`.
+    """
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        app_settings = OpenidAppConfig.get_instance().safe_settings
+        client = OpenidAppConfig.get_instance().get_client(request)
+
+        # prevent CSRF attacks by verifying the state parameter
+        # ref: https://simple-openid-connect.readthedocs.io/en/stable/nonce_and_state.html
+        if request.session.get("openid_auth_state", None) is None:
+            raise InvalidAuthStateError()
+
+        # don't allow login completion if the process was started too long ago
+        if request.session.get("openid_auth_start_time", None) is None or (
+            datetime.now(tz=timezone.utc)
+            - datetime.fromtimestamp(
+                request.session["openid_auth_start_time"], tz=timezone.utc
+            )
+        ) > timedelta(seconds=app_settings.OPENID_LOGIN_TIMEOUT):
+            raise InvalidAuthStateError()
+        else:
+            del request.session["openid_auth_start_time"]
+
+        # exchange the passed code for tokens
+        token_response = client.authorization_code_flow.handle_authentication_result(
+            current_url=request.get_full_path(),
+            state=request.session["openid_auth_state"],
+        )
+        if not isinstance(token_response, TokenSuccessResponse):
+            return TemplateResponse(
+                request,
+                "simple_openid_connect/login_failed.html",
+                {
+                    "token_response": token_response,
+                },
+                status=HTTPStatus.UNAUTHORIZED,
+            )
+
+        # validate the received tokens
+        id_token = IdToken.parse_jwt(token_response.id_token, client.provider_keys)
+        id_token.validate_extern(
+            client.provider_config.issuer,
+            client.client_auth.client_id,
+            nonce=request.session["openid_auth_nonce"],
+        )
+
+        # handle federated user information (create a new user if necessary or update local info) and log the user in
+        user = OpenidAppConfig.get_instance().user_mapper.handle_federated_userinfo(
+            id_token
+        )
+        openid_session = user.openid.update_session(token_response, id_token)
+        request.session["openid_session"] = openid_session.id
+        login(request, user, backend=settings.AUTHENTICATION_BACKENDS[0])
+
+        # redirect to the next get parameter if present, otherwise to the configured default
+        if "login_redirect_url" in request.session.keys():
+            return HttpResponseRedirect(
+                redirect_to=request.session["login_redirect_url"]
+            )
+        else:
+            return HttpResponseRedirect(
+                redirect_to=resolve_url(settings.LOGIN_REDIRECT_URL)
+            )
+
+
+class LogoutView(View):
+    """
+    The view which handles logging a user out.
+    """
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        session_id = request.session.get("openid_session")
+        logout(request)
+        client = OpenidAppConfig.get_instance().get_client(request)
+
+        if settings.LOGOUT_REDIRECT_URL is not None:
+            openid_session = (
+                OpenidSession.objects.get(id=session_id) if session_id else None
+            )
+
+            logout_request = RpInitiatedLogoutRequest(
+                post_logout_redirect_uri=request.build_absolute_uri(
+                    resolve_url(settings.LOGOUT_REDIRECT_URL)
+                )
+            )
+            if openid_session is not None and openid_session.raw_id_token is not None:
+                logout_request.id_token_hint = openid_session.raw_id_token
+            else:
+                logout_request.client_id = client.client_auth.client_id
+        else:
+            logout_request = None
+
+        return HttpResponseRedirect(client.initiate_logout(logout_request))
+
+
+class FrontChannelLogoutNotificationView(View):
+    """
+    A view which handles Openid front-channel logout notifications by logging out the current session
+    """
+
+    @method_decorator(cache_control(no_store=True))
+    def get(self, request: HttpRequest) -> HttpResponse:
+        logout(request)
+        return HttpResponse(status=200)
