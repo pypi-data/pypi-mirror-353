13eee4b5c16b2d60787b731dd1a79dc4df58f266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:08 +0000

    Add package asaas-python-sdk/1.1.4/asaas_python_sdk-1.1.4.tar.gz

---
+++ packages/asaas-python-sdk/asaas_python_sdk-1.1.4.tar.gz/asaas_python_sdk-1.1.4/src/asaas/dtos/webhook.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import List
+from fmconsult.utils.enum import CustomEnum
+from fmconsult.utils.object import CustomObject
+
+class SendType(CustomEnum):
+  SEQUENTIALLY = 'SEQUENTIALLY'
+  NON_SEQUENTIALLY = 'NON_SEQUENTIALLY'
+
+@dataclass
+class Webhook(CustomObject):
+  name: str
+  url: str
+  email: str
+  events: List[str]
+  enabled: bool = True
+  authToken: str = None
+  apiVersion: str = "3"
+  interrupted: bool = False
+  sendType: SendType = SendType.SEQUENTIALLY
