d4ef135999f18d2c29c56efae35748a01a61673f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:28 +0000

    Add package support-dh/0.0.1/support_dh-0.0.1.tar.gz

---
+++ packages/support-dh/support_dh-0.0.1.tar.gz/support_dh-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="support-dh",
+    version="0.0.1",
+    author="astatine",
+    author_email="astatine.147@gmail.com", 
+    description="decorate text", 
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/astatinegithub/support-dh.git",
+    install_requires=[ 
+    "matplotlib==3.5.2", 
+    "numpy==1.21.5", 
+    "pandas==1.4.4", 
+    "scikit_learn==1.2.0", 
+    "scipy==1.9.1", 
+    "seaborn==0.11.2", 
+    "setuptools==63.4.1", 
+    ],
+    include_package_data=True,
+    packages = setuptools.find_packages(), 
+    python_requires=">=3.9.13", 
+)
