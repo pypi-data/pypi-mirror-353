a737f3dca97b0c4283784cc48d31596b527d8e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:11 +0000

    Add package memoryllm/0.1.1/memoryllm-0.1.1.tar.gz

---
+++ packages/memoryllm/memoryllm-0.1.1.tar.gz/memoryllm-0.1.1/src/memoryllm/__init__.py
@@ -0,0 +1,21 @@
+"""
+MemoryLLM - Persistent Memory Management for Large Language Models
+"""
+
+__version__ = "0.1.1"
+__author__ = "Laurent-Philippe Albou"
+__email__ = "lpalbou@gmail.com"
+
+# Placeholder imports for when the library is implemented
+# from .memory_manager import MemoryManager
+# from .policies import MemoryPolicy
+# from .storage import StorageBackend
+
+__all__ = [
+    "__version__",
+    "__author__", 
+    "__email__",
+    # "MemoryManager",
+    # "MemoryPolicy", 
+    # "StorageBackend",
+] 
