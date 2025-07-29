608fa7dadc3bfe905895cb9de50435e0d973471a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:28 +0000

    Add package coher/0.1.0/coher-0.1.0.tar.gz

---
+++ packages/coher/coher-0.1.0.tar.gz/coher-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coher',  # Change this to your library name
+    version='0.1.0',
+    description='A Python library by Unknown',
+    author='Unknown',
+    author_email='123@email.com',
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here if any
+    python_requires='>=3.6',
+)
