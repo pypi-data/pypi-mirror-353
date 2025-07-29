246acf2ac59e1d5cba84174fe11f5f3949d83b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:15 +0000

    Add package rust-code-analysis-python/0.0.4/rust_code_analysis_python-0.0.4-cp310-cp310-manylinux_2_28_aarch64.whl

---
+++ packages/rust-code-analysis-python/rust_code_analysis_python-0.0.4-cp310-cp310-manylinux_2_28_aarch64.whl/rust_code_analysis_python/__init__.py
@@ -0,0 +1,5 @@
+from .rust_code_analysis_python import *
+
+__doc__ = rust_code_analysis_python.__doc__
+if hasattr(rust_code_analysis_python, "__all__"):
+    __all__ = rust_code_analysis_python.__all__
