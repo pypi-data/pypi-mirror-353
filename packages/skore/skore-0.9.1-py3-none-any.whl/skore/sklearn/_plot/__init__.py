2c08d267553e5d85bbf1f2d6ed4b582f1e610966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:51 +0000

    Add package skore/0.9.1/skore-0.9.1-py3-none-any.whl

---
+++ packages/skore/skore-0.9.1-py3-none-any.whl/skore/sklearn/_plot/__init__.py
@@ -0,0 +1,13 @@
+from skore.sklearn._plot.metrics import (
+    ConfusionMatrixDisplay,
+    PrecisionRecallCurveDisplay,
+    PredictionErrorDisplay,
+    RocCurveDisplay,
+)
+
+__all__ = [
+    "ConfusionMatrixDisplay",
+    "RocCurveDisplay",
+    "PrecisionRecallCurveDisplay",
+    "PredictionErrorDisplay",
+]
