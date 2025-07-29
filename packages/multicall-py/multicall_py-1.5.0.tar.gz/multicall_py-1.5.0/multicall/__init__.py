c6d8fc5c7b1fe6fd511b526e993ea02f6462a1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:41 +0000

    Add package multicall-py/1.5.0/multicall_py-1.5.0.tar.gz

---
+++ packages/multicall-py/multicall_py-1.5.0.tar.gz/multicall_py-1.5.0/multicall/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.5.0"
+
+from .call import Call
+from .multicall import Multicall
+from .balance_call import BalanceCall
+from .signature import Signature
+
+__all__ = ["Call", "Multicall", "BalanceCall", "Signature"]
