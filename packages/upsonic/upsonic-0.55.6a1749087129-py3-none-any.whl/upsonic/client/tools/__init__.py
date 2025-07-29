47830a187ec900e9f90f9dddfbc8ec16234f2c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:10:43 +0000

    Add package upsonic/0.55.6a1749087129/upsonic-0.55.6a1749087129-py3-none-any.whl

---
+++ packages/upsonic/upsonic-0.55.6a1749087129-py3-none-any.whl/upsonic/client/tools/__init__.py
@@ -0,0 +1,6 @@
+from .tools import ComputerUse, Search, BrowserUse
+
+
+
+
+__all__ = ["ComputerUse", "Search", "BrowserUse"]
---
+++ packages/upsonic/upsonic-0.55.6a1749087129-py3-none-any.whl/upsonic/server/level_utilized/cu/__init__.py
@@ -0,0 +1,17 @@
+
+
+from .computer import ComputerTool
+
+from .computer import ComputerUse_tools, ComputerUse_screenshot_tool, ComputerUse_screenshot_tool_bytes
+
+__ALL__ = [
+
+
+    ComputerTool,
+
+
+
+    ComputerUse_tools,
+    ComputerUse_screenshot_tool,
+    ComputerUse_screenshot_tool_bytes
+]
