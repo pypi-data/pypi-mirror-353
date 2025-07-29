8afd5eb8c1d4fe6abec4e1040e66f886f4f6a4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:10:02 +0000

    Add package D7s-Style/0.0.1/d7s_style-0.0.1-py3-none-any.whl

---
+++ packages/D7s-Style/d7s_style-0.0.1-py3-none-any.whl/D7s_Style/__init__.py
@@ -0,0 +1,7 @@
+from D7s_Style.D7s_Style import Loading
+from D7s_Style.D7s_Style import Animation
+from D7s_Style.D7s_Style import D7s_Style
+from D7s_Style.D7s_Style import My_Style
+
+## The DARK7STORM :
+#-------------------------------------
