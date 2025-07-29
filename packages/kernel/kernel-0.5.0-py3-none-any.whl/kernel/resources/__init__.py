a40639e07158067d36d90f0349c2c3b157c95b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:47 +0000

    Add package kernel/0.5.0/kernel-0.5.0-py3-none-any.whl

---
+++ packages/kernel/kernel-0.5.0-py3-none-any.whl/kernel/resources/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .apps import (
+    AppsResource,
+    AsyncAppsResource,
+    AppsResourceWithRawResponse,
+    AsyncAppsResourceWithRawResponse,
+    AppsResourceWithStreamingResponse,
+    AsyncAppsResourceWithStreamingResponse,
+)
+from .browsers import (
+    BrowsersResource,
+    AsyncBrowsersResource,
+    BrowsersResourceWithRawResponse,
+    AsyncBrowsersResourceWithRawResponse,
+    BrowsersResourceWithStreamingResponse,
+    AsyncBrowsersResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "AppsResource",
+    "AsyncAppsResource",
+    "AppsResourceWithRawResponse",
+    "AsyncAppsResourceWithRawResponse",
+    "AppsResourceWithStreamingResponse",
+    "AsyncAppsResourceWithStreamingResponse",
+    "BrowsersResource",
+    "AsyncBrowsersResource",
+    "BrowsersResourceWithRawResponse",
+    "AsyncBrowsersResourceWithRawResponse",
+    "BrowsersResourceWithStreamingResponse",
+    "AsyncBrowsersResourceWithStreamingResponse",
+]
