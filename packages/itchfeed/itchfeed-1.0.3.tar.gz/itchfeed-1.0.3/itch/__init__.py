7b53144f3f5124a92261bb353bfddaef077ba20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:55 +0000

    Add package itchfeed/1.0.3/itchfeed-1.0.3.tar.gz

---
+++ packages/itchfeed/itchfeed-1.0.3.tar.gz/itchfeed-1.0.3/itch/__init__.py
@@ -0,0 +1,10 @@
+"""
+Nasdaq TotalView-ITCH 5.0 Parser
+"""
+
+__author__ = "Bertin Balouki SIMYELI"
+__copyright__ = "2025 Bertin Balouki SIMYELI"
+__email__ = "bertin@bbstrader.com"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
