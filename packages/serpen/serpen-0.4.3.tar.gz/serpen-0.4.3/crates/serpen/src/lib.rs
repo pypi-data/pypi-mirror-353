03262e99b170498fffe96f30152111aea5a7803e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:40 +0000

    Add package serpen/0.4.3/serpen-0.4.3.tar.gz

---
+++ packages/serpen/serpen-0.4.3.tar.gz/serpen-0.4.3/crates/serpen/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod ast_rewriter;
+pub mod bundler;
+pub mod config;
+pub mod dependency_graph;
+pub mod emit;
+pub mod resolver;
+pub mod unused_import_trimmer;
+pub mod unused_imports_simple;
+pub mod util;
+
+pub use bundler::Bundler;
+pub use config::Config;
