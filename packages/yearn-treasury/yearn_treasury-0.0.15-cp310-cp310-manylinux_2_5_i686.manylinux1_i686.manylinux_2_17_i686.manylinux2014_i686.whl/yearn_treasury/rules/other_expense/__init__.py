877bf55315668a72882ce52ce5d57801f1027242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:06 +0000

    Add package yearn-treasury/0.0.15/yearn_treasury-0.0.15-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/yearn-treasury/yearn_treasury-0.0.15-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/yearn_treasury/rules/other_expense/__init__.py
@@ -0,0 +1,6 @@
+from yearn_treasury.rules.other_expense.boost import *
+from yearn_treasury.rules.other_expense.bugs import *
+from yearn_treasury.rules.other_expense.dyfi import *
+from yearn_treasury.rules.other_expense.events import *
+from yearn_treasury.rules.other_expense.misc import *
+from yearn_treasury.rules.other_expense.revshare import *
