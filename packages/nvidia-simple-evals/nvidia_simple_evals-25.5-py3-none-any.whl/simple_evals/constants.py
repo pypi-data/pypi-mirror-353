dd4fec5fce65f7b6b7bbb7d6122f0357b412ccd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:14 +0000

    Add package nvidia-simple-evals/25.5/nvidia_simple_evals-25.5-py3-none-any.whl

---
+++ packages/nvidia-simple-evals/nvidia_simple_evals-25.5-py3-none-any.whl/simple_evals/constants.py
@@ -0,0 +1,10 @@
+MULTILINGUAL_MMLU = [
+    'mmlu_EN-US', 'mmlu_AR-XY', 'mmlu_BN-BD', 'mmlu_DE-DE', 'mmlu_ES-LA',
+    'mmlu_FR-FR', 'mmlu_HI-IN', 'mmlu_ID-ID', 'mmlu_IT-IT', 'mmlu_JA-JP',
+    'mmlu_KO-KR', 'mmlu_PT-BR', 'mmlu_ZH-CN', 'mmlu_SW-KE', 'mmlu_YO-NG'
+]
+
+GPQA_VARIANTS = [
+    "gpqa_diamond", "gpqa_experts", "gpqa_extended", "gpqa_main",
+    "gpqa_diamond_nemo"
+]
