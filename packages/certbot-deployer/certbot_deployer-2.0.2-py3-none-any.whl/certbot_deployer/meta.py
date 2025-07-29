01d37da4b1ae56fbf8697ffb2bb2ed8cf5528174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:20 +0000

    Add package certbot-deployer/2.0.2/certbot_deployer-2.0.2-py3-none-any.whl

---
+++ packages/certbot-deployer/certbot_deployer-2.0.2-py3-none-any.whl/certbot_deployer/meta.py
@@ -0,0 +1,9 @@
+"""
+Package metadata
+"""
+
+__author__ = "IAS"
+__version__ = "2.0.2"
+__title__ = "certbot_deployer"
+__prog__ = "certbot-deployer"
+__license__ = "License :: OSI Approved :: MIT License"
