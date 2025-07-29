649fd4e08abe6da61d8ba77e37038e54483d6ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:06 +0000

    Add package gov-uk-dashboards/23.1.0/gov_uk_dashboards-23.1.0-py3-none-any.whl

---
+++ packages/gov-uk-dashboards/gov_uk_dashboards-23.1.0-py3-none-any.whl/gov_uk_dashboards/template.py
@@ -0,0 +1,18 @@
+"""
+Read government dashboard template
+"""
+import os
+
+
+def read_template(favicon_path: str = r"assets\images\MHCLG_favicon.png") -> str:
+    """
+    Read the government html template.
+    Args:
+        favicon_path (str): Optional file path to favicon.
+    :return: String version of the template.
+    """
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, "template.html")
+    with open(path, encoding="utf-8") as file:
+        template = file.read()
+        return template.replace("{{favicon_path}}", favicon_path)
