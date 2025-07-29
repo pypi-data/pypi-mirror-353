36a40892dda71f34aebd1b38b34e6e5d3ad5ca6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:58 +0000

    Add package path-explorator/0.0.4.7/path_explorator-0.0.4.7.tar.gz

---
+++ packages/path-explorator/path_explorator-0.0.4.7.tar.gz/path_explorator-0.0.4.7/src/path_explorator/core/explorer/path/path_creator.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+class PathCreator:
+    def __init__(self):
+        pass
+
+    def join_paths(self, path1, path2):
+        new_path = Path(path1 / path2)
+        return new_path.__str__()
+
