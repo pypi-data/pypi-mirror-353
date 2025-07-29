2ff8b47cb23e28f425c9814dbd1d9e878f303c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:32 +0000

    Add package django-bulk-lifecycle/0.1.50/django_bulk_lifecycle-0.1.50-py3-none-any.whl

---
+++ packages/django-bulk-lifecycle/django_bulk_lifecycle-0.1.50-py3-none-any.whl/django_bulk_lifecycle/models.py
@@ -0,0 +1,25 @@
+from django_bulk_lifecycle.manager import BulkLifecycleManager
+from django.db import models, transaction
+
+
+class LifecycleModelMixin(models.Model):
+    objects = BulkLifecycleManager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, *args, **kwargs):
+        self.before_delete()
+
+        with transaction.atomic():
+            result = super().delete(*args, **kwargs)
+
+        self.after_delete()
+
+        return result
+
+    def before_delete(self):
+        pass
+
+    def after_delete(self):
+        pass
