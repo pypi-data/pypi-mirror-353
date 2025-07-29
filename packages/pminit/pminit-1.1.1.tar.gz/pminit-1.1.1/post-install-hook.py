953c55b1b360bcc666020432484906f177ac2f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:26 +0000

    Add package pminit/1.1.1/pminit-1.1.1.tar.gz

---
+++ packages/pminit/pminit-1.1.1.tar.gz/pminit-1.1.1/post-install-hook.py
@@ -0,0 +1,13 @@
+import os
+import pmpm
+
+WORK_DIR = os.path.join(
+  os.path.realpath(os.path.dirname(__file__)),
+  "pythonmonkey"
+)
+
+def main():
+  pmpm.main(WORK_DIR) # cd pythonmonkey && npm i
+
+if __name__ == "__main__":
+  main()
