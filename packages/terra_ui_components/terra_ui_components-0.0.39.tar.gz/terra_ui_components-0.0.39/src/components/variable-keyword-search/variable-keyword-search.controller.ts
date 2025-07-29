e6c8d6cb703c50f8d8474871e2b4c802835a2306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:27 +0000

    Add package terra_ui_components/0.0.39/terra_ui_components-0.0.39.tar.gz

---
+++ packages/terra_ui_components/terra_ui_components-0.0.39.tar.gz/terra_ui_components-0.0.39/src/components/variable-keyword-search/variable-keyword-search.controller.ts
@@ -0,0 +1,54 @@
+import type { StatusRenderer } from '@lit/task'
+import { Task, TaskStatus } from '@lit/task'
+import type { ReactiveControllerHost } from 'lit'
+import type { ListItem, ReadableTaskStatus } from './variable-keyword-search.types.js'
+
+const apiError = new Error(
+    'Failed to fetch the data required to make a list of searchable items.'
+)
+
+export class FetchController {
+    #apiTask: Task<[], any[]>
+
+    constructor(host: ReactiveControllerHost) {
+        this.#apiTask = new Task(host, {
+            task: async () => {
+                /** @see {@link https://solr.apache.org/guide/solr/latest/query-guide/terms-component.html} */
+                const response = await fetch(
+                    'https://windmill-load-balancer-641499207.us-east-1.elb.amazonaws.com/api/r/giovanni/aesir-keywords'
+                )
+
+                if (!response.ok) {
+                    throw apiError
+                }
+
+                return await response.json()
+            },
+            args: (): any => [],
+        })
+    }
+
+    get taskComplete() {
+        return this.#apiTask.taskComplete
+    }
+
+    get value() {
+        return this.#apiTask.value
+    }
+
+    get taskStatus() {
+        const readableStatus = Object.entries(TaskStatus).reduce<
+            Record<number, ReadableTaskStatus>
+        >((accumulator, [key, value]) => {
+            accumulator[value] = key as ReadableTaskStatus
+
+            return accumulator
+        }, {})
+
+        return readableStatus[this.#apiTask.status]
+    }
+
+    render(renderFunctions: StatusRenderer<ListItem[]>) {
+        return this.#apiTask.render(renderFunctions)
+    }
+}
