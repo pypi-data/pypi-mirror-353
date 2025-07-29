91723318e1e4cfa2baad89d2c788b5164a287606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:09:24 +0000

    Add package patpy/0.9.1/patpy-0.9.1-py3-none-any.whl

---
+++ packages/patpy/patpy-0.9.1-py3-none-any.whl/patpy/tl/__init__.py
@@ -0,0 +1,25 @@
+from .evaluation import (
+    evaluate_prediction,
+    evaluate_representation,
+    persistence_evaluation,
+    predict_knn,
+    test_distances_significance,
+    test_proportions,
+)
+from .sample_representation import (
+    MOFA,
+    PILOT,
+    CellGroupComposition,
+    DiffusionEarthMoverDistance,
+    GloScope,
+    GroupedPseudobulk,
+    MrVI,
+    Pseudobulk,
+    RandomVector,
+    SampleRepresentationMethod,
+    SCPoli,
+    WassersteinTSNE,
+    correlate_cell_type_expression,
+    correlate_composition,
+    describe_metadata,
+)
