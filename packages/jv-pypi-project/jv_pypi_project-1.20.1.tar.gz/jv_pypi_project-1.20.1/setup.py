c2c2a59189b4a094e0906239c2ffb9f5fbe43d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:34 +0000

    Add package jv-pypi-project/1.20.1/jv_pypi_project-1.20.1.tar.gz

---
+++ packages/jv-pypi-project/jv_pypi_project-1.20.1.tar.gz/jv_pypi_project-1.20.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jv_pypi_project",
+    home_page="xxx.xx",
+    version="1.20.1",
+    author="netel",
+    author_email="xx@xx.xx",
+    license="MIT License",
+    install_requires = "websocket",
+    description="A simple test project for PyPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
