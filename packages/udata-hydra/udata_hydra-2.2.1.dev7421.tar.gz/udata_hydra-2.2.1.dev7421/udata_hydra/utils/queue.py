4d2030ad63a44c330da44132583e27af9f9a1a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:45 +0000

    Add package udata-hydra/2.2.1.dev7421/udata_hydra-2.2.1.dev7421.tar.gz

---
+++ packages/udata-hydra/udata_hydra-2.2.1.dev7421.tar.gz/udata_hydra-2.2.1.dev7421/udata_hydra/utils/queue.py
@@ -0,0 +1,16 @@
+from udata_hydra import context
+from udata_hydra.logger import setup_logging
+
+log = setup_logging()
+
+
+def enqueue(fn, *args, **kwargs):
+    """
+    Enqueue a task
+    Simple helper useful to facilitate mock in testing
+    Should be used like this (import matters):
+        from udata_hydra import queue
+        queue.enqueue(fn, a, b=b, _priority="low")
+    """
+    priority = kwargs.pop("_priority", "default")
+    return context.queue(priority).enqueue(fn, *args, **kwargs)
