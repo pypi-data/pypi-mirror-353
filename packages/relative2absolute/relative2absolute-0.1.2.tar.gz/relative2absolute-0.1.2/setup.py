81b32ee51f17ca393795f83a6da05b69cb3e31ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:27 +0000

    Add package relative2absolute/0.1.2/relative2absolute-0.1.2.tar.gz

---
+++ packages/relative2absolute/relative2absolute-0.1.2.tar.gz/relative2absolute-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='relative2absolute',
+    version='0.1.2',
+    description='Convert relative imports in a Python package to absolute imports.',
+    author='Sami Jneidy',
+    author_email='samihanijneidy@gmail.com',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'relative2absolute=relative2absolute.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    readme = "README.md"
+)
