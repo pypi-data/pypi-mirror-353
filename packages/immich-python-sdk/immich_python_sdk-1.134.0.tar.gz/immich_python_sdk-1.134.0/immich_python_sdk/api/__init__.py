e383ce7dba430c85d3eb449e4582e1434ae872bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:57 +0000

    Add package immich-python-sdk/1.134.0/immich_python_sdk-1.134.0.tar.gz

---
+++ packages/immich-python-sdk/immich_python_sdk-1.134.0.tar.gz/immich_python_sdk-1.134.0/immich_python_sdk/api/__init__.py
@@ -0,0 +1,36 @@
+# flake8: noqa
+
+# import apis into api package
+from immich_python_sdk.api.api_keys_api import APIKeysApi
+from immich_python_sdk.api.activities_api import ActivitiesApi
+from immich_python_sdk.api.albums_api import AlbumsApi
+from immich_python_sdk.api.assets_api import AssetsApi
+from immich_python_sdk.api.authentication_api import AuthenticationApi
+from immich_python_sdk.api.deprecated_api import DeprecatedApi
+from immich_python_sdk.api.download_api import DownloadApi
+from immich_python_sdk.api.duplicates_api import DuplicatesApi
+from immich_python_sdk.api.faces_api import FacesApi
+from immich_python_sdk.api.jobs_api import JobsApi
+from immich_python_sdk.api.libraries_api import LibrariesApi
+from immich_python_sdk.api.map_api import MapApi
+from immich_python_sdk.api.memories_api import MemoriesApi
+from immich_python_sdk.api.notifications_api import NotificationsApi
+from immich_python_sdk.api.notifications_admin_api import NotificationsAdminApi
+from immich_python_sdk.api.o_auth_api import OAuthApi
+from immich_python_sdk.api.partners_api import PartnersApi
+from immich_python_sdk.api.people_api import PeopleApi
+from immich_python_sdk.api.search_api import SearchApi
+from immich_python_sdk.api.server_api import ServerApi
+from immich_python_sdk.api.sessions_api import SessionsApi
+from immich_python_sdk.api.shared_links_api import SharedLinksApi
+from immich_python_sdk.api.stacks_api import StacksApi
+from immich_python_sdk.api.sync_api import SyncApi
+from immich_python_sdk.api.system_config_api import SystemConfigApi
+from immich_python_sdk.api.system_metadata_api import SystemMetadataApi
+from immich_python_sdk.api.tags_api import TagsApi
+from immich_python_sdk.api.timeline_api import TimelineApi
+from immich_python_sdk.api.trash_api import TrashApi
+from immich_python_sdk.api.users_api import UsersApi
+from immich_python_sdk.api.users_admin_api import UsersAdminApi
+from immich_python_sdk.api.view_api import ViewApi
+
