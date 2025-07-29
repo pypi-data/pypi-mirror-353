1ec786dc7c1de9eeaeb0046e75cb051d9558e477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:22 +0000

    Add package model-wrapper/1.1.0/model-wrapper-1.1.0.tar.gz

---
+++ packages/model-wrapper/model-wrapper-1.1.0.tar.gz/model-wrapper-1.1.0/model_wrapper/__init__.py
@@ -0,0 +1,32 @@
+from model_wrapper.training import predict_dataset, acc_predict_dataset
+from model_wrapper.wrapper import (
+    ModelWrapper,
+    FastModelWrapper,
+    FastModelWrapper as SimpleModelWrapper,
+    SplitModelWrapper,
+    ClassifyModelWrapper,
+    FastClassifyModelWrapper,
+    FastClassifyModelWrapper as SimpleClassifyModelWrapper,
+    SplitClassifyModelWrapper,
+    RegressModelWrapper,
+    FastRegressModelWrapper,
+    FastRegressModelWrapper as SimpleRegressModelWrapper,
+    SplitRegressModelWrapper,
+)
+
+__all__ = [
+    "ModelWrapper",
+    "FastModelWrapper",
+    "SimpleModelWrapper",
+    "SplitModelWrapper",
+    "ClassifyModelWrapper",
+    "FastClassifyModelWrapper",
+    "SimpleClassifyModelWrapper",
+    "SplitClassifyModelWrapper",
+    "RegressModelWrapper",
+    "FastRegressModelWrapper",
+    "SimpleRegressModelWrapper",
+    "SplitRegressModelWrapper",
+    "predict_dataset",
+    "acc_predict_dataset",
+]
