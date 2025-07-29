c6ad57f0f08865de625600ae1025f950581100b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:19 +0000

    Add package odoo-addon-sale-layout-category-hide-detail/16.0.1.1.2.2/odoo_addon_sale_layout_category_hide_detail-16.0.1.1.2.2-py3-none-any.whl

---
+++ packages/odoo-addon-sale-layout-category-hide-detail/odoo_addon_sale_layout_category_hide_detail-16.0.1.1.2.2-py3-none-any.whl/odoo/addons/sale_layout_category_hide_detail/static/src/js/sale_layout_category_hide_detail.esm.js
@@ -0,0 +1,68 @@
+/** @odoo-module **/
+/* Copyright 2019 Tecnativa - Ernesto Tejeda
+/* Copyright 2022 Tecnativa - Víctor Martínez
+/* Copyright 2023 Tecnativa - Yadier Quesada
+/* License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).*/
+
+import {SectionAndNoteListRenderer} from "@account/components/section_and_note_fields_backend/section_and_note_fields_backend";
+import {patch} from "@web/core/utils/patch";
+
+patch(SectionAndNoteListRenderer.prototype, "new_widgets_buttons_patch", {
+    getColumns(record) {
+        // Set record to use it in getSectionColumns()
+        this.record = record;
+        return this._super.apply(this, arguments);
+    },
+    getSectionColumns(columns) {
+        // We do not want to display icons in notes, only in sections
+        if (this.record.data.display_type !== "line_section") {
+            return this._super.apply(this, arguments);
+        }
+        var sectionCols = this._super.apply(this, arguments);
+        const widgetCols = columns.filter((col) => col.widget === "boolean_fa_icon");
+        const sectionWidget = widgetCols.map((col) => {
+            return {...col, colspan: 1};
+        });
+        sectionCols.forEach(function (item) {
+            if (item.colspan > 1) {
+                item.colspan -= widgetCols.length;
+            }
+        });
+        return sectionCols.concat(sectionWidget);
+    },
+
+    getCellClass(column) {
+        var classNames = this._super.apply(this, arguments);
+        if (column.widget === "boolean_fa_icon") {
+            classNames = classNames.replace("o_hidden", "");
+        }
+        return classNames;
+    },
+
+    getColumnClass(column) {
+        if (column.widget === "boolean_fa_icon") {
+            column.hasLabel = false;
+        }
+        return this._super.apply(this, arguments);
+    },
+    /**
+     * @override method from ListRenderer.isSortable
+     * @param {Object} column - The column to render
+     */
+    isSortable(column) {
+        if (column.widget === "boolean_fa_icon") {
+            return false;
+        }
+        return this._super.apply(this, arguments);
+    },
+    /**
+     * @override method from ListRenderer.calculateColumnWidth
+     * @param {Object} column - The column to render
+     */
+    calculateColumnWidth(column) {
+        if (column.widget === "boolean_fa_icon") {
+            return {type: "absolute", value: "20px"};
+        }
+        return this._super.apply(this, arguments);
+    },
+});
---
+++ packages/odoo-addon-sale-layout-category-hide-detail/odoo_addon_sale_layout_category_hide_detail-16.0.1.1.2.2-py3-none-any.whl/odoo/addons/sale_layout_category_hide_detail/tests/test_sale_layout_category_hide_detail.py
@@ -0,0 +1,54 @@
+# Copyright 2019 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import tagged
+
+from odoo.addons.sale.tests.common import SaleCommon
+
+
+@tagged("post_install", "-at_install")
+class TestSaleLayoutCategoryHideDetail(SaleCommon):
+    @classmethod
+    def setUpClass(cls):
+        super(TestSaleLayoutCategoryHideDetail, cls).setUpClass()
+        if not cls.env.company.chart_template_id:
+            # Load a CoA if there's none in current company
+            coa = cls.env.ref("l10n_generic_coa.configurable_chart_template", False)
+            if not coa:
+                # Load the first available CoA
+                coa = cls.env["account.chart.template"].search(
+                    [("visible", "=", True)], limit=1
+                )
+            coa.try_loading(company=cls.env.company, install_demo=False)
+        cls.product = cls.env["product.product"].create(
+            {"name": "Producto test", "type": "consu", "invoice_policy": "order"}
+        )
+        cls.partner = cls.env["res.partner"].create({"name": "partner_test"})
+        cls.sale_order = cls.env["sale.order"].create({"partner_id": cls.partner.id})
+        cls.so_line = cls.env["sale.order.line"].create(
+            {
+                "order_id": cls.sale_order.id,
+                "product_id": cls.product.id,
+                "product_uom_qty": 10,
+            }
+        )
+        cls.sale_order.action_confirm()
+
+    def test_prepare_invoice_line(self):
+        res = self.so_line._prepare_invoice_line()
+        self.assertEqual(res["quantity"], 10)
+        self.assertEqual(res["product_id"], self.product.id)
+        self.assertEqual(res["show_details"], True)
+        self.assertEqual(res["show_subtotal"], True)
+        self.so_line.write({"show_details": False, "show_subtotal": False})
+        res = self.so_line._prepare_invoice_line()
+        self.assertEqual(res["quantity"], 10)
+        self.assertEqual(res["product_id"], self.product.id)
+        self.assertEqual(res["show_details"], False)
+        self.assertEqual(res["show_subtotal"], False)
+
+    def test_create_invoices(self):
+        self.so_line.write({"show_details": False, "show_subtotal": False})
+        invoice = self.sale_order._create_invoices()
+        self.assertEqual(invoice.invoice_line_ids.show_details, False)
+        self.assertEqual(invoice.invoice_line_ids.show_subtotal, False)
