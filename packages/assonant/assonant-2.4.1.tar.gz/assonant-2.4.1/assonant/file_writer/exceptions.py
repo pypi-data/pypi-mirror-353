513d1411f504cde90e9674fb8402b88e21a0baca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:38 +0000

    Add package assonant/2.4.1/assonant-2.4.1.tar.gz

---
+++ packages/assonant/assonant-2.4.1.tar.gz/assonant-2.4.1/assonant/file_writer/exceptions.py
@@ -0,0 +1,10 @@
+"""Exceptions triggered to warn about internal errors in Assonant File Writer module."""
+
+
+class AssonantFileWriterError(Exception):
+    """Assonant Data Sender Error.
+
+    Custom Exception to warn when any error occurs inside AssonantFileWriter
+    """
+
+    pass
