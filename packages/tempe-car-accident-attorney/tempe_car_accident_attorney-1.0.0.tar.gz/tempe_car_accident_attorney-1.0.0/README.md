bb3aba24a845b2f0304931025836391af7e3da9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:37 +0000

    Add package tempe-car-accident-attorney/1.0.0/tempe_car_accident_attorney-1.0.0.tar.gz

---
+++ packages/tempe-car-accident-attorney/tempe_car_accident_attorney-1.0.0.tar.gz/tempe_car_accident_attorney-1.0.0/README.md
@@ -0,0 +1,3 @@
+# Tempe Car Accident Attorney
+
+Looking for guidance after an Arizona crash? Visit our [Tempe car accident attorney](https://tempeazcaraccidentattorney.com/) page for legal help, insurance advice, and local injury recovery resources.
---
+++ packages/tempe-car-accident-attorney/tempe_car_accident_attorney-1.0.0.tar.gz/tempe_car_accident_attorney-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='tempe-car-accident-attorney',
+    version='1.0.0',
+    description='Tempe car accident legal resource reference.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Las Vegas Car Accident Lawyer',
+    url='https://tempeazcaraccidentattorney.com/',
+    project_urls={
+        'Homepage': 'https://tempeazcaraccidentattorney.com/',
+        'Free Case Review': 'https://tempeazcaraccidentattorney.com/contact/',
+        'Legal Blog': 'https://tempeazcaraccidentattorney.com/blog/',
+    },
+    packages=['tempe_car_accident_attorney'],
+)
