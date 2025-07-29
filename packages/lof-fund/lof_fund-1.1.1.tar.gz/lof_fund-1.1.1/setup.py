2ca1acc96b7dd0e0d58fef33535e04a2cb07ef9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:59 +0000

    Add package lof-fund/1.1.1/lof_fund-1.1.1.tar.gz

---
+++ packages/lof-fund/lof_fund-1.1.1.tar.gz/lof_fund-1.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="lof-fund",
+    version="1.1.1",
+    author="Egg*4",
+    author_email="1127885315@qq.com",  # 替换为你的邮箱
+    description="获取基金溢价率的脚本",
+    long_description=open("./README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["lof"],
+    install_requires=[
+        "chinesecalendar",
+        "schedule",
+        "requests",
+        "pandas",
+        "akshare",
+        "beautifulsoup4",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "lofpm=lof:main"
+        ]
+    },
+)
