ebef3ea99a6b5473e14786ed9fd15db5e46b922d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:56 +0000

    Add package cepik-vehicle-history-client/0.1.0/cepik_vehicle_history_client-0.1.0-py3-none-any.whl

---
+++ packages/cepik-vehicle-history-client/cepik_vehicle_history_client-0.1.0-py3-none-any.whl/cepik_vehicle_history_client/__init__.py
@@ -0,0 +1,14 @@
+from .client import VehicleHistoryClient, VehicleInfo, VehicleHistoryClientError
+
+__all__ = [
+    "VehicleHistoryClient",
+    "VehicleInfo",
+    "VehicleHistoryClientError",
+]
+
+__version__ = "0.1.0"
+__author__ = "Mateusz Tatko"
+__email__ = "mtatko@mtatko.dev"
+__license__ = "MIT"
+__url__ = "https://github.com/mtatko/cepik-vehicle-history-client"
+__description__ = "A Python client for the CEPiK vehicle history service."
