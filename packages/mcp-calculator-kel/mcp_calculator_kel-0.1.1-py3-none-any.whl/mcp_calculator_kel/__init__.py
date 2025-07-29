4e21c5358d8537981ba8d6bd864d50b53368d08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:20 +0000

    Add package mcp-calculator-kel/0.1.1/mcp_calculator_kel-0.1.1-py3-none-any.whl

---
+++ packages/mcp-calculator-kel/mcp_calculator_kel-0.1.1-py3-none-any.whl/mcp_calculator_kel/__init__.py
@@ -0,0 +1,9 @@
+from . import server
+
+def main():
+    """Main entry point for the package."""
+    server.main()  # 修改为调用server.py中的main函数
+
+
+# Expose important items at package level
+__all__ = ['main', 'server']
