d88bff13680ab33c1f9b97d64261478012297ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:07 +0000

    Add package geospatial/0.11.6/geospatial-0.11.6.tar.gz

---
+++ packages/geospatial/geospatial-0.11.6.tar.gz/geospatial-0.11.6/tests/test_geospatial.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geospatial` package."""
+
+
+import unittest
+
+from geospatial import geospatial
+
+
+class TestGeospatial(unittest.TestCase):
+    """Tests for `geospatial` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
