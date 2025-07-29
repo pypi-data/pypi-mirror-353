744116a17ef222664db3a6ff0973aeadf5f29f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:39 +0000

    Add package runloop-api-client/0.39.0/runloop_api_client-0.39.0-py3-none-any.whl

---
+++ packages/runloop-api-client/runloop_api_client-0.39.0-py3-none-any.whl/runloop_api_client/types/benchmark_run_view.py
@@ -0,0 +1,57 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List, Optional
+from typing_extensions import Literal
+
+from pydantic import Field as FieldInfo
+
+from .._models import BaseModel
+from .scoring_contract_result_view import ScoringContractResultView
+
+__all__ = ["BenchmarkRunView", "ScenarioRun"]
+
+
+class ScenarioRun(BaseModel):
+    scenario_id: str
+    """ID of the Scenario that has been run."""
+
+    scoring_result: ScoringContractResultView = FieldInfo(alias="scoringResult")
+    """The scoring result of the ScenarioRun."""
+
+    scenario_run_id: Optional[str] = FieldInfo(alias="scenarioRunId", default=None)
+    """ID of the scenario run."""
+
+
+class BenchmarkRunView(BaseModel):
+    id: str
+    """The ID of the BenchmarkRun."""
+
+    benchmark_id: str
+    """The ID of the Benchmark."""
+
+    metadata: Dict[str, str]
+    """User defined metadata to attach to the benchmark run for organization."""
+
+    pending_scenarios: List[str]
+    """List of Scenarios that have yet to be scored."""
+
+    scenario_runs: List[ScenarioRun]
+    """List of Scenarios have been completed."""
+
+    start_time_ms: int
+    """The time the benchmark run execution started (Unix timestamp milliseconds)."""
+
+    state: Literal["running", "canceled", "completed"]
+    """The state of the BenchmarkRun."""
+
+    duration_ms: Optional[int] = None
+    """The duration for the BenchmarkRun to complete."""
+
+    name: Optional[str] = None
+    """The name of the BenchmarkRun."""
+
+    score: Optional[float] = None
+    """The final score across the BenchmarkRun, present once completed.
+
+    Calculated as sum of scenario scores / number of scenario runs.
+    """
