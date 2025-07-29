e543abd48166bf50c4fd4632f1e844e614b1869f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:15 +0000

    Add package bdext/0.1.32/bdext-0.1.32.tar.gz

---
+++ packages/bdext/bdext-0.1.32.tar.gz/bdext-0.1.32/bdeissct_dl/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+import warnings
+
+# Numpy emits a warning for log 0, but we might get it a lot during likelihood calculations
+warnings.filterwarnings('ignore', r'divide by zero encountered in log')
+
+
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'models')
+TRAINING_PATH = os.path.join(os.path.dirname(__file__), 'data')
+MODEL_FINDER_PATH = os.path.join(MODEL_PATH, 'MF')
+
+
+
+
+
+
