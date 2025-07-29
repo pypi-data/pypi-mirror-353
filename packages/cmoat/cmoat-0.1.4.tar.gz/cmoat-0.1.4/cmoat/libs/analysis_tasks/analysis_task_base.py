2e9cb94a2851707f891bea2c1132c4e084ba55e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:50 +0000

    Add package cmoat/0.1.4/cmoat-0.1.4.tar.gz

---
+++ packages/cmoat/cmoat-0.1.4.tar.gz/cmoat-0.1.4/cmoat/libs/analysis_tasks/analysis_task_base.py
@@ -0,0 +1,53 @@
+from abc import abstractmethod, ABC
+
+class AnalysisTaskBase(ABC):
+    """
+    The base class of analysis tasks
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def preprocess(self) -> None:
+        """
+        The preprocess function
+        """
+        pass
+
+    @abstractmethod
+    def process(self) -> None:
+        """
+        The main process function
+        """
+        pass
+
+    @abstractmethod
+    def plot(self) -> None:
+        """
+        The plot function
+        """
+        pass
+
+    @property
+    def result(self):
+        """
+        The result property, use this to return the task result
+        """
+        pass
+
+    def run_task(self):
+        self.preprocess()
+        self.process()
+        self.plot()
+        return self.result
+
+class PreprocessError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+class ProcessError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
