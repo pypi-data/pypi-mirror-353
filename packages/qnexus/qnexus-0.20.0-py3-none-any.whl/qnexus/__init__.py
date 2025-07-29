e2dd57a3342e5c634c823cfcf4b45873c97521cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:09:13 +0000

    Add package qnexus/0.20.0/qnexus-0.20.0-py3-none-any.whl

---
+++ packages/qnexus/qnexus-0.20.0-py3-none-any.whl/qnexus/__init__.py
@@ -0,0 +1,80 @@
+"""The qnexus package."""
+
+import warnings
+
+import nest_asyncio  # type: ignore
+
+from qnexus import context, filesystem
+from qnexus.client import (
+    auth,
+    circuits,
+    credentials,
+    devices,
+    hugr,
+    jobs,
+    projects,
+    quotas,
+    roles,
+    teams,
+    users,
+    wasm_modules,
+)
+from qnexus.client.auth import login, login_with_credentials, logout
+from qnexus.client.jobs import compile, execute
+from qnexus.client.jobs._compile import start_compile_job
+from qnexus.client.jobs._execute import start_execute_job
+from qnexus.models import (
+    AerConfig,
+    AerStateConfig,
+    AerUnitaryConfig,
+    BackendConfig,
+    BraketConfig,
+    IBMQConfig,
+    IBMQEmulatorConfig,
+    ProjectQConfig,
+    QuantinuumConfig,
+    QulacsConfig,
+)
+
+warnings.filterwarnings("default", category=DeprecationWarning, module=__name__)
+
+# This is necessary for use in Jupyter notebooks to allow for nested asyncio loops
+try:
+    nest_asyncio.apply()
+except (RuntimeError, ValueError):
+    # May fail in some cloud environments: ignore.
+    pass
+
+__all__ = [
+    "context",
+    "roles",
+    "auth",
+    "circuits",
+    "credentials",
+    "devices",
+    "hugr",
+    "jobs",
+    "projects",
+    "quotas",
+    "teams",
+    "compile",
+    "execute",
+    "users",
+    "wasm_modules",
+    "filesystem",
+    "start_compile_job",
+    "start_execute_job",
+    "login",
+    "login_with_credentials",
+    "logout",
+    "AerConfig",
+    "AerStateConfig",
+    "AerUnitaryConfig",
+    "BackendConfig",
+    "BraketConfig",
+    "IBMQConfig",
+    "IBMQEmulatorConfig",
+    "ProjectQConfig",
+    "QuantinuumConfig",
+    "QulacsConfig",
+]
