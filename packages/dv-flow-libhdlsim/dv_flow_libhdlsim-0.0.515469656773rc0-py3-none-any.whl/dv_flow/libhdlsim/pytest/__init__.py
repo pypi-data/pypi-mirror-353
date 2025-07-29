17dc9c1b5cb4b34344033421f485aac911661647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:18 +0000

    Add package dv-flow-libhdlsim/0.0.515469656773rc0/dv_flow_libhdlsim-0.0.515469656773rc0-py3-none-any.whl

---
+++ packages/dv-flow-libhdlsim/dv_flow_libhdlsim-0.0.515469656773rc0-py3-none-any.whl/dv_flow/libhdlsim/pytest/__init__.py
@@ -0,0 +1,37 @@
+
+import os
+import dataclasses as dc
+import pytest
+import shutil
+import pytest_dfm
+
+def _available_sims():
+    sims = []
+    for sim,exe in [
+        ('mti', 'vsim'),
+        ('vcs', 'vcs'),
+        ('vlt', 'verilator'),
+        ('xcm', 'xmvlog'),
+        ('xsm', 'xvlog')]:
+        if shutil.which(exe) is not None:
+            sims.append(sim)
+
+    return sims
+
+@dc.dataclass
+class HdlSimDvFlow(pytest_dfm.DvFlow):
+    sim: str = ""
+
+    def __post_init__(self):
+        super().__post_init__()
+#        self.addOverride("hdlsim", "hdlsim.%s" % self.sim)
+
+
+@pytest.fixture(scope='function', params=_available_sims())
+def hdlsim_dvflow(request, tmpdir):
+    dvflow = HdlSimDvFlow(
+        request,
+        os.path.dirname(request.fspath),
+        tmpdir,
+        sim=request.param)
+    return dvflow
