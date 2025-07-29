108488bccc376d8149fc49641ac057d388f6dd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:25 +0000

    Add package streamlit-tree-selector/1.1.0/streamlit_tree_selector-1.1.0.tar.gz

---
+++ packages/streamlit-tree-selector/streamlit_tree_selector-1.1.0.tar.gz/streamlit_tree_selector-1.1.0/streamlit_tree_select/frontend/build/precache-manifest.e315b19f8d9c953fb3a70cb3aed3f336.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247c2494e5adf33f1d4107ed05b847c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bdd288d346817bfa96fb",
+    "url": "./static/js/2.b4d56d1d.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.b4d56d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1e21136f96ca7d2d18",
+    "url": "./static/js/main.bdf0cdac.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
