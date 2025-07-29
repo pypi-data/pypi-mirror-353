2e73ecfe394bf2afbcc614ba4814afd187aa3e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:09:33 +0000

    Add package dissect.util/3.22.dev3/dissect_util-3.22.dev3-py3-none-any.whl

---
+++ packages/dissect.util/dissect_util-3.22.dev3-py3-none-any.whl/dissect/util/__init__.py
@@ -0,0 +1,17 @@
+from dissect.util.compression import (
+    lz4,
+    lznt1,
+    lzo,
+    lzxpress,
+    lzxpress_huffman,
+    sevenbit,
+)
+
+__all__ = [
+    "lz4",
+    "lznt1",
+    "lzo",
+    "lzxpress",
+    "lzxpress_huffman",
+    "sevenbit",
+]
