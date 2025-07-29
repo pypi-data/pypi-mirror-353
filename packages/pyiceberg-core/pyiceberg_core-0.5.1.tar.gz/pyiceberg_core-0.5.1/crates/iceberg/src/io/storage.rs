eb4812e77d038ca31fe58a3f9dca4c1cad759bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:43 +0000

    Add package pyiceberg-core/0.5.1/pyiceberg_core-0.5.1.tar.gz

---
+++ packages/pyiceberg-core/pyiceberg_core-0.5.1.tar.gz/pyiceberg_core-0.5.1/crates/iceberg/src/io/storage.rs
@@ -0,0 +1,195 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::sync::Arc;
+
+use opendal::layers::RetryLayer;
+#[cfg(feature = "storage-gcs")]
+use opendal::services::GcsConfig;
+#[cfg(feature = "storage-oss")]
+use opendal::services::OssConfig;
+#[cfg(feature = "storage-s3")]
+use opendal::services::S3Config;
+use opendal::{Operator, Scheme};
+
+use super::FileIOBuilder;
+use crate::{Error, ErrorKind};
+
+/// The storage carries all supported storage services in iceberg
+#[derive(Debug)]
+pub(crate) enum Storage {
+    #[cfg(feature = "storage-memory")]
+    Memory(Operator),
+    #[cfg(feature = "storage-fs")]
+    LocalFs,
+    #[cfg(feature = "storage-s3")]
+    S3 {
+        /// s3 storage could have `s3://` and `s3a://`.
+        /// Storing the scheme string here to return the correct path.
+        scheme_str: String,
+        config: Arc<S3Config>,
+    },
+    #[cfg(feature = "storage-oss")]
+    Oss { config: Arc<OssConfig> },
+    #[cfg(feature = "storage-gcs")]
+    Gcs { config: Arc<GcsConfig> },
+}
+
+impl Storage {
+    /// Convert iceberg config to opendal config.
+    pub(crate) fn build(file_io_builder: FileIOBuilder) -> crate::Result<Self> {
+        let (scheme_str, props) = file_io_builder.into_parts();
+        let scheme = Self::parse_scheme(&scheme_str)?;
+
+        match scheme {
+            #[cfg(feature = "storage-memory")]
+            Scheme::Memory => Ok(Self::Memory(super::memory_config_build()?)),
+            #[cfg(feature = "storage-fs")]
+            Scheme::Fs => Ok(Self::LocalFs),
+            #[cfg(feature = "storage-s3")]
+            Scheme::S3 => Ok(Self::S3 {
+                scheme_str,
+                config: super::s3_config_parse(props)?.into(),
+            }),
+            #[cfg(feature = "storage-gcs")]
+            Scheme::Gcs => Ok(Self::Gcs {
+                config: super::gcs_config_parse(props)?.into(),
+            }),
+            #[cfg(feature = "storage-oss")]
+            Scheme::Oss => Ok(Self::Oss {
+                config: super::oss_config_parse(props)?.into(),
+            }),
+            // Update doc on [`FileIO`] when adding new schemes.
+            _ => Err(Error::new(
+                ErrorKind::FeatureUnsupported,
+                format!("Constructing file io from scheme: {scheme} not supported now",),
+            )),
+        }
+    }
+
+    /// Creates operator from path.
+    ///
+    /// # Arguments
+    ///
+    /// * path: It should be *absolute* path starting with scheme string used to construct [`FileIO`].
+    ///
+    /// # Returns
+    ///
+    /// The return value consists of two parts:
+    ///
+    /// * An [`opendal::Operator`] instance used to operate on file.
+    /// * Relative path to the root uri of [`opendal::Operator`].
+    pub(crate) fn create_operator<'a>(
+        &self,
+        path: &'a impl AsRef<str>,
+    ) -> crate::Result<(Operator, &'a str)> {
+        let path = path.as_ref();
+        let (operator, relative_path): (Operator, &str) = match self {
+            #[cfg(feature = "storage-memory")]
+            Storage::Memory(op) => {
+                if let Some(stripped) = path.strip_prefix("memory:/") {
+                    Ok::<_, crate::Error>((op.clone(), stripped))
+                } else {
+                    Ok::<_, crate::Error>((op.clone(), &path[1..]))
+                }
+            }
+            #[cfg(feature = "storage-fs")]
+            Storage::LocalFs => {
+                let op = super::fs_config_build()?;
+
+                if let Some(stripped) = path.strip_prefix("file:/") {
+                    Ok::<_, crate::Error>((op, stripped))
+                } else {
+                    Ok::<_, crate::Error>((op, &path[1..]))
+                }
+            }
+            #[cfg(feature = "storage-s3")]
+            Storage::S3 { scheme_str, config } => {
+                let op = super::s3_config_build(config, path)?;
+                let op_info = op.info();
+
+                // Check prefix of s3 path.
+                let prefix = format!("{}://{}/", scheme_str, op_info.name());
+                if path.starts_with(&prefix) {
+                    Ok((op, &path[prefix.len()..]))
+                } else {
+                    Err(Error::new(
+                        ErrorKind::DataInvalid,
+                        format!("Invalid s3 url: {}, should start with {}", path, prefix),
+                    ))
+                }
+            }
+
+            #[cfg(feature = "storage-gcs")]
+            Storage::Gcs { config } => {
+                let operator = super::gcs_config_build(config, path)?;
+                let prefix = format!("gs://{}/", operator.info().name());
+                if path.starts_with(&prefix) {
+                    Ok((operator, &path[prefix.len()..]))
+                } else {
+                    Err(Error::new(
+                        ErrorKind::DataInvalid,
+                        format!("Invalid gcs url: {}, should start with {}", path, prefix),
+                    ))
+                }
+            }
+            #[cfg(feature = "storage-oss")]
+            Storage::Oss { config } => {
+                let op = super::oss_config_build(config, path)?;
+
+                // Check prefix of oss path.
+                let prefix = format!("oss://{}/", op.info().name());
+                if path.starts_with(&prefix) {
+                    Ok((op, &path[prefix.len()..]))
+                } else {
+                    Err(Error::new(
+                        ErrorKind::DataInvalid,
+                        format!("Invalid oss url: {}, should start with {}", path, prefix),
+                    ))
+                }
+            }
+            #[cfg(all(
+                not(feature = "storage-s3"),
+                not(feature = "storage-fs"),
+                not(feature = "storage-gcs"),
+                not(feature = "storage-oss")
+            ))]
+            _ => Err(Error::new(
+                ErrorKind::FeatureUnsupported,
+                "No storage service has been enabled",
+            )),
+        }?;
+
+        // Transient errors are common for object stores; however there's no
+        // harm in retrying temporary failures for other storage backends as well.
+        let operator = operator.layer(RetryLayer::new());
+
+        Ok((operator, relative_path))
+    }
+
+    /// Parse scheme.
+    fn parse_scheme(scheme: &str) -> crate::Result<Scheme> {
+        match scheme {
+            "memory" => Ok(Scheme::Memory),
+            "file" | "" => Ok(Scheme::Fs),
+            "s3" | "s3a" => Ok(Scheme::S3),
+            "gs" | "gcs" => Ok(Scheme::Gcs),
+            "oss" => Ok(Scheme::Oss),
+            s => Ok(s.parse::<Scheme>()?),
+        }
+    }
+}
---
+++ packages/pyiceberg-core/pyiceberg_core-0.5.1.tar.gz/pyiceberg_core-0.5.1/crates/integrations/datafusion/tests/integration_datafusion_test.rs
@@ -0,0 +1,301 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//! Integration tests for Iceberg Datafusion with Hive Metastore.
+
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::vec;
+
+use datafusion::arrow::array::{Array, StringArray};
+use datafusion::arrow::datatypes::{DataType, Field, Schema as ArrowSchema};
+use datafusion::execution::context::SessionContext;
+use datafusion::parquet::arrow::PARQUET_FIELD_ID_META_KEY;
+use iceberg::io::FileIOBuilder;
+use iceberg::spec::{NestedField, PrimitiveType, Schema, StructType, Type};
+use iceberg::{Catalog, NamespaceIdent, Result, TableCreation};
+use iceberg_catalog_memory::MemoryCatalog;
+use iceberg_datafusion::IcebergCatalogProvider;
+use tempfile::TempDir;
+
+fn temp_path() -> String {
+    let temp_dir = TempDir::new().unwrap();
+    temp_dir.path().to_str().unwrap().to_string()
+}
+
+fn get_iceberg_catalog() -> MemoryCatalog {
+    let file_io = FileIOBuilder::new_fs_io().build().unwrap();
+    MemoryCatalog::new(file_io, Some(temp_path()))
+}
+
+fn get_struct_type() -> StructType {
+    StructType::new(vec![
+        NestedField::required(4, "s_foo1", Type::Primitive(PrimitiveType::Int)).into(),
+        NestedField::required(5, "s_foo2", Type::Primitive(PrimitiveType::String)).into(),
+    ])
+}
+
+async fn set_test_namespace(catalog: &MemoryCatalog, namespace: &NamespaceIdent) -> Result<()> {
+    let properties = HashMap::new();
+
+    catalog.create_namespace(namespace, properties).await?;
+
+    Ok(())
+}
+
+fn get_table_creation(
+    location: impl ToString,
+    name: impl ToString,
+    schema: Option<Schema>,
+) -> Result<TableCreation> {
+    let schema = match schema {
+        None => Schema::builder()
+            .with_schema_id(0)
+            .with_fields(vec![
+                NestedField::required(1, "foo1", Type::Primitive(PrimitiveType::Int)).into(),
+                NestedField::required(2, "foo2", Type::Primitive(PrimitiveType::String)).into(),
+            ])
+            .build()?,
+        Some(schema) => schema,
+    };
+
+    let creation = TableCreation::builder()
+        .location(location.to_string())
+        .name(name.to_string())
+        .properties(HashMap::new())
+        .schema(schema)
+        .build();
+
+    Ok(creation)
+}
+
+#[tokio::test]
+async fn test_provider_plan_stream_schema() -> Result<()> {
+    let iceberg_catalog = get_iceberg_catalog();
+    let namespace = NamespaceIdent::new("test_provider_get_table_schema".to_string());
+    set_test_namespace(&iceberg_catalog, &namespace).await?;
+
+    let creation = get_table_creation(temp_path(), "my_table", None)?;
+    iceberg_catalog.create_table(&namespace, creation).await?;
+
+    let client = Arc::new(iceberg_catalog);
+    let catalog = Arc::new(IcebergCatalogProvider::try_new(client).await?);
+
+    let ctx = SessionContext::new();
+    ctx.register_catalog("catalog", catalog);
+
+    let provider = ctx.catalog("catalog").unwrap();
+    let schema = provider.schema("test_provider_get_table_schema").unwrap();
+
+    let table = schema.table("my_table").await.unwrap().unwrap();
+    let table_schema = table.schema();
+
+    let expected = [("foo1", &DataType::Int32), ("foo2", &DataType::Utf8)];
+
+    for (field, exp) in table_schema.fields().iter().zip(expected.iter()) {
+        assert_eq!(field.name(), exp.0);
+        assert_eq!(field.data_type(), exp.1);
+        assert!(!field.is_nullable())
+    }
+
+    let df = ctx
+        .sql("select foo2 from catalog.test_provider_get_table_schema.my_table")
+        .await
+        .unwrap();
+
+    let task_ctx = Arc::new(df.task_ctx());
+    let plan = df.create_physical_plan().await.unwrap();
+    let stream = plan.execute(1, task_ctx).unwrap();
+
+    // Ensure both the plan and the stream conform to the same schema
+    assert_eq!(plan.schema(), stream.schema());
+    assert_eq!(
+        stream.schema().as_ref(),
+        &ArrowSchema::new(vec![
+            Field::new("foo2", DataType::Utf8, false).with_metadata(HashMap::from([(
+                PARQUET_FIELD_ID_META_KEY.to_string(),
+                "2".to_string(),
+            )]))
+        ]),
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_provider_list_table_names() -> Result<()> {
+    let iceberg_catalog = get_iceberg_catalog();
+    let namespace = NamespaceIdent::new("test_provider_list_table_names".to_string());
+    set_test_namespace(&iceberg_catalog, &namespace).await?;
+
+    let creation = get_table_creation(temp_path(), "my_table", None)?;
+    iceberg_catalog.create_table(&namespace, creation).await?;
+
+    let client = Arc::new(iceberg_catalog);
+    let catalog = Arc::new(IcebergCatalogProvider::try_new(client).await?);
+
+    let ctx = SessionContext::new();
+    ctx.register_catalog("catalog", catalog);
+
+    let provider = ctx.catalog("catalog").unwrap();
+    let schema = provider.schema("test_provider_list_table_names").unwrap();
+
+    let expected = vec!["my_table"];
+    let result = schema.table_names();
+
+    assert_eq!(result, expected);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_provider_list_schema_names() -> Result<()> {
+    let iceberg_catalog = get_iceberg_catalog();
+    let namespace = NamespaceIdent::new("test_provider_list_schema_names".to_string());
+    set_test_namespace(&iceberg_catalog, &namespace).await?;
+
+    let client = Arc::new(iceberg_catalog);
+    let catalog = Arc::new(IcebergCatalogProvider::try_new(client).await?);
+
+    let ctx = SessionContext::new();
+    ctx.register_catalog("catalog", catalog);
+
+    let provider = ctx.catalog("catalog").unwrap();
+
+    let expected = ["test_provider_list_schema_names"];
+    let result = provider.schema_names();
+
+    assert!(
+        expected
+            .iter()
+            .all(|item| result.contains(&item.to_string()))
+    );
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_table_projection() -> Result<()> {
+    let iceberg_catalog = get_iceberg_catalog();
+    let namespace = NamespaceIdent::new("ns".to_string());
+    set_test_namespace(&iceberg_catalog, &namespace).await?;
+
+    let schema = Schema::builder()
+        .with_schema_id(0)
+        .with_fields(vec![
+            NestedField::required(1, "foo1", Type::Primitive(PrimitiveType::Int)).into(),
+            NestedField::required(2, "foo2", Type::Primitive(PrimitiveType::String)).into(),
+            NestedField::optional(3, "foo3", Type::Struct(get_struct_type())).into(),
+        ])
+        .build()?;
+    let creation = get_table_creation(temp_path(), "t1", Some(schema))?;
+    iceberg_catalog.create_table(&namespace, creation).await?;
+
+    let client = Arc::new(iceberg_catalog);
+    let catalog = Arc::new(IcebergCatalogProvider::try_new(client).await?);
+
+    let ctx = SessionContext::new();
+    ctx.register_catalog("catalog", catalog);
+    let table_df = ctx.table("catalog.ns.t1").await.unwrap();
+
+    let records = table_df
+        .clone()
+        .explain(false, false)
+        .unwrap()
+        .collect()
+        .await
+        .unwrap();
+    assert_eq!(1, records.len());
+    let record = &records[0];
+    // the first column is plan_type, the second column plan string.
+    let s = record
+        .column(1)
+        .as_any()
+        .downcast_ref::<StringArray>()
+        .unwrap();
+    assert_eq!(2, s.len());
+    // the first row is logical_plan, the second row is physical_plan
+    assert!(s.value(1).contains("projection:[foo1,foo2,foo3]"));
+
+    // datafusion doesn't support query foo3.s_foo1, use foo3 instead
+    let records = table_df
+        .select_columns(&["foo1", "foo3"])
+        .unwrap()
+        .explain(false, false)
+        .unwrap()
+        .collect()
+        .await
+        .unwrap();
+    assert_eq!(1, records.len());
+    let record = &records[0];
+    let s = record
+        .column(1)
+        .as_any()
+        .downcast_ref::<StringArray>()
+        .unwrap();
+    assert_eq!(2, s.len());
+    assert!(
+        s.value(1)
+            .contains("IcebergTableScan projection:[foo1,foo3]")
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_table_predict_pushdown() -> Result<()> {
+    let iceberg_catalog = get_iceberg_catalog();
+    let namespace = NamespaceIdent::new("ns".to_string());
+    set_test_namespace(&iceberg_catalog, &namespace).await?;
+
+    let schema = Schema::builder()
+        .with_schema_id(0)
+        .with_fields(vec![
+            NestedField::required(1, "foo", Type::Primitive(PrimitiveType::Int)).into(),
+            NestedField::optional(2, "bar", Type::Primitive(PrimitiveType::String)).into(),
+        ])
+        .build()?;
+    let creation = get_table_creation(temp_path(), "t1", Some(schema))?;
+    iceberg_catalog.create_table(&namespace, creation).await?;
+
+    let client = Arc::new(iceberg_catalog);
+    let catalog = Arc::new(IcebergCatalogProvider::try_new(client).await?);
+
+    let ctx = SessionContext::new();
+    ctx.register_catalog("catalog", catalog);
+    let records = ctx
+        .sql("select * from catalog.ns.t1 where (foo > 1 and length(bar) = 1 ) or bar is null")
+        .await
+        .unwrap()
+        .explain(false, false)
+        .unwrap()
+        .collect()
+        .await
+        .unwrap();
+    assert_eq!(1, records.len());
+    let record = &records[0];
+    // the first column is plan_type, the second column plan string.
+    let s = record
+        .column(1)
+        .as_any()
+        .downcast_ref::<StringArray>()
+        .unwrap();
+    assert_eq!(2, s.len());
+    // the first row is logical_plan, the second row is physical_plan
+    let expected = "predicate:[(foo > 1) OR (bar IS NULL)]";
+    assert!(s.value(1).trim().contains(expected));
+    Ok(())
+}
