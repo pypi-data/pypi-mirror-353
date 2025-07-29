67469cd942da2536b77ca69ffd48954837767781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:37 +0000

    Add package magique-ai/0.3.0/magique_ai-0.3.0-py3-none-any.whl

---
+++ packages/magique-ai/magique_ai-0.3.0-py3-none-any.whl/magique/ai/constant.py
@@ -0,0 +1,11 @@
+import os
+
+DEFAULT_MAGIQUE_SERVER_URL = "ws://magique.spateo.aristoteleo.com/ws|ws://server.magique1.aristoteleo.com/ws|ws://server.magique2.aristoteleo.com/ws"""
+
+_SERVER_URL = os.environ.get("MAGIQUE_SERVER_URL", DEFAULT_MAGIQUE_SERVER_URL)
+
+SERVER_URLS = []
+if "\n" in _SERVER_URL:
+    SERVER_URLS = _SERVER_URL.split("|")
+else:
+    SERVER_URLS = [_SERVER_URL]
