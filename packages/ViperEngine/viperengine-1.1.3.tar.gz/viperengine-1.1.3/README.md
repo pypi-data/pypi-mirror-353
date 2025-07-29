455be41a2feda1213c2705a32c1b7bea2893f04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package ViperEngine/1.1.3/viperengine-1.1.3.tar.gz

---
+++ packages/ViperEngine/viperengine-1.1.3.tar.gz/viperengine-1.1.3/README.md
@@ -0,0 +1,3 @@
+Hi this is Viper Engine,
+
+For more info visit the docs : https://viperengine.github.io/Viper-web/
---
+++ packages/ViperEngine/viperengine-1.1.3.tar.gz/viperengine-1.1.3/ViperEngine/__init__.py
@@ -0,0 +1,16 @@
+from .constants import *
+from .Viper import (
+    init,
+    quit,
+    display_set_mode,
+    display_set_caption,
+    display_flip,
+    event_get as poll_event,
+    key_get_pressed,
+    load_image,
+    Surface,
+    Rect,
+    Clock,
+    EventType,
+    Display,
+)
