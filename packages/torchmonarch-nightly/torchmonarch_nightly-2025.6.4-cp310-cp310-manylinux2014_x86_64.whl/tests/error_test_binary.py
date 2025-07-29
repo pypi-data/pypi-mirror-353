01a138e08a48d3524fa776eefb9186b8a6788729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:51 +0000

    Add package torchmonarch-nightly/2025.6.4/torchmonarch_nightly-2025.6.4-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/torchmonarch-nightly/torchmonarch_nightly-2025.6.4-cp310-cp310-manylinux2014_x86_64.whl/tests/error_test_binary.py
@@ -0,0 +1,139 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import ctypes
+import sys
+
+from monarch._rust_bindings.monarch_extension.panic import panicking_function
+
+from monarch.actor_mesh import Actor, endpoint
+from monarch.proc_mesh import proc_mesh
+
+
+class ErrorActor(Actor):
+    """An actor that has endpoints cause segfaults."""
+
+    @endpoint
+    async def cause_segfault(self) -> None:
+        """Endpoint that causes a segmentation fault."""
+        # Create a C function pointer to an invalid memory address
+        # This will reliably cause a segmentation fault when called
+        function_type = ctypes.CFUNCTYPE(None)
+        # Use a non-zero but invalid address to avoid ctypes null pointer checks
+        invalid_address = 0xDEADBEEF
+        invalid_function = function_type(invalid_address)
+        # Calling this function will cause a segfault
+        invalid_function()
+
+    @endpoint
+    async def cause_panic(self) -> None:
+        """Endpoint that calls a Rust function that panics."""
+        panicking_function()
+
+
+class ErrorActorSync(Actor):
+    """An actor that has endpoints cause segfaults."""
+
+    @endpoint  # pyre-ignore
+    def cause_segfault(self) -> None:
+        """Endpoint that causes a segmentation fault."""
+        # Create a C function pointer to an invalid memory address
+        # This will reliably cause a segmentation fault when called
+        function_type = ctypes.CFUNCTYPE(None)
+        # Use a non-zero but invalid address to avoid ctypes null pointer checks
+        invalid_address = 0xDEADBEEF
+        invalid_function = function_type(invalid_address)
+        # Calling this function will cause a segfault
+        invalid_function()
+
+    @endpoint  # pyre-ignore
+    def cause_panic(self) -> None:
+        """Endpoint that calls a Rust function that panics."""
+        panicking_function()
+
+
+def _run_error_test_sync(num_procs, sync_endpoint, endpoint_name):
+    proc = proc_mesh(gpus=num_procs).get()
+    if sync_endpoint:
+        actor_class = ErrorActorSync
+    else:
+        actor_class = ErrorActor
+    error_actor = proc.spawn("error_actor", actor_class).get()
+
+    # This output is checked in the test to make sure that the process actually got here
+    print("I actually ran")
+    sys.stdout.flush()
+
+    if endpoint_name == "cause_segfault":
+        endpoint = error_actor.cause_segfault
+    elif endpoint_name == "cause_panic":
+        endpoint = error_actor.cause_panic
+    else:
+        raise ValueError(f"Unknown endpoint name: {endpoint_name}")
+
+    # Exercise both call() and call_one() in our tests, to check that error
+    # aggregation behavior is consistent.
+    if num_procs == 1:
+        endpoint.call_one().get()
+    else:
+        endpoint.call().get()
+
+
+def _run_error_test(num_procs, sync_endpoint, endpoint_name):
+    import asyncio
+
+    if sync_endpoint:
+        actor_class = ErrorActorSync
+    else:
+        actor_class = ErrorActor
+
+    async def run_test():
+        proc = await proc_mesh(gpus=num_procs)
+        error_actor = await proc.spawn("error_actor", actor_class)
+
+        # This output is checked in the test to make sure that the process actually got here
+        print("I actually ran")
+        sys.stdout.flush()
+
+        if endpoint_name == "cause_segfault":
+            endpoint = error_actor.cause_segfault
+        elif endpoint_name == "cause_panic":
+            endpoint = error_actor.cause_panic
+        else:
+            raise ValueError(f"Unknown endpoint name: {endpoint_name}")
+
+        # Exercise both call() and call_one() in our tests, to check that error
+        # aggregation behavior is consistent.
+        if num_procs == 1:
+            await endpoint.call_one()
+        else:
+            await endpoint.call()
+
+    asyncio.run(run_test())
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num-procs", type=int)
+    parser.add_argument("--sync-test-impl", type=bool)
+    parser.add_argument("--sync-endpoint", type=bool)
+    parser.add_argument("--endpoint-name", type=str)
+    args = parser.parse_args()
+
+    print(
+        f"Running segfault test: {args.num_procs=} {args.sync_test_impl=} {args.sync_endpoint=}, {args.endpoint_name=}"
+    )
+
+    if args.sync_test_impl:
+        _run_error_test_sync(args.num_procs, args.sync_endpoint, args.endpoint_name)
+    else:
+        _run_error_test(args.num_procs, args.sync_endpoint, args.endpoint_name)
+
+
+if __name__ == "__main__":
+    main()
