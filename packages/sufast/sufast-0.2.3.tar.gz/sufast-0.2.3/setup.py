6a01d50798c28841802bf4678db3dafd51c0d4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:09 +0000

    Add package sufast/0.2.3/sufast-0.2.3.tar.gz

---
+++ packages/sufast/sufast-0.2.3.tar.gz/sufast-0.2.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="sufast",
+    version="0.2.3",  # Bump the version!
+    author="Shohan",
+    author_email="shohan.dev.cse@gmail.com",
+    description="A blazing-fast Python web framework powered by Rust 🚀",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shohan-dev/sufast",
+    project_urls={
+        "Bug Tracker": "https://github.com/shohan-dev/sufast/issues",
+        "Documentation": "https://sufast.shohan.dev",
+    },
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,  # ✅ this is required
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        # "Framework :: sufast", # still not supported
+        "Intended Audience :: Developers",
+
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3.8",
+)
