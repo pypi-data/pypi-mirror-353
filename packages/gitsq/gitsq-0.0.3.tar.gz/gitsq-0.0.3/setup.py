5adb0354621a98da093e8cafdbd09f11989980ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:19 +0000

    Add package gitsq/0.0.3/gitsq-0.0.3.tar.gz

---
+++ packages/gitsq/gitsq-0.0.3.tar.gz/gitsq-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import os
+
+import setuptools
+
+
+with open(f"{os.path.dirname(os.path.abspath(__file__))}/requirements.txt") as requirements:
+    with open(f"{os.path.dirname(os.path.abspath(__file__))}/README.md") as readme:
+        setuptools.setup(
+            name="gitsq",
+            version="0.0.3",
+            description="Automatic squash tool for Git written in Python",
+            long_description=readme.read(),
+            long_description_content_type="text/markdown",
+            author="Vladimir Chebotarev",
+            author_email="vladimir.chebotarev@gmail.com",
+            license="MIT",
+            classifiers=[
+                "Development Status :: 5 - Production/Stable",
+                "Environment :: Console",
+                "Intended Audience :: Developers",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Programming Language :: Python :: 3 :: Only",  # FIXME
+                "Topic :: Software Development",
+                "Topic :: Terminals",
+                "Topic :: Utilities",
+            ],
+            keywords=["git", "automatic", "squash"],
+            project_urls={
+                "Documentation": "https://github.com/excitoon/gitsq/blob/master/README.md",
+                "Source": "https://github.com/excitoon/gitsq",
+                "Tracker": "https://github.com/excitoon/gitsq/issues",
+            },
+            url="https://github.com/excitoon/gitsq",
+            packages=[],
+            scripts=["gitsq", "gitsq.cmd"],
+            install_requires=requirements.read().splitlines(),
+        )
