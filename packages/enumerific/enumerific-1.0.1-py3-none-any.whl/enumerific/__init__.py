9636cfd51c58dc77ced23f48e0c38229eacb5b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:51 +0000

    Add package enumerific/1.0.1/enumerific-1.0.1-py3-none-any.whl

---
+++ packages/enumerific/enumerific-1.0.1-py3-none-any.whl/enumerific/__init__.py
@@ -0,0 +1,23 @@
+from enum import *
+
+from .logging import logger
+
+from .exceptions import EnumValueError
+
+from .extensible import (
+    Enumeration,
+    EnumerationType,
+    EnumerationInteger,
+    EnumerationString,
+    EnumerationFloat,
+    EnumerationComplex,
+    EnumerationBytes,
+    EnumerationTuple,
+    EnumerationSet,
+    EnumerationList,
+    EnumerationDictionary,
+    EnumerationFlag,
+    auto,
+)
+
+from .standard import Enum
