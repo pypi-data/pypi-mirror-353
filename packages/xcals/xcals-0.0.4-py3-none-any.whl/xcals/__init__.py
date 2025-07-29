3f90a65ac729b8e30f9ea3416d44166d136a95e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:56 +0000

    Add package xcals/0.0.4/xcals-0.0.4-py3-none-any.whl

---
+++ packages/xcals/xcals-0.0.4-py3-none-any.whl/xcals/__init__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------
+Created on 2025/5/25 20:54
+@author: ZhangYundi
+@email: yundi.xxii@outlook.com
+---------------------------------------------
+"""
+import os.path
+
+from .api import (
+    DATE_FORMAT,
+    TIME_FORMAT,
+    FILE,
+    update,
+    get_tradingdays,
+    get_tradingtime,
+    get_recent_reportdate,
+    get_recent_tradeday,
+    is_tradeday,
+    is_reportdate,
+    shift_tradeday,
+    shift_tradedt,
+    shift_tradetime,
+    shift_reportdate,
+    today,
+    now,
+)
+
+__version__ = "v0.0.4"
+
+if not os.path.exists(FILE):
+    print(f"{FILE} missing")
+    update()
+
+
+__all__ = [
+    "DATE_FORMAT",
+    "TIME_FORMAT",
+    "FILE",
+    "update",
+    "get_tradingdays",
+    "get_tradingtime",
+    "get_recent_reportdate",
+    "get_recent_tradeday",
+    "is_tradeday",
+    "is_reportdate",
+    "shift_tradeday",
+    "shift_tradetime",
+    "shift_tradedt",
+    "shift_reportdate",
+    "today",
+    "now",
+]
