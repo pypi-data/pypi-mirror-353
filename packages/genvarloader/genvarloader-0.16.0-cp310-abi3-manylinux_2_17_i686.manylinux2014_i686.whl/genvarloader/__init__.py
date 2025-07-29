0a76d8b87cf236b37e7eae718b43db40001c6737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:24 +0000

    Add package genvarloader/0.16.0/genvarloader-0.16.0-cp310-abi3-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/genvarloader/genvarloader-0.16.0-cp310-abi3-manylinux_2_17_i686.manylinux2014_i686.whl/genvarloader/__init__.py
@@ -0,0 +1,41 @@
+import importlib.metadata
+
+from seqpro._ragged import Ragged
+from seqpro.bed import read as read_bedlike
+from seqpro.bed import with_len as with_length
+
+from . import data_registry
+from ._bigwig import BigWigs
+from ._dataset._impl import ArrayDataset, Dataset, RaggedDataset
+from ._dataset._rag_variants import RaggedVariants
+from ._dataset._reference import RefDataset, Reference
+from ._dataset._write import write
+from ._dummy import get_dummy_dataset
+from ._ragged import RaggedAnnotatedHaps
+from ._torch import to_nested_tensor
+from ._types import AnnotatedHaps
+from ._variants._sitesonly import DatasetWithSites, SitesSchema, sites_vcf_to_table
+
+__version__ = importlib.metadata.version("genvarloader")
+
+__all__ = [
+    "write",
+    "Dataset",
+    "BigWigs",
+    "read_bedlike",
+    "with_length",
+    "Ragged",
+    "get_dummy_dataset",
+    "ArrayDataset",
+    "RaggedDataset",
+    "Reference",
+    "to_nested_tensor",
+    "sites_vcf_to_table",
+    "SitesSchema",
+    "DatasetWithSites",
+    "RefDataset",
+    "data_registry",
+    "AnnotatedHaps",
+    "RaggedAnnotatedHaps",
+    "RaggedVariants",
+]
