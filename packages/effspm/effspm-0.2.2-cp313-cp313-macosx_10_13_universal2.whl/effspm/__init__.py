d13eb3a30ee1fd4df1c997fc1042d6ca58806226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:14 +0000

    Add package effspm/0.2.2/effspm-0.2.2-cp313-cp313-macosx_10_13_universal2.whl

---
+++ packages/effspm/effspm-0.2.2-cp313-cp313-macosx_10_13_universal2.whl/effspm/__init__.py
@@ -0,0 +1,3 @@
+# effspm/__init__.py
+from ._effspm import PrefixProjection, LargePrefixProjection, BTMiner ,LargeBTMiner,LargeHTMiner,HTMiner
+__all__ = ["PrefixProjection", "LargePrefixProjection", "BTMiner","LargeBTMiner","LargeHTMiner","HTMiner"]
