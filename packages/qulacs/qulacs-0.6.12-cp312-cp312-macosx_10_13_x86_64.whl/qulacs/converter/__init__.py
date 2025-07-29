48b47ed783326b2dc01e9a29a08c5b4ae1997f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:06 +0000

    Add package qulacs/0.6.12/qulacs-0.6.12-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/qulacs/qulacs-0.6.12-cp312-cp312-macosx_10_13_x86_64.whl/qulacs/converter/__init__.py
@@ -0,0 +1,6 @@
+from qulacs.converter.qasm_converter import (
+    convert_QASM_to_qulacs_circuit,
+    convert_qulacs_circuit_to_QASM,
+)
+
+__all__ = ["convert_QASM_to_qulacs_circuit", "convert_qulacs_circuit_to_QASM"]
