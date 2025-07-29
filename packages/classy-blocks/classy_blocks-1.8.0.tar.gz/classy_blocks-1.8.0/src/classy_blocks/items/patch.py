41b805a25c3f69d1e8794477115107150df81958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:23 +0000

    Add package classy-blocks/1.8.0/classy_blocks-1.8.0.tar.gz

---
+++ packages/classy-blocks/classy_blocks-1.8.0.tar.gz/classy_blocks-1.8.0/src/classy_blocks/items/patch.py
@@ -0,0 +1,26 @@
+import warnings
+from typing import List, Set
+
+from classy_blocks.items.side import Side
+
+
+class Patch:
+    """Definition of a patch, including type, belonging faces and other settings"""
+
+    def __init__(self, name: str):
+        self.name = name
+
+        self.sides: Set[Side] = set()
+
+        self.kind = "patch"  # 'type'
+        self.settings: List[str] = []
+
+        self.slave = False
+
+    def add_side(self, side: Side) -> None:
+        """Adds a side to the list if it doesn't exist yet"""
+        if side in self.sides:
+            warnings.warn(f"Side {side} has already been assigned to {self.name}", stacklevel=2)
+            return
+
+        self.sides.add(side)
