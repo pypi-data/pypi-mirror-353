d2485a8f613bc527d186c9b489a04952f81efa32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:04 +0000

    Add package statsig/0.58.1/statsig-0.58.1.tar.gz

---
+++ packages/statsig/statsig-0.58.1.tar.gz/statsig-0.58.1/statsig/globals.py
@@ -0,0 +1,23 @@
+import os
+
+from .statsig_options import StatsigOptions
+from .statsig_telemetry_logger import StatsigTelemetryLogger
+
+STATSIG_BATCHING_INTERVAL_SECONDS = 60.0
+STATSIG_LOGGING_INTERVAL_SECONDS = 1.0
+
+logger = StatsigTelemetryLogger()
+
+os.environ["GRPC_VERBOSITY"] = "NONE"
+
+
+def init_logger(options: StatsigOptions):
+    if options.custom_logger is not None:
+        logger.set_logger(options.custom_logger)
+    elif options.output_logger_level is not None:
+        logger.set_log_level(options.output_logger_level)
+    if options.observability_client is not None:
+        logger.set_ob_client(options.observability_client)
+    if options.sdk_error_callback is not None:
+        logger.set_sdk_error_callback(options.sdk_error_callback)
+    logger.init()
