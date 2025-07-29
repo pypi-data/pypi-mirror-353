0d9740db9f9ce27dd96d1815a20e48525a922233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:33 +0000

    Add package libera_utils/3.2.1rc2/libera_utils-3.2.1rc2.tar.gz

---
+++ packages/libera_utils/libera_utils-3.2.1rc2.tar.gz/libera_utils-3.2.1rc2/libera_utils/version.py
@@ -0,0 +1,7 @@
+"""Module for anything related to package versioning"""
+from importlib import metadata
+
+
+def version():
+    """Get package version from metadata"""
+    return metadata.version('libera_utils')
