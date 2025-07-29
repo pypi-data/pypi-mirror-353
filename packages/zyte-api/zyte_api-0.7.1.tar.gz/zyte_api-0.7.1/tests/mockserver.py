0024e2b9097db04d3a2e1dccaa8cd68e4d8017e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:20 +0000

    Add package zyte-api/0.7.1/zyte_api-0.7.1.tar.gz

---
+++ packages/zyte-api/zyte_api-0.7.1.tar.gz/zyte_api-0.7.1/tests/mockserver.py
@@ -0,0 +1,194 @@
+import argparse
+import json
+import socket
+import sys
+import time
+from base64 import b64encode
+from importlib import import_module
+from subprocess import PIPE, Popen
+from typing import Any
+from urllib.parse import urlparse
+
+from twisted.internet import reactor
+from twisted.internet.task import deferLater
+from twisted.web.resource import Resource
+from twisted.web.server import NOT_DONE_YET, Site
+
+
+# https://github.com/scrapy/scrapy/blob/02b97f98e74a994ad3e4d74e7ed55207e508a576/tests/mockserver.py#L27C1-L33C19
+def getarg(request, name, default=None, type=None):
+    if name in request.args:
+        value = request.args[name][0]
+        if type is not None:
+            value = type(value)
+        return value
+    return default
+
+
+def get_ephemeral_port():
+    s = socket.socket()
+    s.bind(("", 0))
+    return s.getsockname()[1]
+
+
+class DropResource(Resource):
+    isLeaf = True
+
+    def deferRequest(self, request, delay, f, *a, **kw):
+        def _cancelrequest(_):
+            # silence CancelledError
+            d.addErrback(lambda _: None)
+            d.cancel()
+
+        d = deferLater(reactor, delay, f, *a, **kw)
+        request.notifyFinish().addErrback(_cancelrequest)
+        return d
+
+    def render_POST(self, request):
+        request.setHeader(b"Content-Length", b"1024")
+        self.deferRequest(request, 0, self._delayedRender, request)
+        return NOT_DONE_YET
+
+    def _delayedRender(self, request):
+        abort = getarg(request, b"abort", 0, type=int)
+        request.write(b"this connection will be dropped\n")
+        tr = request.channel.transport
+        try:
+            if abort and hasattr(tr, "abortConnection"):
+                tr.abortConnection()
+            else:
+                tr.loseConnection()
+        finally:
+            request.finish()
+
+
+class DefaultResource(Resource):
+    request_count = 0
+
+    def getChild(self, path, request):
+        return self
+
+    def render_POST(self, request):
+        request_data = json.loads(request.content.read())
+
+        request.responseHeaders.setRawHeaders(
+            b"Content-Type",
+            [b"application/json"],
+        )
+        request.responseHeaders.setRawHeaders(
+            b"request-id",
+            [b"abcd1234"],
+        )
+
+        url = request_data["url"]
+        domain = urlparse(url).netloc
+        if domain == "e429.example":
+            request.setResponseCode(429)
+            response_data = {"status": 429, "type": "/limits/over-user-limit"}
+            return json.dumps(response_data).encode()
+        if domain == "e500.example":
+            request.setResponseCode(500)
+            return ""
+        if domain == "e520.example":
+            request.setResponseCode(520)
+            response_data = {"status": 520, "type": "/download/temporary-error"}
+            return json.dumps(response_data).encode()
+        if domain == "e521.example":
+            request.setResponseCode(521)
+            response_data = {"status": 521, "type": "/download/internal-error"}
+            return json.dumps(response_data).encode()
+        if domain == "exception.example":
+            request.setResponseCode(401)
+            response_data = {
+                "status": 401,
+                "type": "/auth/key-not-found",
+                "title": "Authentication Key Not Found",
+                "detail": "The authentication key is not valid or can't be matched.",
+            }
+            return json.dumps(response_data).encode()
+        if domain == "empty-body-exception.example":
+            request.setResponseCode(500)
+            return b""
+        if domain == "nonjson.example":
+            request.setResponseCode(200)
+            return b"foo"
+        if domain == "nonjson-exception.example":
+            request.setResponseCode(500)
+            return b"foo"
+        if domain == "array-exception.example":
+            request.setResponseCode(500)
+            return b'["foo"]'
+
+        response_data: dict[str, Any] = {
+            "url": url,
+        }
+
+        html = "<html><body>Hello<h1>World!</h1></body></html>"
+        if "httpResponseBody" in request_data:
+            body = b64encode(html.encode()).decode()
+            response_data["httpResponseBody"] = body
+        else:
+            assert "browserHtml" in request_data
+            response_data["browserHtml"] = html
+
+        return json.dumps(response_data).encode()
+
+
+class MockServer:
+    def __init__(self, resource=None, port=None):
+        resource = resource or DefaultResource
+        self.resource = f"{resource.__module__}.{resource.__name__}"
+        self.proc = None
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.port = port or get_ephemeral_port()
+        self.root_url = f"http://{self.host}:{self.port}"
+
+    def __enter__(self):
+        self.proc = Popen(
+            [
+                sys.executable,
+                "-u",
+                "-m",
+                "tests.mockserver",
+                self.resource,
+                "--port",
+                str(self.port),
+            ],
+            stdout=PIPE,
+        )
+        assert self.proc.stdout is not None
+        self.proc.stdout.readline()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        assert self.proc is not None
+        self.proc.kill()
+        self.proc.wait()
+        time.sleep(0.2)
+
+    def urljoin(self, path):
+        return self.root_url + path
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("resource")
+    parser.add_argument("--port", type=int)
+    args = parser.parse_args()
+    module_name, name = args.resource.rsplit(".", 1)
+    sys.path.append(".")
+    resource = getattr(import_module(module_name), name)()
+    # Typing issue: https://github.com/twisted/twisted/issues/9909
+    http_port = reactor.listenTCP(args.port, Site(resource))  # type: ignore[attr-defined]
+
+    def print_listening():
+        host = http_port.getHost()
+        print(f"Mock server {resource} running at http://{host.host}:{host.port}")
+
+    # Typing issue: https://github.com/twisted/twisted/issues/9909
+    reactor.callWhenRunning(print_listening)  # type: ignore[attr-defined]
+    reactor.run()  # type: ignore[attr-defined]
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/zyte-api/zyte_api-0.7.1.tar.gz/zyte_api-0.7.1/zyte_api/_async.py
@@ -0,0 +1,222 @@
+from __future__ import annotations
+
+import asyncio
+import time
+from asyncio import Future
+from functools import partial
+from typing import TYPE_CHECKING, Any
+
+import aiohttp
+from tenacity import AsyncRetrying
+
+from ._errors import RequestError
+from ._retry import zyte_api_retrying
+from ._utils import _AIO_API_TIMEOUT, create_session
+from .apikey import get_apikey
+from .constants import API_URL
+from .stats import AggStats, ResponseStats
+from .utils import USER_AGENT, _process_query
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    _ResponseFuture = Future[dict[str, Any]]
+
+
+def _post_func(session):
+    """Return a function to send a POST request"""
+    if session is None:
+        return partial(aiohttp.request, method="POST", timeout=_AIO_API_TIMEOUT)
+    return session.post
+
+
+class _AsyncSession:
+    def __init__(self, client, **session_kwargs):
+        self._client = client
+        self._session = create_session(client.n_conn, **session_kwargs)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *exc_info):
+        await self._session.close()
+
+    async def close(self):
+        await self._session.close()
+
+    async def get(
+        self,
+        query: dict,
+        *,
+        endpoint: str = "extract",
+        handle_retries=True,
+        retrying: AsyncRetrying | None = None,
+    ):
+        return await self._client.get(
+            query=query,
+            endpoint=endpoint,
+            handle_retries=handle_retries,
+            retrying=retrying,
+            session=self._session,
+        )
+
+    def iter(
+        self,
+        queries: list[dict],
+        *,
+        endpoint: str = "extract",
+        handle_retries=True,
+        retrying: AsyncRetrying | None = None,
+    ) -> Iterator[Future]:
+        return self._client.iter(
+            queries=queries,
+            endpoint=endpoint,
+            session=self._session,
+            handle_retries=handle_retries,
+            retrying=retrying,
+        )
+
+
+class AsyncZyteAPI:
+    """:ref:`Asynchronous Zyte API client <asyncio_api>`.
+
+    Parameters work the same as for :class:`ZyteAPI`.
+    """
+
+    def __init__(
+        self,
+        *,
+        api_key=None,
+        api_url=API_URL,
+        n_conn=15,
+        retrying: AsyncRetrying | None = None,
+        user_agent: str | None = None,
+    ):
+        if retrying is not None and not isinstance(retrying, AsyncRetrying):
+            raise ValueError(
+                "The retrying parameter, if defined, must be an instance of "
+                "AsyncRetrying."
+            )
+        self.api_key = get_apikey(api_key)
+        self.api_url = api_url
+        self.n_conn = n_conn
+        self.agg_stats = AggStats()
+        self.retrying = retrying or zyte_api_retrying
+        self.user_agent = user_agent or USER_AGENT
+        self._semaphore = asyncio.Semaphore(n_conn)
+
+    async def get(
+        self,
+        query: dict,
+        *,
+        endpoint: str = "extract",
+        session=None,
+        handle_retries=True,
+        retrying: AsyncRetrying | None = None,
+    ) -> _ResponseFuture:
+        """Asynchronous equivalent to :meth:`ZyteAPI.get`."""
+        retrying = retrying or self.retrying
+        post = _post_func(session)
+        auth = aiohttp.BasicAuth(self.api_key)
+        headers = {"User-Agent": self.user_agent, "Accept-Encoding": "br"}
+
+        response_stats = []
+        start_global = time.perf_counter()
+
+        async def request():
+            stats = ResponseStats.create(start_global)
+            self.agg_stats.n_attempts += 1
+
+            safe_query = _process_query(query)
+            post_kwargs = {
+                "url": self.api_url + endpoint,
+                "json": safe_query,
+                "auth": auth,
+                "headers": headers,
+            }
+
+            try:
+                async with self._semaphore, post(**post_kwargs) as resp:
+                    stats.record_connected(resp.status, self.agg_stats)
+                    if resp.status >= 400:
+                        content = await resp.read()
+                        resp.release()
+                        stats.record_read()
+                        stats.record_request_error(content, self.agg_stats)
+
+                        raise RequestError(
+                            request_info=resp.request_info,
+                            history=resp.history,
+                            status=resp.status,
+                            message=resp.reason,
+                            headers=resp.headers,
+                            response_content=content,
+                            query=safe_query,
+                        )
+
+                    response = await resp.json()
+                    stats.record_read(self.agg_stats)
+                    return response
+            except Exception as e:
+                if not isinstance(e, RequestError):
+                    self.agg_stats.n_errors += 1
+                    stats.record_exception(e, agg_stats=self.agg_stats)
+                raise
+            finally:
+                response_stats.append(stats)
+
+        if handle_retries:
+            request = retrying.wraps(request)
+
+        try:
+            # Try to make a request
+            result = await request()
+            self.agg_stats.n_success += 1
+        except Exception:
+            self.agg_stats.n_fatal_errors += 1
+            raise
+
+        return result
+
+    def iter(
+        self,
+        queries: list[dict],
+        *,
+        endpoint: str = "extract",
+        session: aiohttp.ClientSession | None = None,
+        handle_retries=True,
+        retrying: AsyncRetrying | None = None,
+    ) -> Iterator[_ResponseFuture]:
+        """Asynchronous equivalent to :meth:`ZyteAPI.iter`.
+
+        .. note:: Yielded futures, when awaited, do raise their exceptions,
+                  instead of only returning them.
+        """
+
+        def _request(query):
+            return self.get(
+                query,
+                endpoint=endpoint,
+                session=session,
+                handle_retries=handle_retries,
+                retrying=retrying,
+            )
+
+        return asyncio.as_completed([_request(query) for query in queries])
+
+    def session(self, **kwargs):
+        """Asynchronous equivalent to :meth:`ZyteAPI.session`.
+
+        You do not need to use :meth:`~AsyncZyteAPI.session` as an async
+        context manager as long as you await ``close()`` on the object it
+        returns when you are done:
+
+        .. code-block:: python
+
+            session = client.session()
+            try:
+                ...
+            finally:
+                await session.close()
+        """
+        return _AsyncSession(client=self, **kwargs)
