fbd46a7a78bcbc635b58b9cd45570323396acda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:53 +0000

    Add package alonso/0.0.1/alonso-0.0.1.tar.gz

---
+++ packages/alonso/alonso-0.0.1.tar.gz/alonso-0.0.1/alonso/__init__.py
@@ -0,0 +1,4 @@
+# Alonso: Approximate Vertex Cover Solver https://pypi.org/project/alonso
+# Author: Frank Vega
+
+__all__ = ["utils", "bipartite", "algorithm", "parser", "applogger", "test", "app", "batch"]
