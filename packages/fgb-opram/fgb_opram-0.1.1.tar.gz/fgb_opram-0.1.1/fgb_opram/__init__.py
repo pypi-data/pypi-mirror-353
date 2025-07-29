7aa1181a2a5746c3693e0424303d2cdaa765ab96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:56 +0000

    Add package fgb-opram/0.1.1/fgb_opram-0.1.1.tar.gz

---
+++ packages/fgb-opram/fgb_opram-0.1.1.tar.gz/fgb_opram-0.1.1/fgb_opram/__init__.py
@@ -0,0 +1,15 @@
+""" 
+FGB-OPRAm Package: Fuzzy Granular-Ball and OPRAm Spatial Reasoning Modules
+Version: 0.1.0
+Author: Bongjae Kwon <bongjae.kwon@snu.ac.kr>
+"""
+
+__version__ = "0.1.0"
+
+from .granular_ball import GranularBall
+from .fuzzy_overlap import fuzzy_overlap, fuzzy_intersection_and_union
+from .rcc8 import compute_fuzzy_rcc8
+from .opram import opram_direction, opram_converse, opram_composition
+from .spatial_query_engine import SpatialQueryEngine
+from .visualize import visualize_fuzzy_balls
+from .gui import run_spatial_example, main  # GUI entry points
