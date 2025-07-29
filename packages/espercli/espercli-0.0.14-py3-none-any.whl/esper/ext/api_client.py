2d9fe0e4804786fd02743fb8159195193e0f88fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package espercli/0.0.14/espercli-0.0.14-py3-none-any.whl

---
+++ packages/espercli/espercli-0.0.14-py3-none-any.whl/esper/ext/api_client.py
@@ -0,0 +1,41 @@
+import esperclient as client
+from esperclient.configuration import Configuration
+import requests
+
+
+class APIClient:
+    def __init__(self, credential):
+        self.config = Configuration()
+        self.config.api_key['Authorization'] = credential["api_key"]
+        self.config.api_key_prefix['Authorization'] = 'Bearer'
+        self.config.host = f"https://{credential['environment']}-api.esper.cloud/api"
+
+    def get_enterprise_api_client(self):
+        return client.EnterpriseApi(client.ApiClient(self.config))
+
+    def get_device_api_client(self):
+        return client.DeviceApi(client.ApiClient(self.config))
+
+    def get_application_api_client(self):
+        return client.ApplicationApi(client.ApiClient(self.config))
+
+    def get_command_api_client(self):
+        return client.CommandsApi(client.ApiClient(self.config))
+
+    def get_group_api_client(self):
+        return client.DeviceGroupApi(client.ApiClient(self.config))
+
+    def get_group_command_api_client(self):
+        return client.GroupCommandsApi(client.ApiClient(self.config))
+
+    def get_remoteadb_api_client(self):
+        return client.DeviceApi(client.ApiClient(self.config))
+
+    def get_token_api_client(self):
+        return client.TokenApi(client.ApiClient(self.config))
+
+    def get_content_api_client(self):
+        return client.ContentApi(client.ApiClient(self.config))
+        
+    def get_commandsV2_api_client(self):
+        return client.CommandsV2Api(client.ApiClient(self.config))
