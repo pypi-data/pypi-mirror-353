c5264bb7711a7b220ecdbcd19a7c75e1c6598151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:22 +0000

    Add package zenodo-api/0.2.2/zenodo_api-0.2.2-py3-none-any.whl

---
+++ packages/zenodo-api/zenodo_api-0.2.2-py3-none-any.whl/zenodo_api/__init__.py
@@ -0,0 +1,7 @@
+"""A template Python module"""
+
+__version__ = "0.2.2"
+from .upload_files import *  # noqa
+from .url_selector import *  # noqa
+from .retrieve import *  # noqa
+from .cli import cli  # noqa
