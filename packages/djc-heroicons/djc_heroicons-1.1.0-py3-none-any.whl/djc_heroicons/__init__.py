0bf1bfafd221db2fb57b1294e2c6df83b3008d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:01 +0000

    Add package djc-heroicons/1.1.0/djc_heroicons-1.1.0-py3-none-any.whl

---
+++ packages/djc-heroicons/djc_heroicons-1.1.0-py3-none-any.whl/djc_heroicons/__init__.py
@@ -0,0 +1,15 @@
+# Public API
+# isort: off
+from djc_heroicons.app_settings import HeroIconsSettings
+from djc_heroicons.components.icon import Icon
+from djc_heroicons.icons import IconName, VariantName
+
+# isort: on
+
+
+__all__ = [
+    "HeroIconsSettings",
+    "Icon",
+    "IconName",
+    "VariantName",
+]
