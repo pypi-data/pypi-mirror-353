6cbed132bd68d4d72ba163b51785697069903d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:07:32 +0000

    Add package pyakta/0.5.0/pyakta-0.5.0-py3-none-any.whl

---
+++ packages/pyakta/pyakta-0.5.0-py3-none-any.whl/pyakta/__init__.py
@@ -0,0 +1,8 @@
+from .credentials import VerifiableCredential, ProofModel
+from .models import VerifiableCredentialModel
+
+__all__ = [
+    "VerifiableCredential",
+    "ProofModel",
+    "VerifiableCredentialModel",
+]
