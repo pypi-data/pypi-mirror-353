e296e79bb7d50352bb0df73a1f69e4100d9aaa21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:05 +0000

    Add package yandex-ris/0.1.2/yandex_ris-0.1.2.tar.gz

---
+++ packages/yandex-ris/yandex_ris-0.1.2.tar.gz/yandex_ris-0.1.2/yandex_ris/__init__.py
@@ -0,0 +1,8 @@
+"""
+Yandex Image Crawler - A professional image crawling tool using Yandex's image search.
+"""
+
+from .crawler import YandexImageCrawler
+from .version import __version__
+
+__all__ = ['YandexImageCrawler', '__version__'] 
