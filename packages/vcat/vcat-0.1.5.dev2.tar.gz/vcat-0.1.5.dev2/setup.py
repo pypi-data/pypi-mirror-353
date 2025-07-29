0580414e6042444a881a93710bc81e6c9bb48161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:08:11 +0000

    Add package vcat/0.1.5.dev2/vcat-0.1.5.dev2.tar.gz

---
+++ packages/vcat/vcat-0.1.5.dev2.tar.gz/vcat-0.1.5.dev2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="vcat",
+    version="0.1.5-dev2",
+    description="A CLI tool to generates human friendly visualizations for files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Alex Xi",
+    url="https://github.com/alexxi19/vcat",  
+    packages=find_packages(),
+    install_requires=[
+        "openai",  # Required dependency
+    ],
+    entry_points={
+        "console_scripts": [
+            "vcat=vcat.cli:main",  
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  
+)
