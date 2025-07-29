553466b7c1d5465afcb0cbc4cd371d844890bcbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:32 +0000

    Add package ramifice/0.3.6/ramifice-0.3.6-py3-none-any.whl

---
+++ packages/ramifice/ramifice-0.3.6-py3-none-any.whl/ramifice/fields/choice_text_mult_dyn_field.py
@@ -0,0 +1,63 @@
+"""Field of Model.
+Type of selective text field with dynamic addition of elements.
+"""
+
+from ..mixins import JsonMixin
+from .general.choice_group import ChoiceGroup
+from .general.field import Field
+
+
+class ChoiceTextMultDynField(Field, ChoiceGroup, JsonMixin):
+    """Field of Model.
+    Type of selective text field with dynamic addition of elements.
+    For simulate relationship Many-to-Many.
+    How to use, see <a href="https://github.com/kebasyaty/ramifice/tree/main/examples/dynamic_choices" target="_blank">example</a>.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        label: str = "",
+        disabled: bool = False,
+        hide: bool = False,
+        ignored: bool = False,
+        hint: str = "",
+        warning: list[str] | None = None,
+        required: bool = False,
+        readonly: bool = False,
+    ):
+
+        Field.__init__(
+            self,
+            label=label,
+            disabled=disabled,
+            hide=hide,
+            ignored=ignored,
+            hint=hint,
+            warning=warning,
+            field_type="ChoiceTextMultDynField",
+            group="choice",
+        )
+        ChoiceGroup.__init__(
+            self,
+            required=required,
+            readonly=readonly,
+            multiple=True,
+        )
+        JsonMixin.__init__(self)
+
+        self.value: list[str] | None = None
+        self.choices: dict[str, str] | None = None
+
+    def has_value(self) -> bool:
+        """Does the field value match the possible options in choices."""
+        value = self.value
+        if value is not None:
+            choices = self.choices
+            if len(value) == 0 or not bool(choices):
+                return False
+            value_list = choices.values()  # type: ignore[union-attr]
+            for item in value:
+                if item not in value_list:
+                    return False
+        return True
