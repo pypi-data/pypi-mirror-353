203dedf475c452f2edb43e2f88b654bbb6bc969d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:40 +0000

    Add package ability-hand/0.1.4/ability_hand-0.1.4.tar.gz

---
+++ packages/ability-hand/ability_hand-0.1.4.tar.gz/ability_hand-0.1.4/ah_examples/plot_motors.py
@@ -0,0 +1,41 @@
+import time
+import threading
+from math import pi, sin
+
+from ah_wrapper.ah_serial_client import AHSerialClient
+from ah_plotting.plots import RealTimePlotMotors
+
+RUNNING = True
+
+
+def hand_wave_thread(hand_client):
+    try:
+        pos = [30, 30, 30, 30, 30, -30]
+        while RUNNING:
+            current_time = time.time()
+            for i in range(0, len(pos)):
+                ft = current_time * 3 + i * (2 * pi) / 12
+                pos[i] = (0.5 * sin(ft) + 0.5) * 45 + 15
+            pos[5] = -pos[5]
+            hand_client.set_position(positions=pos, reply_mode=2)
+            hand_client.send_command()
+            time.sleep(1 / hand_client.rate_hz)
+    except KeyboardInterrupt:
+        pass
+
+
+def main():
+    client = AHSerialClient(write_thread=False)
+    write_thread = threading.Thread(target=hand_wave_thread, args=(client,))
+    write_thread.start()
+    plotter = RealTimePlotMotors(client.hand)
+    try:
+        plotter.start()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.close()
+
+
+if __name__ == "__main__":
+    main()
