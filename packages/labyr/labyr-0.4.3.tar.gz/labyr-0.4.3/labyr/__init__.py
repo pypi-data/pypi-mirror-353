e35d37fe2be68a5f312ca355594f6026680cf381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:39 +0000

    Add package labyr/0.4.3/labyr-0.4.3.tar.gz

---
+++ packages/labyr/labyr-0.4.3.tar.gz/labyr-0.4.3/labyr/__init__.py
@@ -0,0 +1,54 @@
+from typing import Any, Callable, Dict, List, Tuple
+
+from .utils import color
+
+__all__ = ["labyr", "LabyrGame"]
+
+
+def genlvls(
+    chars: Dict[str, Tuple[str, Callable]],
+    lvlSizes: Dict[int, Tuple[int, int]],
+    *,
+    c: color.C,
+):
+    out = {}
+    SPACE_DEFAULT = (".", c.dark_gray)
+    WALL_DEFAULT = ("#", c.dark_gray)
+    PLAYER_DEFAULT = ("@", c.blue)
+    EXIT_DEFAULT = ("E", c.green)
+    MONSTER_DEFAULT = ("M", c.red)
+
+    def init(dimen: Tuple[int, int]) -> List[List[str]]:
+        space = chars.get("space", SPACE_DEFAULT)[0]
+        x, y = dimen
+        grid = [[space] * x for _ in range(y)]
+
+        for i in range(x):
+            for j in range(y):
+                if i == 0 or i == x - 1 or j == 0 or j == y - 1:
+                    grid[j][i] = "#"
+
+        return grid
+
+    for lvl, dimen in lvlSizes.items():
+        out[lvl] = init(dimen)
+
+    print(out)
+
+
+class LabyrGame:
+    def __init__(self) -> None:
+        c = color.C()
+        self.clvl = 0
+        self.chars = {
+            "space": (".", c.dark_gray),
+            "wall": ("#", c.dark_gray),
+            "player": ("@", c.blue),
+            "exit": ("E", c.green),
+            "monster": ("M", c.red),
+        }
+        lvlSizes = {0: (5, 3)}
+        levels = genlvls(self.chars, lvlSizes, c=c)
+
+
+labyr = LabyrGame()
