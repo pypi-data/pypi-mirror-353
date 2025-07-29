b322a5a53e8cb530062735b37e10653e29f568fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:53 +0000

    Add package devsecops-engine-tools/1.63.1/devsecops_engine_tools-1.63.1.tar.gz

---
+++ packages/devsecops-engine-tools/devsecops_engine_tools-1.63.1.tar.gz/devsecops_engine_tools-1.63.1/devsecops_engine_tools/engine_sca/engine_dependencies/src/domain/model/gateways/tool_gateway.py
@@ -0,0 +1,9 @@
+from abc import ABCMeta, abstractmethod
+
+
+class ToolGateway(metaclass=ABCMeta):
+    @abstractmethod
+    def run_tool_dependencies_sca(
+        self, remote_config, dict_args,to_scan, secret_tool, token_engine_dependencies,**kwargs
+    ) -> str:
+        "run tool dependencies sca"
