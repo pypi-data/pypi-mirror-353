a840517bf0106d373608cc80f4160ed5bc59486c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:52 +0000

    Add package dqtool-sayhello/1.1.1/dqtool_sayhello-1.1.1.tar.gz

---
+++ packages/dqtool-sayhello/dqtool_sayhello-1.1.1.tar.gz/dqtool_sayhello-1.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dqtool-sayhello",
+    version="1.1.1",
+    packages=find_packages(),
+    description="A simple hello world package",
+    author="shubham",
+    author_email="your@email.com",
+    url="https://github.com/yourname/dqtool-sayhello",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
