790b7fd753eeeba89009e1305aacddb1ee3867c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:00 +0000

    Add package minwei-tools/0.1.1/minwei_tools-0.1.1.tar.gz

---
+++ packages/minwei-tools/minwei_tools-0.1.1.tar.gz/minwei_tools-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minwei_tools',
+    version='0.1.1',
+    author='OUYANGMINWEI',
+    author_email='wesley91345@gmail.com',
+    description='Some useful tools for Python development',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/OuYangMinOa/minwei_tools',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
