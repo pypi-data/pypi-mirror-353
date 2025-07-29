9eca23c4c59eec156f21c322ca6773ccf15c45ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:37 +0000

    Add package pricepy/0.2.1/pricepy-0.2.1-py3-none-any.whl

---
+++ packages/pricepy/pricepy-0.2.1-py3-none-any.whl/pricepy/__init__.py
@@ -0,0 +1,105 @@
+# src/pricepy/__init__.py
+
+__version__ = "0.2.1"
+
+from .pricepy import *
+
+# from .pricepy import (
+#     # config
+#     ALLOW_INFINITY,
+#     WARN_INFINITY,
+#     DATA_PATH,
+#     # warnings & errors
+#     _WARN_INFINITY,
+#     _ERROR_INFINITY,
+#     # data structures
+#     Candle,
+#     OHLC,
+#     # data manipulation
+#     dropsd,
+#     dropif,
+#     downsample,
+#     setLen,
+#     # algebra
+#     inter,
+#     # statistics
+#     pdf,
+#     sd,
+#     mean,
+#     corr,
+#     sma,
+#     # price trend analysis
+#     logReturns,
+#     ariReturns,
+#     logReturn,
+#     ariReturn,
+#     cumReturns,
+#     totReturn,
+#     # groups
+#     grBounds,
+#     grGet,
+#     grCount,
+#     grFreq,
+#     grProb,
+#     # tools
+#     rolWin,
+#     # visualisation
+#     Display,
+#     line,
+#     scat,
+#     hist,
+#     histline,
+#     bar,
+#     Distribution,
+#     multiplot,
+# )
+# 
+# __all__ = [
+#     # config
+#     "ALLOW_INFINITY",
+#     "WARN_INFINITY",
+#     "DATA_PATH",
+#     # warnings & errors
+#     "_WARN_INFINITY",
+#     "_ERROR_INFINITY",
+#     # data structures
+#     "Candle",
+#     "OHLC",
+#     # data manipulation
+#     "dropsd",
+#     "dropif",
+#     "downsample",
+#     "setLen",
+#     # algebra
+#     "inter",
+#     # statistics
+#     "pdf",
+#     "sd",
+#     "mean",
+#     "corr",
+#     "sma",
+#     # price trend analysis
+#     "logReturns",
+#     "ariReturns",
+#     "logReturn",
+#     "ariReturn",
+#     "cumReturns",
+#     "totReturn",
+#     # groups
+#     "grBounds",
+#     "grGet",
+#     "grCount",
+#     "grFreq",
+#     "grProb",
+#     # tools
+#     "rolWin",
+#     # visualisation
+#     "Aesthetics",
+#     "line",
+#     "scat",
+#     "hist",
+#     "histline",
+#     "bar",
+#     "distr",
+#     "multiplot",
+# ]
