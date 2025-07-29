3de1c85045434954feb4c1ad7ac52ac60e0f78e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:45 +0000

    Add package leak/2.0.1/leak-2.0.1-py3-none-any.whl

---
+++ packages/leak/leak-2.0.1-py3-none-any.whl/leak/config.py
@@ -0,0 +1,9 @@
+import datetime
+import os
+
+DEBUG = bool(os.getenv("LEAK_DEBUG", default=""))
+DATE_FORMAT = "%Y/%m/%d %H:%M"
+EPOCH_BEGIN = datetime.datetime.fromtimestamp(0)
+SHOW_LATEST_RELEASES = 12
+SHOW_PAGER = 30
+PANEL_WIDTH = 70
