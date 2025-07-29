269f6acf65e8fa05e42b65ad7b7ff07710dc98bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:10:19 +0000

    Add package ob-metaflow-stubs/6.0.3.176rc6/ob_metaflow_stubs-6.0.3.176rc6-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.176rc6-py2.py3-none-any.whl/metaflow-stubs/plugins/gcp/gs_storage_client_factory.pyi
@@ -0,0 +1,27 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-04T22:04:18.241891                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+class GcpDefaultClientProvider(object, metaclass=type):
+    @staticmethod
+    def get_gs_storage_client(*args, **kwargs):
+        ...
+    @staticmethod
+    def get_credentials(scopes, *args, **kwargs):
+        ...
+    ...
+
+cached_provider_class: None
+
+def get_gs_storage_client():
+    ...
+
+def get_credentials(scopes, *args, **kwargs):
+    ...
+
