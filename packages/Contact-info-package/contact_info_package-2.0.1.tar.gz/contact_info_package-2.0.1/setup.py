dc43f89daf38c9256937573071374cb07edeb18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:58 +0000

    Add package Contact-info-package/2.0.1/contact_info_package-2.0.1.tar.gz

---
+++ packages/Contact-info-package/contact_info_package-2.0.1.tar.gz/contact_info_package-2.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='Contact_info_package',
+    version='2.0.1',
+    packages=find_packages(),
+    description='A package for managing contact information',
+    author='Vijay Choudhary',
+    # author_email='Vijayghelot2510@gmau.com',
+    # url='https://github.com/yourusername/Contact_info_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # 'License :: OSI Approved :: MIT License',
+        # 'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
