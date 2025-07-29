84cb04343dad1bee9986b9449cc68a9a5b0ed972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:10 +0000

    Add package emmet-core/0.84.7rc3/emmet_core-0.84.7rc3.tar.gz

---
+++ packages/emmet-core/emmet_core-0.84.7rc3.tar.gz/emmet_core-0.84.7rc3/emmet/core/spectrum.py
@@ -0,0 +1,39 @@
+""" Core definition of Spectrum document """
+
+from datetime import datetime
+from typing import List
+
+from pydantic import Field
+
+from emmet.core.mpid import MPID
+from emmet.core.structure import StructureMetadata
+
+
+class SpectrumDoc(StructureMetadata):
+    """
+    Base model definition for any spectra document. This should contain
+    metadata on the structure the spectra pertains to
+    """
+
+    spectrum_name: str
+
+    material_id: MPID | None = Field(
+        None,
+        description="The ID of the material, used as a universal reference across proeprty documents."
+        "This comes in the form: mp-******.",
+    )
+
+    spectrum_id: str = Field(
+        ...,
+        title="Spectrum Document ID",
+        description="The unique ID for this spectrum document.",
+    )
+
+    last_updated: datetime = Field(
+        description="Timestamp for the most recent calculation update for this property.",
+        default_factory=datetime.utcnow,
+    )
+
+    warnings: List[str] = Field(
+        [], description="Any warnings related to this property."
+    )
