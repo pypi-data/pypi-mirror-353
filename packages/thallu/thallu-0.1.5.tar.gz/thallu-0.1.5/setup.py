478f21266d35c9de798ac810bf2c933250505496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:52 +0000

    Add package thallu/0.1.5/thallu-0.1.5.tar.gz

---
+++ packages/thallu/thallu-0.1.5.tar.gz/thallu-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='thallu',
+    version='0.1.5',
+    author='Naveensivam03',
+    description='Lightweight CLI to streamline Git workflows',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Naveensivam03/thallu',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Version Control :: Git',
+    ],
+    python_requires='>=3.7',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'thallu = thallu:main',
+        ],
+    },
+)
