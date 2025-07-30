1c6834daef4e5a07ce128ffb7fa0860a5b1481df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:12 +0000

    Add package media-calc/0.2.2/media_calc-0.2.2-py3-none-any.whl

---
+++ packages/media-calc/media_calc-0.2.2-py3-none-any.whl/media/__init__.py
@@ -0,0 +1,19 @@
+from .funciones import (
+    media,
+    mediana,
+    moda,
+    varianza,
+    desviacion_estandar
+)
+
+__version__ = '0.2.1'
+
+__all__ = [
+    'media',
+    'mediana',
+    'moda',
+    'varianza',
+    'desviacion_estandar'
+]
+
+
