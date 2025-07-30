448fe65196edddb3ee0a42d4b56060b5db402313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:38 +0000

    Add package liminal-cli/0.0.0a2/liminal_cli-0.0.0a2.tar.gz

---
+++ packages/liminal-cli/liminal_cli-0.0.0a2.tar.gz/liminal_cli-0.0.0a2/liminal/config.py
@@ -0,0 +1,4 @@
+import os
+
+API_ADDRESS = os.environ.get('LIMINAL_SHELLSYNC_API_ADDRESS', 'https://shellsync.liminalbios.com/api/v1')
+SYNC_ADDRESS = os.environ.get('LIMINAL_SHELLSYNC_ATUIN_ADDRESS', 'https://shellsync.liminalbios.com') 
