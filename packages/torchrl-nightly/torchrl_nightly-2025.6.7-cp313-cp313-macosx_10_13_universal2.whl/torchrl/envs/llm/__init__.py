7049ef11ae5a694cbae7db04337dcc3ba97054a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:38 +0000

    Add package torchrl-nightly/2025.6.7/torchrl_nightly-2025.6.7-cp313-cp313-macosx_10_13_universal2.whl

---
+++ packages/torchrl-nightly/torchrl_nightly-2025.6.7-cp313-cp313-macosx_10_13_universal2.whl/torchrl/envs/llm/__init__.py
@@ -0,0 +1,49 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .chat import ChatEnv, DatasetChatEnv
+from .datasets import (
+    GSM8KEnv,
+    GSM8KPrepareQuestion,
+    IFEvalData,
+    IFEvalEnv,
+    make_gsm8k_env,
+)
+from .envs import LLMEnv, LLMHashingEnv
+from .libs import make_mlgym, MLGymWrapper
+from .reward import GSM8KRewardParser, IFEvalScoreData, IfEvalScorer
+from .transforms import (
+    as_nested_tensor,
+    as_padded_tensor,
+    DataLoadingPrimer,
+    KLRewardTransform,
+    PythonInterpreter,
+    TemplateTransform,
+    Tokenizer,
+)
+
+__all__ = [
+    "ChatEnv",
+    "DatasetChatEnv",
+    "GSM8KEnv",
+    "make_gsm8k_env",
+    "GSM8KPrepareQuestion",
+    "IFEvalEnv",
+    "PythonInterpreter",
+    "IFEvalData",
+    "LLMEnv",
+    "LLMHashingEnv",
+    "as_nested_tensor",
+    "as_padded_tensor",
+    "DataLoadingPrimer",
+    "GSM8KRewardParser",
+    "make_mlgym",
+    "IFEvalScoreData",
+    "MLGymWrapper",
+    "KLRewardTransform",
+    "TemplateTransform",
+    "Tokenizer",
+    "IfEvalScorer",
+]
