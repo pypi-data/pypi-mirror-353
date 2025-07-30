a8ce192dd1bbf65fa0d2cd901fba817387dd84be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:42 +0000

    Add package learnify-ml/0.0.28/learnify_ml-0.0.28.tar.gz

---
+++ packages/learnify-ml/learnify_ml-0.0.28.tar.gz/learnify_ml-0.0.28/learnify_ml/config/config_paths.py
@@ -0,0 +1,4 @@
+DATA_PREPROCESSING_OUTPUT = "artifacts/dataset/preprocessed_data.csv"
+DATA_PREPROCESSING_INPUT = "artifacts/dataset/train.csv"
+
+MODEL_SAVE_PATH = "artifacts/model/"
---
+++ packages/learnify-ml/learnify_ml-0.0.28.tar.gz/learnify_ml-0.0.28/learnify_ml/src/__init__.py
@@ -0,0 +1,4 @@
+from .custom_exception import *
+from .logger import *
+from .data_preprocessing import *
+from .model_training import *
