8b920e1ce4b281a2d74dfcee65560b64dc7892a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:58 +0000

    Add package fealpy/3.3.0/fealpy-3.3.0.tar.gz

---
+++ packages/fealpy/fealpy-3.3.0.tar.gz/fealpy-3.3.0/fealpy/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger('fealpy')
+logger.setLevel(logging.WARNING)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(name)s: %(message)s', datefmt='%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+
+if not logger.handlers:
+    logger.addHandler(handler)
+    logger.propagate = False
