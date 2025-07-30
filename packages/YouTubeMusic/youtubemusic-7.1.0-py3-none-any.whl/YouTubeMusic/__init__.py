bddd4651b97da4705677c4a1d89f450fcd93d597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:00 +0000

    Add package YouTubeMusic/7.1.0/youtubemusic-7.1.0-py3-none-any.whl

---
+++ packages/YouTubeMusic/youtubemusic-7.1.0-py3-none-any.whl/YouTubeMusic/__init__.py
@@ -0,0 +1,8 @@
+from .YtSearch import Search
+from .Models import format_dur, process_video, extract_artist
+from .Utils import parse_dur, format_ind, format_views
+from .Search import Search
+
+
+
+__version__ = "7.0.0"
