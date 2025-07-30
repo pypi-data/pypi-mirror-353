52dd7a8326c11a92c18cb093d7f6ac5d75fab675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:22 +0000

    Add package wsidicomizer/0.22.1/wsidicomizer-0.22.1-py3-none-any.whl

---
+++ packages/wsidicomizer/wsidicomizer-0.22.1-py3-none-any.whl/wsidicomizer/cli.py
@@ -0,0 +1,242 @@
+#    Copyright 2021 SECTRA AB
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import argparse
+import os
+from pathlib import Path
+from typing import Optional, Sequence
+
+from wsidicom.codec import Jpeg2kSettings, JpegSettings, Settings, Subsampling
+from wsidicom.metadata.schema.json.wsi import WsiMetadataJsonSchema
+from wsidicom.metadata.wsi import WsiMetadata
+
+from wsidicomizer.wsidicomizer import WsiDicomizer
+
+
+class WsiDicomizerCli:
+    def __init__(self):
+        self._parser = argparse.ArgumentParser(
+            description=("Convert compatible wsi file to DICOM")
+        )
+
+        self._parser.add_argument(
+            "-i", "--input", type=Path, required=True, help="Path to input wsi file."
+        )
+        self._parser.add_argument(
+            "-o",
+            "--output",
+            type=Path,
+            help=(
+                "Path to output folder. Folder will be created and must not "
+                "exist. If not specified a folder named after the input file "
+                "is created in the same path."
+            ),
+        )
+        self._parser.add_argument(
+            "-t",
+            "--tile-size",
+            type=int,
+            default=512,
+            help=(
+                "Tile size (same for width and height). Required for ndpi and "
+                "openslide formats E.g. 512"
+            ),
+        )
+        self._parser.add_argument(
+            "-m",
+            "--metadata",
+            type=Path,
+            help=(
+                "Path to json metadata that will override metadata from source image "
+                "file."
+            ),
+        )
+        self._parser.add_argument(
+            "-d",
+            "--default-metadata",
+            type=Path,
+            help=("Path to json metadata that will be used as default values."),
+        )
+        self._parser.add_argument(
+            "-l",
+            "--levels",
+            type=int,
+            nargs="+",
+            help=(
+                "Pyramid levels to include, if not all. E.g. 0 1 for base and "
+                "first pyramid layer. "
+            ),
+        )
+        self._parser.add_argument(
+            "--add-missing-levels",
+            action="store_true",
+            help="If to add missing dyadic levels up to the single tile level.",
+        )
+        self._parser.add_argument(
+            "--label",
+            type=Path,
+            help="Optional label image to use instead of label found in file.",
+        )
+        self._parser.add_argument(
+            "--no-label", action="store_true", help="If not to include label"
+        )
+        self._parser.add_argument(
+            "--no-overview", action="store_true", help="If not to include overview"
+        )
+        self._parser.add_argument(
+            "--no-confidential",
+            action="store_true",
+            help="If not to include confidential metadata",
+        )
+        self._parser.add_argument(
+            "-w",
+            "--workers",
+            type=int,
+            default=os.cpu_count(),
+            help="Number of worker threads to use",
+        )
+        self._parser.add_argument(
+            "--chunk-size",
+            type=int,
+            default=100,
+            help="Number of tiles to give each worker at a time",
+        )
+        self._parser.add_argument(
+            "--format",
+            type=str,
+            default="jpeg",
+            help="Encoding format to use if re-encoding. 'jpeg' or 'jpeg2000'.",
+        )
+        self._parser.add_argument(
+            "--quality",
+            type=float,
+            default=90,
+            help=(
+                "Quality to use if re-encoding. It is recommended to not use > 95 for "
+                "jpeg. Use < 1 or > 1000 for lossless jpeg2000."
+            ),
+        )
+        self._parser.add_argument(
+            "--subsampling",
+            type=str,
+            default="420",
+            help=(
+                "Subsampling option if using jpeg for re-encoding. Use '444' "
+                "for no subsampling, '422' for 2x1 subsampling, and '420' for "
+                "2x2 subsampling."
+            ),
+        )
+        self._parser.add_argument(
+            "--offset-table",
+            type=str,
+            default="bot",
+            help=(
+                "Offset table to use, 'bot' basic offset table, 'eot' "
+                "extended offset table, 'None' - no offset table."
+            ),
+        )
+
+    def cli(self):
+        args = self._parser.parse_args()
+        if not args.metadata:
+            metadata = None
+        else:
+            metadata = self._load_metadata(args.metadata)
+        if not args.default_metadata:
+            default_metadata = None
+        else:
+            default_metadata = self._load_metadata(args.default_metadata)
+        if not args.levels:
+            levels = None
+        else:
+            levels = args.levels
+        encoding_format = args.format
+        if encoding_format == "jpeg":
+            subsampling = Subsampling.from_string(args.subsampling)
+            encoding_settings = JpegSettings(
+                quality=args.quality, subsampling=subsampling
+            )
+        elif encoding_format == "jpeg2000":
+            encoding_settings = Jpeg2kSettings(levels=[args.quality])
+        else:
+            encoding_settings = None
+        self.convert(
+            filepath=args.input,
+            output_path=args.output,
+            metadata=metadata,
+            default_metadata=default_metadata,
+            tile_size=args.tile_size,
+            add_missing_levels=args.add_missing_levels,
+            include_levels=levels,
+            include_label=not args.no_label,
+            include_overview=not args.no_overview,
+            include_confidential=not args.no_confidential,
+            workers=args.workers,
+            chunk_size=args.chunk_size,
+            encoding_settings=encoding_settings,
+            offset_table=args.offset_table,
+            label=args.label,
+        )
+
+    def convert(
+        self,
+        filepath: Path,
+        output_path: Path,
+        metadata: Optional[WsiMetadata] = None,
+        default_metadata: Optional[WsiMetadata] = None,
+        tile_size: int = 512,
+        add_missing_levels: bool = False,
+        include_levels: Optional[Sequence[int]] = None,
+        include_label: bool = True,
+        include_overview: bool = True,
+        include_confidential: bool = True,
+        workers: Optional[int] = None,
+        chunk_size: Optional[int] = None,
+        encoding_settings: Optional[Settings] = None,
+        offset_table: str = "bot",
+        label: Optional[Path] = None,
+    ):
+        WsiDicomizer.convert(
+            filepath=filepath,
+            output_path=output_path,
+            metadata=metadata,
+            default_metadata=default_metadata,
+            tile_size=tile_size,
+            add_missing_levels=add_missing_levels,
+            include_levels=include_levels,
+            include_label=include_label,
+            include_overview=include_overview,
+            include_confidential=include_confidential,
+            workers=workers,
+            chunk_size=chunk_size,
+            encoding=encoding_settings,
+            offset_table=offset_table,
+            label=label,
+        )
+
+    @staticmethod
+    def _load_metadata(filepath: Path) -> WsiMetadata:
+        with open(filepath) as json_file:
+            metadata = WsiMetadataJsonSchema().loads(json_file.read())
+            assert isinstance(metadata, WsiMetadata)
+            return metadata
+
+
+def main():
+    cli = WsiDicomizerCli()
+    cli.cli()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/wsidicomizer/wsidicomizer-0.22.1-py3-none-any.whl/wsidicomizer/metadata.py
@@ -0,0 +1,249 @@
+#    Copyright 2023 SECTRA AB
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+"""Base model for metadata."""
+from dataclasses import Field, fields, is_dataclass, replace
+from typing import Any, Callable, Optional, Sequence, Type, TypeVar
+
+from PIL import ImageCms
+from pydicom import Dataset
+from pydicom.uid import UID
+from wsidicom.metadata import (
+    Equipment,
+    Image,
+    Label,
+    OpticalPath,
+    Patient,
+    Series,
+    Slide,
+    Study,
+    WsiMetadata,
+)
+
+ModelType = TypeVar("ModelType")
+
+
+class WsiDicomizerMetadata(WsiMetadata):
+    """Subclass of WsiMetadata that can be constructed with optional parameters."""
+
+    def __init__(
+        self,
+        study: Optional[Study] = None,
+        series: Optional[Series] = None,
+        patient: Optional[Patient] = None,
+        equipment: Optional[Equipment] = None,
+        optical_paths: Optional[Sequence[OpticalPath]] = None,
+        slide: Optional[Slide] = None,
+        label: Optional[Label] = None,
+        image: Optional[Image] = None,
+        frame_of_reference_uid: Optional[UID] = None,
+        dimension_organization_uids: Optional[Sequence[UID]] = None,
+    ):
+        super().__init__(
+            study if study is not None else Study(),
+            series if series is not None else Series(),
+            patient if patient is not None else Patient(),
+            equipment if equipment is not None else Equipment(),
+            optical_paths if optical_paths is not None else [],
+            slide if slide is not None else Slide(),
+            label if label is not None else Label(),
+            image if image is not None else Image(),
+            frame_of_reference_uid,
+            dimension_organization_uids,
+        )
+
+    def merge(
+        self,
+        user: Optional[WsiMetadata],
+        default: Optional[WsiMetadata],
+        include_confidential: bool,
+    ) -> "WsiDicomizerMetadata":
+        if not include_confidential:
+            base = self._remove_confidential()
+        else:
+            base = self
+        if user is None and default is None:
+            return self._merge_not_none(WsiDicomizerMetadata, base, None, None)
+        if user is None:
+            user = WsiDicomizerMetadata()
+        if default is None:
+            default = WsiDicomizerMetadata()
+        return WsiDicomizerMetadata(
+            study=self._merge(Study, base.study, user.study, default.study),
+            series=self._merge(Series, base.series, user.series, default.series),
+            patient=self._merge(Patient, base.patient, user.patient, default.patient),
+            equipment=self._merge(
+                Equipment, base.equipment, user.equipment, default.equipment
+            ),
+            optical_paths=self._merge_list(
+                OpticalPath,
+                base.optical_paths,
+                user.optical_paths,
+                default.optical_paths,
+            ),
+            slide=self._merge(Slide, base.slide, user.slide, default.slide),
+            label=self._merge(Label, base.label, user.label, default.label),
+            image=self._merge(Image, base.image, user.image, default.image),
+        )
+
+    def _remove_confidential(self) -> "WsiDicomizerMetadata":
+        return WsiDicomizerMetadata(
+            study=None,
+            series=None,
+            patient=self.patient.remove_confidential() if self.patient else None,
+            equipment=self.equipment.remove_confidential() if self.equipment else None,
+            optical_paths=self.optical_paths,
+            slide=None,
+            label=self.label.remove_confidential() if self.label else None,
+            image=self.image.remove_confidential() if self.image else None,
+            frame_of_reference_uid=None,
+            dimension_organization_uids=None,
+        )
+
+    @classmethod
+    def _merge_list(
+        cls,
+        model_class: Type[ModelType],
+        base: Sequence[ModelType],
+        user: Sequence[ModelType],
+        default: Sequence[ModelType],
+    ) -> Sequence[ModelType]:
+        models = [model for model in (user, base, default) if len(model) > 0]
+        if len(models) == 0 or all(item is None for model in models for item in model):
+            # All lists empty
+            return []
+        if len(models) == 1:
+            # Only one list not empty
+            return models[0]
+        user_expanded = cls._repeat_list(base, user)
+        default_expanded = cls._repeat_list(base, default)
+        return [
+            cls._merge_not_none(model_class, base_item, user_item, default_item)
+            for base_item, user_item, default_item in zip(
+                base, user_expanded, default_expanded
+            )
+        ]
+
+    @staticmethod
+    def _repeat_list(
+        base: Sequence[ModelType], to_repeat: Sequence[ModelType]
+    ) -> Sequence[Optional[ModelType]]:
+        if len(to_repeat) > 1 and len(to_repeat) != len(base):
+            raise ValueError(
+                "List to repeat must have length 0, 1 or length of base. "
+                f"Length of list to repeat: {len(to_repeat)}. "
+                f"Length of base: {len(base)}."
+            )
+        if len(to_repeat) == len(base):
+            return to_repeat
+        if len(to_repeat) == 0:
+            item_to_repeat = None
+        else:
+            item_to_repeat = to_repeat[0]
+        return [item_to_repeat for _ in range(len(base))]
+
+    @classmethod
+    def _merge(
+        cls,
+        model_class: Type[ModelType],
+        base: Optional[ModelType],
+        user: Optional[ModelType],
+        default: Optional[ModelType],
+    ) -> Optional[ModelType]:
+        """Merge three models to a new model.
+
+        - base - model from file.
+        - user - user defined model that overrides base.
+        - default - user defined model with default values used for fields not defined
+        in base or user model.
+
+        """
+        not_none = [model for model in [user, base, default] if model is not None]
+        if len(not_none) == 0:
+            return None
+        assert is_dataclass(model_class)
+        attributes = {
+            field.name: cls._select_value(field, base, user, default)
+            for field in fields(model_class)
+        }
+
+        return model_class(**attributes)
+
+    @classmethod
+    def _merge_not_none(
+        cls,
+        model_class: Type[ModelType],
+        base: Optional[ModelType],
+        user: Optional[ModelType],
+        default: Optional[ModelType],
+    ) -> ModelType:
+        merged = cls._merge(model_class, base, user, default)
+        assert merged is not None
+        return merged
+
+    @classmethod
+    def _select_value(
+        cls,
+        field: Field,
+        base: Optional[ModelType],
+        user: Optional[ModelType],
+        default: Optional[ModelType],
+    ) -> Any:
+        base_value = getattr(base, field.name, None)
+        user_value = getattr(user, field.name, None)
+        default_value = getattr(default, field.name, None)
+        value = next(
+            (
+                value
+                for value in [user_value, base_value, default_value]
+                if value is not None
+            ),
+            None,
+        )
+        if is_dataclass(value) and isinstance(value, object):
+            value = cls._merge(type(value), base_value, user_value, default_value)
+        return value
+
+    def insert_default_icc_profile(self) -> WsiMetadata:
+        if len(self.optical_paths) == 0:
+            # No optical paths defined, add one with icc profile
+            optical_paths = [
+                OpticalPath(icc_profile=self._create_default_icc_profile())
+            ]
+        else:
+            # Optical paths defined, add icc profile if missing
+            optical_paths = [
+                (
+                    replace(
+                        optical_path,
+                        icc_profile=self._create_default_icc_profile(),
+                    )
+                    if optical_path.icc_profile is None
+                    else optical_path
+                )
+                for optical_path in self.optical_paths
+            ]
+        return replace(
+            self,
+            optical_paths=optical_paths,
+            frame_of_reference_uid=self.default_frame_of_reference_uid,
+            dimension_organization_uids=self.default_dimension_organization_uids,
+        )
+
+    @staticmethod
+    def _create_default_icc_profile() -> bytes:
+        return ImageCms.ImageCmsProfile(ImageCms.createProfile("sRGB")).tobytes()
+
+
+MetadataPostProcessor = Callable[[Dataset, WsiMetadata], Dataset]
