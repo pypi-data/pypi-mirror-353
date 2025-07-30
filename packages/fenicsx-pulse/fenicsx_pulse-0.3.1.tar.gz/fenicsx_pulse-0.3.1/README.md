c7d28e681fb092a3a75399733338d2d6fd51a0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:22 +0000

    Add package fenicsx-pulse/0.3.1/fenicsx_pulse-0.3.1.tar.gz

---
+++ packages/fenicsx-pulse/fenicsx_pulse-0.3.1.tar.gz/fenicsx_pulse-0.3.1/README.md
@@ -0,0 +1,36 @@
+![_](docs/pulse-logo.png)
+
+[![MIT](https://img.shields.io/github/license/finsberg/fenicsx-pulse)](https://github.com/finsberg/fenicsx-pulse/blob/main/LICENSE)
+[![PyPI version](https://badge.fury.io/py/fenicsx-pulse.svg)](https://pypi.org/project/fenicsx_pulse/)
+[![Test package](https://github.com/finsberg/fenicsx-pulse/actions/workflows/test_package_coverage.yml/badge.svg)](https://github.com/finsberg/fenicsx-pulse/actions/workflows/test_package_coverage.yml)
+[![Pre-commit](https://github.com/finsberg/fenicsx-pulse/actions/workflows/pre-commit.yml/badge.svg)](https://github.com/finsberg/fenicsx-pulse/actions/workflows/pre-commit.yml)
+[![Deploy static content to Pages](https://github.com/finsberg/fenicsx-pulse/actions/workflows/build_docs.yml/badge.svg)](https://github.com/finsberg/fenicsx-pulse/actions/workflows/build_docs.yml)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Create and publish a Docker image](https://github.com/finsberg/fenicsx-pulse/actions/workflows/docker-image.yml/badge.svg)](https://github.com/finsberg/fenicsx-pulse/pkgs/container/fenicsx_pulse)
+[![Coverage](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/finsberg/a7290de789564f03eb6b1ee122fce423/raw/fenicsx-pulse-coverage.json)](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/finsberg/a7290de789564f03eb6b1ee122fce423/raw/fenicsx-pulse-coverage.json)
+
+# fenicsx-pulse
+
+`fenicsx-pulse` is a cardiac mechanics solver based on FEniCSx. It is a successor of [`pulse`](https://github.com/finsberg/pulse) which is a cardiac mechanics solver based on FEniCS.
+
+* Documentation: https://finsberg.github.io/fenicsx-pulse/
+* Source code: https://github.com/finsberg/fenicsx-pulse
+
+## Install
+
+To install `fenicsx_pulse` you need to first [install FEniCSx](https://github.com/FEniCS/dolfinx#installation). Next you can install `fenicsx_pulse` via pip
+```
+python3 -m pip install fenicsx-pulse
+```
+We also provide a pre-built docker image with FEniCSx and `fenicsx_pulse` installed. You pull this image using the command
+```
+docker pull ghcr.io/finsberg/fenicsx-pulse:v0.3.1
+```
+
+## Getting started
+Checkout out [the demos](https://finsberg.github.io/fenicsx-pulse/demo/unit_cube.html) in the documentation
+
+
+
+## Contributing
+See https://finsberg.github.io/fenicsx-pulse/CONTRIBUTING.html
---
+++ packages/fenicsx-pulse/fenicsx_pulse-0.3.1.tar.gz/fenicsx_pulse-0.3.1/tests/test_material_models.py
@@ -0,0 +1,186 @@
+import math
+
+import dolfinx
+import pytest
+import ufl
+
+import pulse
+
+
+@pytest.mark.parametrize("obj_str", ("float", "Constant", "Function"))
+def test_linear_elastic_model(obj_str, mesh, P1, u) -> None:
+    E = 2.0
+    _nu = 0.2
+    nu = pulse.utils.float2object(f=_nu, obj_str=obj_str, mesh=mesh, V=P1)
+    model = pulse.LinearElastic(E=pulse.Variable(E, "Pa"), nu=nu)
+
+    u.interpolate(lambda x: x)
+    F = pulse.kinematics.DeformationGradient(u)
+    # F = 2I, e = I, tr(e) = 3
+    # sigma = (E / (1 + nu)) * (e + (nu / (1 - 2 * nu)) * tr(e) * I
+    # sigma = (E / (1 + nu)) * (1 + (nu / (1 - 2 * nu)) * 3) * I
+    sigma = model.sigma(F)
+    I = ufl.Identity(3)
+    zero = sigma - (E / (1 + _nu)) * (1 + (_nu / (1 - 2 * _nu)) * 3) * I
+    assert pulse.utils.matrix_is_zero(zero)
+
+
+@pytest.mark.parametrize("obj_str", ("float", "Constant", "Function"))
+def test_linear_elastic_model_with_invalid_range(obj_str, mesh, P1) -> None:
+    E = pulse.Variable(2.0, "kPa")
+    _nu = 0.5
+    nu = pulse.utils.float2object(f=_nu, obj_str=obj_str, mesh=mesh, V=P1)
+
+    with pytest.raises(pulse.exceptions.InvalidRangeError):
+        pulse.LinearElastic(E=E, nu=nu)
+
+
+@pytest.mark.parametrize(
+    "params_func, expected_value",
+    (
+        (pulse.HolzapfelOgden.orthotropic_parameters, 1235.2937267586497),
+        (pulse.HolzapfelOgden.partly_orthotropic_parameters, 1435.8702731579442),
+        (
+            pulse.HolzapfelOgden.transversely_isotropic_parameters,
+            53646.81246074524,
+        ),
+    ),
+)
+def test_holzapfel_ogden(params_func, expected_value, mesh, u) -> None:
+    params = params_func()
+    f0 = dolfinx.fem.Constant(mesh, (1.0, 0.0, 0.0))
+    s0 = dolfinx.fem.Constant(mesh, (0.0, 1.0, 0.0))
+    model = pulse.HolzapfelOgden(f0=f0, s0=s0, **params)
+
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    # F = I + 0.1 I, C = 1.21 I
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+
+    assert math.isclose(value, expected_value)
+
+
+def test_holzapfel_ogden_invalid_range():
+    with pytest.raises(pulse.exceptions.InvalidRangeError):
+        pulse.HolzapfelOgden(a=-1.0)
+
+
+@pytest.mark.parametrize(
+    "params, attr",
+    (
+        ({"a_f": 1}, "f0"),
+        ({"a_s": 1}, "s0"),
+        ({"a_fs": 1}, "f0 and/or s0"),
+    ),
+)
+def test_holzapfel_ogden_raises_MissingModelAttribute(params, attr):
+    with pytest.raises(pulse.exceptions.MissingModelAttribute) as e:
+        pulse.HolzapfelOgden(**params)
+    assert e.value == pulse.exceptions.MissingModelAttribute(
+        attr=attr,
+        model="HolzapfelOgden",
+    )
+
+
+def test_holzapfel_ogden_neohookean(u):
+    model = pulse.HolzapfelOgden(a=1.0)
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+    # F = I + 0.1 I, C = 1.21 I, I1= 3*1.21
+    # psi = (a / 2) * (I1 - 3) = 0.5 (3 * 1.21 - 3) = 0.315
+    # Convert to Pa
+
+    assert math.isclose(value, 315.0)
+
+
+def test_holzapfel_ogden_pure_fiber(u, mesh):
+    f0 = dolfinx.fem.Constant(mesh, (1.0, 0.0, 0.0))
+    model = pulse.HolzapfelOgden(a_f=1.0, f0=f0)
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+    # F = I + 0.1 I, C = 1.21 I, I4f = 1.21
+    # psi = (a_f / 2) * (I4 - 1)**2 = 0.5 * 0.21**2
+    assert math.isclose(value, 1000 * 0.5 * 0.21**2)
+
+
+def test_holzapfel_ogden_pure_fiber_sheets(u, mesh):
+    f0 = dolfinx.fem.Constant(mesh, (1.0, 0.0, 0.0))
+    model = pulse.HolzapfelOgden(a_fs=1.0, f0=f0, s0=f0)
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+    # F = I + 0.1 I, = 1.1 -> I8fs = 1.21
+    # psi = (a_f / 2) * I8fs**2 = 0.5 * 1.21**2
+    assert math.isclose(value, 1000 * 0.5 * 1.21**2)
+
+
+def test_neo_hookean(u, mesh):
+    model = pulse.NeoHookean(mu=1.0)
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+    # F = I + 0.1 I, C = 1.21 I
+    # psi = (mu / 2) * (I1 - 3) = 0.5 * (3.63 - 3)
+    assert math.isclose(value, 1000 * 0.5 * 0.63)
+
+
+def test_saint_venant_kirchhoff(u):
+    lmbda = 1.0
+    mu = 1.0
+    model = pulse.SaintVenantKirchhoff(lmbda=lmbda, mu=mu)
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+    # gradu = 0.1 I, epsilon = 0.5 (gradu + gradu.T) = 0.1 I
+    # tr(epsilon) = 0.1 * 3 = 0.3, tr(epsilon * epsilon) = 0.1**2 * 3 = 0.03
+    # psi = lmbda / 2 * tr(epsilon)**2 + mu * tr(epsilon * epsilon)
+    expected = 0.5 * lmbda * 0.3**2 + mu * 0.03
+    assert math.isclose(value, expected)
+
+
+def test_guccione_isotropic(u):
+    C = 10.0
+    bf = bt = bfs = 1.0
+    model = pulse.Guccione(C=C, bf=bf, bt=bt, bfs=bfs)
+    assert model.is_isotropic()
+
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+    # F = I + 0.1 I, C = 1.21 I, E = 0.5 * (C - I) = 0.105 I
+    # E * E = 0.105**2 I, tr(E * E) = 0.105**2 * 3
+    # psi = 0.5 * C * (exp(E * E) - 1) = 0.5 * 10.0 * (exp(0.105 ** 2) - 1)
+    assert math.isclose(value, 1000 * 0.5 * 10.0 * (math.exp(3 * (0.105**2)) - 1))
+
+
+def test_guccione_anisotropic(u, mesh):
+    f0 = dolfinx.fem.Constant(mesh, (1.0, 0.0, 0.0))
+    s0 = dolfinx.fem.Constant(mesh, (0.0, 1.0, 0.0))
+    n0 = dolfinx.fem.Constant(mesh, (0.0, 0.0, 1.0))
+
+    C = 10.0
+    bf = 1.0
+    bt = 2.0
+    bfs = 3.0
+    model = pulse.Guccione(C=C, bf=bf, bt=bt, bfs=bfs, f0=f0, s0=s0, n0=n0)
+    assert not model.is_isotropic()
+
+    u.interpolate(lambda x: x / 10)
+    F = pulse.kinematics.DeformationGradient(u)
+    psi = model.strain_energy(F)
+    value = dolfinx.fem.assemble_scalar(dolfinx.fem.form(psi * ufl.dx))
+    # F = I + 0.1 I, C = 1.21 I, E = 0.5 * (C - I) = 0.105 I
+    # E11 = E22 = E33 = 0.105, E12 = E13 = E23 = 0
+    # Q = bf * E11**2 + bt * (E22**2 + E33**2 + 2 * E23**2) + bfs * (2 * E12**2 + 2 * E13**2)
+    # Q = E11**2 (bf + 2 bt)
+    Q = (0.105**2) * (1 + 2 * 2)
+    assert math.isclose(value, 1000 * 0.5 * 10.0 * (math.exp(Q) - 1))
