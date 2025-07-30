0eeae8a93ba1cebc4707e1c541d170a527839c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:13 +0000

    Add package sportsball/0.5.6/sportsball-0.5.6.tar.gz

---
+++ packages/sportsball/sportsball-0.5.6.tar.gz/sportsball-0.5.6/sportsball/data/nfl/position.py
@@ -0,0 +1,16 @@
+"""The enumeration of the different supported positions."""
+
+from enum import StrEnum
+
+
+class Position(StrEnum):
+    """An enumeration over the different positions."""
+
+    LINE_BACKER = "LB"
+    DEFENSIVE_TACKLE = "DT"
+    QUARTER_BACK = "QB"
+    DEFENSIVE_LINESMAN = "DL"
+    OFFENSIVE_LINESMAN = "OL"
+    SAFETY = "S"
+    WIDE_RECEIVER = "WR"
+    GUARD = "G"
