7d2059a19c8147401635cb31c6ddd46cdb11a08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:41 +0000

    Add package instagram-collector/0.0.6/instagram_collector-0.0.6.tar.gz

---
+++ packages/instagram-collector/instagram_collector-0.0.6.tar.gz/instagram_collector-0.0.6/instagram_collector/__init__.py
@@ -0,0 +1,17 @@
+from .hashtag import InstagramHashtagCollector
+from .tagged import InstagramTaggedCollector
+from .comments import InstagramCommentCollector
+from .brands import InstagramBrandCollector
+from .profile import InstagramProfileCollector
+from .post_recent import InstagramPostRecentCollector
+from .utils import transform_selling_product, hashtag_detect
+
+__all__ = [
+    'InstagramHashtagCollector',
+    'InstagramTaggedCollector',
+    'InstagramCommentCollector',
+    'InstagramBrandCollector',
+    'InstagramPostRecentCollector',
+    'InstagramProfileCollector'
+]
+__version__ = "0.0.6"
