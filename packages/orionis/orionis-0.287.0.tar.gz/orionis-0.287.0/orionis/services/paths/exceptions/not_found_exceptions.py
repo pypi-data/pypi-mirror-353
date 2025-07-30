6d72416d60a120e302746e86c41af96b61401a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:29 +0000

    Add package orionis/0.287.0/orionis-0.287.0.tar.gz

---
+++ packages/orionis/orionis-0.287.0.tar.gz/orionis-0.287.0/orionis/services/paths/exceptions/not_found_exceptions.py
@@ -0,0 +1,28 @@
+class OrionisFileNotFoundException(Exception):
+    """
+    Exception raised when a file is not found in the Orionis framework.
+
+    Args:
+        msg (str): A detailed message describing the missing file.
+
+    Example:
+        raise OrionisFileNotFoundException("File 'config.yaml' not found.")
+    """
+
+    def __init__(self, msg: str):
+        """
+        Initializes the exception with a custom error message.
+
+        Args:
+            msg (str): The error message describing the exception.
+        """
+        super().__init__(msg)
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of the exception, including the class name and the first argument.
+
+        Returns:
+            str: A formatted string with the exception class name and the first argument.
+        """
+        return f"{self.__class__.__name__}: {self.args[0]}"
