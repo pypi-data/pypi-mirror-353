c4dcfd44515ca245984e274a51ef913300baf202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:16 +0000

    Add package greetlibrary/0.1.0/greetlibrary-0.1.0.tar.gz

---
+++ packages/greetlibrary/greetlibrary-0.1.0.tar.gz/greetlibrary-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="greetlibrary",
+    version="0.1.0",
+    description="A simple greeting library",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
