53f99a387e4c70009f56ef2a16c7e7dfdf545df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:39 +0000

    Add package varun-portal-gun/0.20.0/varun_portal_gun-0.20.0-py3-none-any.whl

---
+++ packages/varun-portal-gun/varun_portal_gun-0.20.0-py3-none-any.whl/varun_portal_gun/main.py
@@ -0,0 +1,32 @@
+import typer
+from pathlib import Path
+from typing import Optional
+
+from typing_extensions import Annotated
+import os
+import joblib
+import pandas as pd
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    pass
+
+@app.command()
+def predict(path: Annotated[str, typer.Option()] = None):
+    print("Here are your input files: " + path)
+
+    for filename in os.listdir(path):
+        print(filename)
+
+
+
+@app.command()
+def run_model():
+    print("Loaded Model")
+    loaded_model = joblib.load("/Users/vyelluru/Desktop/bdc_power_det_v2.sav")
+    X_test = [1.00000, 17.7500, 4.413225, 1858.00]
+    result = loaded_model.predict(pd.Series(X_test))
+    print("Success")
