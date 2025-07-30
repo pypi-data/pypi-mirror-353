f38666b51bb38c4b0e89f8080515e1df6977b379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:13 +0000

    Add package RinUI/0.1.1/rinui-0.1.1.tar.gz

---
+++ packages/RinUI/rinui-0.1.1.tar.gz/rinui-0.1.1/RinUI/core/translator.py
@@ -0,0 +1,25 @@
+from PySide6.QtCore import QTranslator, QLocale
+from .config import RINUI_PATH
+import os
+
+
+class RinUITranslator(QTranslator):
+    """
+    RinUI i18n translator.
+    :param locale: QLocale, optional, default is system locale
+    """
+    def __init__(self, locale: QLocale = QLocale.system().name(), parent=None):  # follow system
+        super().__init__(parent)
+        self.load(locale or QLocale())
+
+    def load(self, locale: QLocale) -> bool:
+        """
+        Load translation file for the given locale.
+        :param locale: QLocale, the locale to load (eg = QLocale(QLocale.Chinese, QLocale.China), QLocale("zh_CN"))
+        :return: bool
+        """
+        print(f"🌏 Current locale: {locale.name()}")
+        path = os.path.join(RINUI_PATH, "RinUI", "languages", f"{locale.name()}.qm")
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Cannot find translation file: {path}")
+        return super().load(path)
