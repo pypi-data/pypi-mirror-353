d367c547eb1b3a645e92e45f363b817b22682f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:15 +0000

    Add package gafoc/0.3.3/gafoc-0.3.3.tar.gz

---
+++ packages/gafoc/gafoc-0.3.3.tar.gz/gafoc-0.3.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gafoc",
+    version="0.3.3",
+    author="Gafoo",
+    author_email="gafarssprts@gmail.com",
+    description="Advanced file compression library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gafoo173/Calculators",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    # entry_points تحذفها لو مش عايز CLI
+)
