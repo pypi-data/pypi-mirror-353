62782d2297a3283b77242256463e318f068b3aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:02 +0000

    Add package tatools01/1.0/tatools01-1.0.tar.gz

---
+++ packages/tatools01/tatools01-1.0.tar.gz/tatools01-1.0/tatools01/__init__.py
@@ -0,0 +1,57 @@
+"""
+ntanh
+
+An python parametters library.
+"""
+
+__version__ = "1.0" # Nhớ update cả Readme.md
+
+__author__ = "Nguyễn Tuấn Anh - nt.anh.fai@gmail.com"
+__credits__ = "MIT License"
+__console__ = "ntanh, ntanh_aug, ntanh_img_del"
+import os
+import sys
+import argparse
+import json
+ 
+   
+from . import ParamsBase
+from tatools01.Thoi_gian.taTimers import MultiTimer
+
+
+# draw_xyxyn_rectangles_and_show, draw_rectangles_and_show, draw_yolo_boxes_and_show, get_Org_xyxyn__from_Cropped_resized_bboxn
+
+
+# ImageAugmentation.Augment_image_in_multiple_ways
+# fnPlot_Warp_Text
+# PlotText_UTF8
+# PlotText_UTF8_outline
+# opposite_color
+# putTextWithOutline
+# draw_bounding_box
+# PlotText
+
+# rotate_image
+# ImageInfomation
+
+# fnPlot_Warp_Text
+# PlotText_UTF8
+# PlotText_UTF8_outline
+# putTextWithOutline
+
+
+# MultiTimer()
+# test()
+__help__ = """
+from ntanh.ParamsBase import tactParametters
+from ntanh.yolo_boxes import xyxy_to_yolo_str, yolo_str_to_xyxy, calculate_luminance
+"""
+
+def console_main():
+    print(
+        """
+Running: 
+1. tact
+        """
+    )
+     
