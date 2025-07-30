7dec58525806c3f61316de1e64c7992df20da029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:22 +0000

    Add package varun-portal-gun/0.11.0/varun_portal_gun-0.11.0.tar.gz

---
+++ packages/varun-portal-gun/varun_portal_gun-0.11.0.tar.gz/varun_portal_gun-0.11.0/src/varun_portal_gun/main.py
@@ -0,0 +1,33 @@
+import typer
+from pathlib import Path
+from typing import Optional
+
+from typing_extensions import Annotated
+import os
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    pass
+
+@app.command()
+def predict(path: Annotated[Optional[Path], typer.Option()] = None):
+    print("Here are your input files: " + path)
+
+    for filename in os.listdir(path):
+        print(filename)
+
+
+@app.command()
+def tester(config: Annotated[Optional[Path], typer.Option()] = None):
+    if config is None:
+        print("No config file")
+        raise typer.Abort()
+    if config.is_file():
+        text = config.read_text()
+        print(f"Config file contents: {text}")
+    elif config.is_dir():
+        print("Config is a directory, will use all its config files")
+    elif not config.exists():
+        print("The config doesn't exist")
