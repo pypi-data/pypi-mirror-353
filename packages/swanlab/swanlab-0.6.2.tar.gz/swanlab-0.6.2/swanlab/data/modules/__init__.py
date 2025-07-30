120a608dd0106e463c78b8407c00de4b895b33da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:56 +0000

    Add package swanlab/0.6.2/swanlab-0.6.2.tar.gz

---
+++ packages/swanlab/swanlab-0.6.2.tar.gz/swanlab-0.6.2/swanlab/data/modules/__init__.py
@@ -0,0 +1,34 @@
+from typing import List, Union
+
+from swankit.core.data import BaseType, MediaBuffer, MediaType
+
+from .audio import Audio
+from .echarts import echarts, Echarts, PyEchartsBase
+from .image import Image
+from .line import FloatConvertible, Line
+from .object3d import Model3D, Object3D, PointCloud, Molecule
+from .text import Text
+from .wrapper import DataWrapper
+
+DataType = Union[int, float, FloatConvertible, BaseType, List[BaseType]]
+ChartType = BaseType.Chart
+
+__all__ = [
+    "FloatConvertible",
+    "DataWrapper",
+    "MediaType",
+    "Image",
+    "Audio",
+    "Text",
+    "Line",
+    "DataType",
+    "ChartType",
+    "MediaBuffer",
+    "Object3D",
+    "PointCloud",
+    "Model3D",
+    "Molecule",
+    "Echarts",
+    "echarts",
+    "PyEchartsBase",
+]
