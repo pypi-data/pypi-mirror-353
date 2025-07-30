2ad1d8e556fed4ffa733af945704492ce6e094da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:01 +0000

    Add package skypilot-nightly/1.0.0.dev20250606/skypilot_nightly-1.0.0.dev20250606.tar.gz

---
+++ packages/skypilot-nightly/skypilot_nightly-1.0.0.dev20250606.tar.gz/skypilot_nightly-1.0.0.dev20250606/sky/models.py
@@ -0,0 +1,59 @@
+"""Data Models for SkyPilot."""
+
+import collections
+import dataclasses
+from typing import Any, Dict, Optional
+
+
+@dataclasses.dataclass
+class User:
+    """Dataclass to store user information."""
+    # User hash
+    id: str
+    # Display name of the user
+    name: Optional[str] = None
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {'id': self.id, 'name': self.name}
+
+
+RealtimeGpuAvailability = collections.namedtuple(
+    'RealtimeGpuAvailability', ['gpu', 'counts', 'capacity', 'available'])
+
+
+@dataclasses.dataclass
+class KubernetesNodeInfo:
+    """Dataclass to store Kubernetes node information."""
+    name: str
+    accelerator_type: Optional[str]
+    # Resources available on the node. E.g., {'nvidia.com/gpu': '2'}
+    total: Dict[str, int]
+    free: Dict[str, int]
+
+
+@dataclasses.dataclass
+class KubernetesNodesInfo:
+    """Dataclass to store Kubernetes node info map."""
+    # The nodes in the cluster, keyed by node name.
+    node_info_dict: Dict[str, KubernetesNodeInfo]
+    # Additional hint for the node info.
+    hint: str
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'node_info_dict': {
+                node_name: dataclasses.asdict(node_info)
+                for node_name, node_info in self.node_info_dict.items()
+            },
+            'hint': self.hint,
+        }
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> 'KubernetesNodesInfo':
+        return cls(
+            node_info_dict={
+                node_name: KubernetesNodeInfo(**node_info)
+                for node_name, node_info in data['node_info_dict'].items()
+            },
+            hint=data['hint'],
+        )
