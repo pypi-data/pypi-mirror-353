c35f4b9e062fee7de36196b4a5971d0851caf7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:33 +0000

    Add package attoworld/2025.0.20/attoworld-2025.0.20-cp313-cp313-win_amd64.whl

---
+++ packages/attoworld/attoworld-2025.0.20-cp313-cp313-win_amd64.whl/attoworld/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tools from the Attosecond science group at the Max Planck Institute of Quantum Optics, a.k.a. [Attoworld](https://www.attoworld.de)
+"""
+from . import file
+from . import numeric
+from . import personal
+from . import plot
+from . import spectrum
+from . import wave
+from . import attoworld_rs
