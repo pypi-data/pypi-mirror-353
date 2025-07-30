e40841874cb3b0e761a3a964952d6eeb90378862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:56 +0000

    Add package structifyai/1.119.0/structifyai-1.119.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.119.0-py3-none-any.whl/structify/types/chat_session.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from datetime import datetime
+
+from .._models import BaseModel
+
+__all__ = ["ChatSession"]
+
+
+class ChatSession(BaseModel):
+    id: str
+
+    created_at: datetime
+
+    project_id: str
+
+    updated_at: datetime
+
+    user_id: str
---
+++ packages/structifyai/structifyai-1.119.0-py3-none-any.whl/structify/types/chat_session_with_messages.py
@@ -0,0 +1,36 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+from datetime import datetime
+
+from .._models import BaseModel
+
+__all__ = ["ChatSessionWithMessages", "Message"]
+
+
+class Message(BaseModel):
+    id: str
+
+    chat_session_id: str
+
+    content: str
+
+    created_at: datetime
+
+    role: str
+
+    timestamp: datetime
+
+
+class ChatSessionWithMessages(BaseModel):
+    id: str
+
+    created_at: datetime
+
+    messages: List[Message]
+
+    project_id: str
+
+    updated_at: datetime
+
+    user_id: str
