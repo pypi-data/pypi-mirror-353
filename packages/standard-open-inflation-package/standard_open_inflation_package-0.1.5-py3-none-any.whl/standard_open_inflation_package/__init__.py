d2e2f07b39b8239926ddc9ed1ee728cd904b3f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:05:36 +0000

    Add package standard-open-inflation-package/0.1.5/standard_open_inflation_package-0.1.5-py3-none-any.whl

---
+++ packages/standard-open-inflation-package/standard_open_inflation_package-0.1.5-py3-none-any.whl/standard_open_inflation_package/__init__.py
@@ -0,0 +1,27 @@
+"""
+Standard Open Inflation Package
+
+Модульная библиотека для автоматизации веб-скрапинга и взаимодействия с API 
+через управляемый браузер. Поддерживает прокси, инъекцию заголовков, 
+обработку cookie и множественные методы получения данных.
+"""
+# Импорт основных классов из модульной структуры
+from .models import HttpMethod, Response, Request, Cookie
+from .browser import BaseAPI  
+from .page import Page
+
+# Версия пакета
+__version__ = "0.1.5"
+
+# Публичный API
+__all__ = [
+    # Основные классы
+    'BaseAPI',
+    'Page',
+    
+    # Модели данных
+    'Request',
+    'Response',
+    'HttpMethod',
+    'Cookie'
+]
