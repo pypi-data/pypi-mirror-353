f710bcd3991e7d24694660bb22d16fbb575243a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:16 +0000

    Add package django-devsync/0.1.2/django_devsync-0.1.2.tar.gz

---
+++ packages/django-devsync/django_devsync-0.1.2.tar.gz/django_devsync-0.1.2/README.md
@@ -0,0 +1,114 @@
+# django-devsync
+
+A tiny command-line tool that mimics `synchronize: true` from TypeORM — but for Django.
+
+[![PyPI](https://img.shields.io/pypi/v/django-devsync)](https://pypi.org/project/django-devsync/)
+
+**Strictly for development use. Never use in production.**
+
+---
+
+## What It Does
+
+In early-stage development, database schemas change fast and migrations get messy. `django-devsync` helps you:
+
+- **Delete all migration files**
+- **Reset your local DB schema**
+- **Run `makemigrations` and `migrate` from scratch**
+
+All with a single command.
+
+---
+
+## Don't Use In Production 🛑
+
+This tool **WILL destroy data** and has minimal safeguards.
+
+---
+
+## Installation
+
+```bash
+pip install django-devsync
+```
+
+---
+
+## Usage
+
+```bash
+devsync --delete_migrations --reset_db --run_sync
+```
+
+or
+
+```bash
+python -m django_devsync --delete_migrations --reset_db --run_sync
+```
+
+If no flags are passed, it runs **all steps**.
+
+### Flags
+
+| Flag                  | Description                                                        |
+| --------------------- | ------------------------------------------------------------------ |
+| `--delete_migrations` | Delete all `.py` and `.pyc` files in `migrations/` directories     |
+| `--reset_db`          | Drop all tables/schemas in your local DB (SQLite/PostgreSQL/MySQL) |
+| `--run_sync`          | Run `makemigrations` and `migrate`                                 |
+
+---
+
+## Example
+
+```bash
+devsync
+```
+
+or
+
+```bash
+python -m django_devsync
+```
+
+Output:
+
+```
+No specific flags passed — running ALL steps:
+• Deleting all migrations
+• Resetting the database
+• Running makemigrations and migrate
+
+All done!
+
+```
+
+---
+
+## Supported Databases
+
+- SQLite
+- MySQL
+- PostgreSQL
+
+> Uses Django's `DATABASES['default']['ENGINE']` to detect backend.
+
+---
+
+## How It Works
+
+- Prompts for your `DJANGO_SETTINGS_MODULE` (unless set in env)
+- Verifies `DEBUG=True` before proceeding
+- Uses Django internals: `call_command("makemigrations")`, etc.
+- Drops and recreates schemas/tables directly with SQL
+
+---
+
+## Contributions
+
+Bug reports, feedback, and PRs are welcome. Stars are appreciated.
+
+---
+
+## License
+
+MIT
---
+++ packages/django-devsync/django_devsync-0.1.2.tar.gz/django_devsync-0.1.2/src/django_devsync/core/database.py
@@ -0,0 +1,98 @@
+import os
+
+from django.db import connection
+
+from .utils import confirm
+
+
+def reset_db():
+    """
+    Resets the database schema depending on the engine.
+
+    - PostgreSQL: Drops and recreates the `public` schema.
+    - MySQL: Drops all tables.
+    - SQLite: Deletes the `.sqlite3` file.
+
+    Raises:
+        RuntimeError: If the database engine is unsupported or connection fails.
+    """
+
+    from django.conf import settings
+
+    engine = settings.DATABASES["default"]["ENGINE"]
+
+    if "sqlite" in engine:
+        _handle_sqlite(settings)
+
+    elif "postgresql" in engine:
+        _handle_postgresql()
+
+    elif "mysql" in engine:
+        _handle_mysql()
+
+    else:
+        raise NotImplementedError(
+            f"DB engine '{engine}' not supported for reset."
+        )
+
+
+def _handle_sqlite(settings):
+    db_path = settings.DATABASES["default"]["NAME"]
+    if not os.path.isfile(db_path):
+        print("No SQLite DB file found to delete.")
+        return
+
+    print(f"\nThe {db_path} will be deleted:")
+    if not confirm():
+        return
+
+    os.remove(db_path)
+    print(f"Deleted SQLite DB file at: {db_path}")
+    return
+
+
+def _handle_mysql():
+    with connection.cursor() as cursor:
+        cursor.execute("SHOW TABLES;")
+        tables = [row[0] for row in cursor.fetchall()]
+
+        print(f"\nRemoving tables: {tables}")
+        if not confirm():
+            return
+
+        print("Disabling foreign key checks...")
+        cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
+
+        print("Dropping tables...")
+        for table in tables:
+            cursor.execute(f"DROP TABLE IF EXISTS `{table}`;")
+
+        cursor.execute("SHOW TABLES;")
+        tables = [row[0] for row in cursor.fetchall()]
+        print(f"After reset, tables: {tables}")
+
+        print("Re-enabling foreign key checks...")
+        cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
+
+    print("Dropped all tables in MySQL")
+
+
+def _handle_postgresql():
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT tablename FROM pg_tables WHERE schemaname='public';"
+        )
+
+        print("\nRemoving tables:", cursor.fetchall())
+        if not confirm():
+            return
+
+        cursor.execute("DROP SCHEMA public CASCADE;")
+        cursor.execute("CREATE SCHEMA public;")
+
+        cursor.execute(
+            "SELECT tablename FROM pg_tables WHERE schemaname='public';"
+        )
+        print("After reset, tables:", cursor.fetchall())
+
+    print("Dropped and recreated public schema in PostgreSQL")
