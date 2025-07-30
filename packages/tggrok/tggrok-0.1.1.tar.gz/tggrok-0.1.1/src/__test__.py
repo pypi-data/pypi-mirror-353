f4cd961c24c88fefdb7b98d7c98c973ec18d3084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:15 +0000

    Add package tggrok/0.1.1/tggrok-0.1.1.tar.gz

---
+++ packages/tggrok/tggrok-0.1.1.tar.gz/tggrok-0.1.1/src/__test__.py
@@ -0,0 +1,17 @@
+from tggrok import Grok
+
+
+grok: Grok = Grok(
+    api_id='25652634',
+    api_hash='20f4d4d7d5b58b89e9e9e52efe5c728a',
+    phone_number='+79267228933',
+    workdir='src',
+)
+
+
+response: int = grok.ask(
+    prompt='What is 17 + 22?\nRespond with a number and nothing else',
+    process=lambda x: int(x),
+    keep_context=False,
+)
+print(response)
