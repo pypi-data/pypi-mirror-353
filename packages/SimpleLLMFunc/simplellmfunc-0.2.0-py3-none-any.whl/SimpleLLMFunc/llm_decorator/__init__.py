7ea8d5785ea54938de8ee1637f70ad410fb5ce24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:00 +0000

    Add package SimpleLLMFunc/0.2.0/simplellmfunc-0.2.0-py3-none-any.whl

---
+++ packages/SimpleLLMFunc/simplellmfunc-0.2.0-py3-none-any.whl/SimpleLLMFunc/llm_decorator/__init__.py
@@ -0,0 +1,6 @@
+from SimpleLLMFunc.llm_decorator.llm_function_decorator import llm_function
+from SimpleLLMFunc.llm_decorator.llm_chat_decorator import llm_chat
+__all__ = [
+    "llm_function",
+    "llm_chat"
+]
