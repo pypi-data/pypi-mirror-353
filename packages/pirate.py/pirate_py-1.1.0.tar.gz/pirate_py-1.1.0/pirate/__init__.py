9b17c74cde481d8ec6fe4e402771b8eb12607918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:50 +0000

    Add package pirate.py/1.1.0/pirate_py-1.1.0.tar.gz

---
+++ packages/pirate.py/pirate_py-1.1.0.tar.gz/pirate_py-1.1.0/pirate/__init__.py
@@ -0,0 +1,19 @@
+"""
+Pirate API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic API wrapper for the Pirate API.
+
+:copyright: (c) 2025 Omkaar
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = 'pirate'
+__author__ = 'Omkaar'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2025 Omkaar'
+__version__ = '1.1.0'
+
+
+from .utils import *
