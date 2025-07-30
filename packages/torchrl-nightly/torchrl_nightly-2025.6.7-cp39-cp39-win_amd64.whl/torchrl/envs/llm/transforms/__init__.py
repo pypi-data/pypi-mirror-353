04052e1ba6f8822249dd07e530a05987089aedc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:46 +0000

    Add package torchrl-nightly/2025.6.7/torchrl_nightly-2025.6.7-cp39-cp39-win_amd64.whl

---
+++ packages/torchrl-nightly/torchrl_nightly-2025.6.7-cp39-cp39-win_amd64.whl/torchrl/envs/llm/transforms/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .dataloading import as_nested_tensor, as_padded_tensor, DataLoadingPrimer
+from .format import TemplateTransform
+from .kl import KLRewardTransform
+from .tokenizer import Tokenizer
+from .tools import PythonInterpreter
+
+__all__ = [
+    "DataLoadingPrimer",
+    "Tokenizer",
+    "PythonInterpreter",
+    "TemplateTransform",
+    "KLRewardTransform",
+    "as_nested_tensor",
+    "as_padded_tensor",
+]
