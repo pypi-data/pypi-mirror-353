6ba21c529ffa222fc1f576cd0518b9627d9ca221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:34 +0000

    Add package douyin-live-chat/1.0.2/douyin_live_chat-1.0.2.tar.gz

---
+++ packages/douyin-live-chat/douyin_live_chat-1.0.2.tar.gz/douyin_live_chat-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="douyin-live-chat",
+    version="1.0.2",
+    description="Douyin Live Chat Fetcher",
+    long_description="A package for fetching Douyin live chat messages",
+    author="bubu",
+    author_email="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.31.0',
+        'betterproto>=2.0.0b6',
+        'websocket-client>=1.7.0',
+        'mini_racer>=0.12.4'
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
