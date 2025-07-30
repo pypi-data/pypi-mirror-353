ddd20b5a5a4a013cc687b6f3d93d9b26dbbe0196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:14 +0000

    Add package omicverse/1.7.1/omicverse-1.7.1-py3-none-any.whl

---
+++ packages/omicverse/omicverse-1.7.1-py3-none-any.whl/omicverse/__init__.py
@@ -0,0 +1,45 @@
+r"""
+Pyomic (A omic framework for multi-omic analysis)
+"""
+
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from pkg_resources import get_distribution
+    version = lambda name: get_distribution(name).version
+
+# Core submodules
+from . import bulk, single, utils, bulk2single, pp, space, pl
+
+# Optional PopV module requires ``scvi-tools``. Skip if dependency missing.
+
+#usually
+from .utils._data import read
+from .utils._plot import palette,ov_plot_set,plot_set
+
+
+name = "omicverse"
+try:
+    __version__ = version(name)
+except Exception:
+    __version__ = "unknown"
+
+
+from ._settings import settings,generate_reference_table
+
+
+# 导入 matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# 将 plt 作为 omicverse 的一个属性
+plt = plt  # 注意：确保没有其他变量名冲突
+
+import numpy as np
+
+np = np  # 注意：确保没有其他变量名冲突
+
+# 导入 pandas
+import pandas as pd
+
+# 将 pd 作为 omicverse 的一个属性
+pd = pd  # 注意：确保没有其他变量名冲突
