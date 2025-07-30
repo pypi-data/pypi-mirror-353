0408105dcc0cb8ce2f5eeed7d18f37b5c6ce041f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:44 +0000

    Add package wecomUtils/0.1.1/wecomutils-0.1.1-py3-none-any.whl

---
+++ packages/wecomUtils/wecomutils-0.1.1-py3-none-any.whl/wecomUtils/__init__.py
@@ -0,0 +1,7 @@
+"""WeComUtils package for WeChat Work/WeCom integration."""
+
+__version__ = "0.1.1"
+
+# Import commonly used functions for easy access
+from .storage import *
+from .db_manager import *
