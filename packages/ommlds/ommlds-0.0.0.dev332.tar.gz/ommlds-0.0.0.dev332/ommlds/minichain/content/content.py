06d13d0dfe3dc6775b144ed6b2d56167c2294241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:36 +0000

    Add package ommlds/0.0.0.dev332/ommlds-0.0.0.dev332.tar.gz

---
+++ packages/ommlds/ommlds-0.0.0.dev332.tar.gz/ommlds-0.0.0.dev332/ommlds/minichain/content/content.py
@@ -0,0 +1,31 @@
+"""
+TODO:
+ - metadata?
+  - ContentDetail, TypedValues as RequestOption
+ - ListOfContent? what to name
+"""
+import typing as ta
+
+from omlish import lang
+
+
+##
+
+
+class ExtendedContent(lang.Abstract, lang.PackageSealed):
+    pass
+
+
+##
+
+
+SingleContent: ta.TypeAlias = ta.Union[  # noqa
+    str,
+    ExtendedContent,
+]
+
+
+Content: ta.TypeAlias = ta.Union[  # noqa
+    ta.Sequence['Content'],
+    SingleContent,
+]
