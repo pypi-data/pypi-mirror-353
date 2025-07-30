b4fda4966b642f83d555e736a5162b20971163c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:12 +0000

    Add package particlelife/0.4/particlelife-0.4.tar.gz

---
+++ packages/particlelife/particlelife-0.4.tar.gz/particlelife-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='particlelife',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[
+        'pygame>=2.6.1'
+    ],
+    author='MrPsyghost',
+    author_email='shivaypuri2000@gmail.com'
+)
