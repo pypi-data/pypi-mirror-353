386055580a0beb2e39d9857a6177174051f6071a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:46 +0000

    Add package SigProfilerMatrixGenerator/1.3.4/sigprofilermatrixgenerator-1.3.4-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/sigprofilermatrixgenerator-1.3.4-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.3.4'
+version = '1.3.4'
+Update = 'v1.3.4: Fix mm39 and rn7 context counts and update in save_context_distribution script.'
+
+	
