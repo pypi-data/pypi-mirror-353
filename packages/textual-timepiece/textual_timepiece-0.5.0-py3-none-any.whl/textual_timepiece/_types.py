c980db41270d215ffbc9bf7ba8fdd4c577424ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:45 +0000

    Add package textual-timepiece/0.5.0/textual_timepiece-0.5.0-py3-none-any.whl

---
+++ packages/textual-timepiece/textual_timepiece-0.5.0-py3-none-any.whl/textual_timepiece/_types.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from typing import Literal
+from typing import TypeAlias
+
+Directions: TypeAlias = Literal["up", "right", "down", "left"]
---
+++ packages/textual-timepiece/textual_timepiece-0.5.0-py3-none-any.whl/textual_timepiece/activity_heatmap.py
@@ -0,0 +1,8 @@
+"""Activity Heatmap graph widgets for a yearly overview."""
+
+from __future__ import annotations
+
+from ._activity_heatmap import ActivityHeatmap
+from ._activity_heatmap import HeatmapManager
+
+__all__ = ("ActivityHeatmap", "HeatmapManager")
