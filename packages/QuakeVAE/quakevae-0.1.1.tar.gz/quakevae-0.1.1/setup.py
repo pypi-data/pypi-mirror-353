457eea55facfacce424ea6a2d7a22aae28996760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:40 +0000

    Add package QuakeVAE/0.1.1/quakevae-0.1.1.tar.gz

---
+++ packages/QuakeVAE/quakevae-0.1.1.tar.gz/quakevae-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="QuakeVAE",
+    version="0.1.1",
+    long_description="",
+    long_description_content_type="text/markdown",
+    packages=["quakevae"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
