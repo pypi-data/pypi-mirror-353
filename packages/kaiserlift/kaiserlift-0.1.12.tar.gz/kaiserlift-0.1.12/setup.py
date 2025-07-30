a537e07b30c2e7a40b99dd208365cb0addd7629b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:05:36 +0000

    Add package kaiserlift/0.1.12/kaiserlift-0.1.12.tar.gz

---
+++ packages/kaiserlift/kaiserlift-0.1.12.tar.gz/kaiserlift-0.1.12/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+long_description = long_description.replace(
+    "](images/", "](https://github.com/douglastkaiser/kaiserlift/raw/main/images/"
+)
+long_description = long_description.replace(
+    "[pypi package](https://pypi.org/project/kaiserlift/)",
+    "[repo link](https://github.com/douglastkaiser/kaiserlift)",
+)
+
+setup(
+    name="kaiserlift",
+    version="0.1.12",
+    description="Data-driven progressive overload",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Douglas Kaiser",
+    author_email="douglastkaiser@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "IPython",
+    ],
+    entry_points={
+        "console_scripts": [
+            "kaiserlift-cli = kaiserlift.main:main",
+        ],
+    },
+)
