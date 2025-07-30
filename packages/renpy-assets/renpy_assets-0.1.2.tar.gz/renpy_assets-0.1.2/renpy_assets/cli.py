eeb919b0dee683282045fe6ec9d222842e308e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:29 +0000

    Add package renpy-assets/0.1.2/renpy_assets-0.1.2.tar.gz

---
+++ packages/renpy-assets/renpy_assets-0.1.2.tar.gz/renpy_assets-0.1.2/renpy_assets/cli.py
@@ -0,0 +1,11 @@
+import typer
+from renpy_assets.commands import scan, generate
+
+
+app = typer.Typer()    
+app.add_typer(scan.app)
+app.add_typer(generate.app)
+
+
+if __name__ == '__main__':
+    app()
