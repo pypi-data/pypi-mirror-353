a47dbebc2d0742243740fa2d5bcad191c917125c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:43 +0000

    Add package wowool-common/3.3.2/wowool_common-3.3.2-py3-none-any.whl

---
+++ packages/wowool-common/wowool_common-3.3.2-py3-none-any.whl/wowool/annotation/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from wowool.annotation.annotation import Annotation, ErrorAnnotationNotFound
+from wowool.annotation.token import Token
+from wowool.annotation.entity import Entity
+from wowool.annotation.sentence import Sentence
+from wowool.annotation.concept import Concept
