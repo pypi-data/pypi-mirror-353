7842924484f1d05db0ebb4a76e6c96cdac37b0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:12 +0000

    Add package ehrdata/0.0.5/ehrdata-0.0.5.tar.gz

---
+++ packages/ehrdata/ehrdata-0.0.5.tar.gz/ehrdata-0.0.5/CHANGELOG.md
@@ -0,0 +1,59 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog][],
+and this project adheres to [Semantic Versioning][].
+
+[keep a changelog]: https://keepachangelog.com/en/1.0.0/
+[semantic versioning]: https://semver.org/spec/v2.0.0.html
+
+## [0.0.5]
+
+### Fixed
+
+- Initialize EHRData with X and layers
+
+### Added
+
+### Modified
+
+- Rename `.t` attribute to `.tem`
+
+## [0.0.4]
+
+### Fixed
+
+- Zarr version to less than 3
+
+## [0.0.3]
+
+### Fixed
+
+- Added missing zarr dependency
+
+## [0.0.2]
+
+### Added
+
+- Expanded documentation
+- Improved OMOP Extraction
+- Support for [COO sparse matrices](https://github.com/pydata/sparse) for R
+- A `ed.dt.ehrdata_blobs` test data generator function
+- Replace -1 encoded missing values with nans in physionet2012 challenge data
+
+### Breaking changes
+
+- Renamed `r` to `R`
+
+## [0.0.1] - 2024-11-04
+
+### Added
+
+- Initial release
+
+## [Unreleased]
+
+### Added
+
+- Basic tool, preprocessing and plotting functions
---
+++ packages/ehrdata/ehrdata-0.0.5.tar.gz/ehrdata-0.0.5/tests/conftest.py
@@ -0,0 +1,112 @@
+import anndata as ad
+import dask.array as da
+import duckdb
+import numpy as np
+import pandas as pd
+import pytest
+import sparse as sp
+
+from ehrdata import EHRData
+from ehrdata.io.omop import setup_connection
+
+
+@pytest.fixture
+def X_numpy_32():
+    return np.arange(1, 7).reshape(3, 2)
+
+
+@pytest.fixture
+def X_numpy_33():
+    return np.arange(1, 10).reshape(3, 3)
+
+
+@pytest.fixture
+def R_numpy_322():
+    return np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]])
+
+
+@pytest.fixture
+def R_sparse_322():
+    return sp.COO(np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]))
+
+
+@pytest.fixture
+def R_dask_322():
+    return da.from_array(np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]), chunks=(3, 2, 2))
+
+
+@pytest.fixture
+def R_numpy_333():
+    return np.arange(1, 28).reshape(3, 3, 3)
+
+
+@pytest.fixture
+def obs_31():
+    return pd.DataFrame({"obs_col_1": [1, 2, 3]}, index=["obs1", "obs2", "obs3"])
+
+
+@pytest.fixture
+def var_21():
+    return pd.DataFrame({"var_col_1": [1, 2]}, index=["var1", "var2"])
+
+
+@pytest.fixture
+def var_31():
+    return pd.DataFrame({"var_col_1": [1, 2, 3]}, index=["var1", "var2", "var3"])
+
+
+@pytest.fixture
+def tem_11():
+    return pd.DataFrame({"tem_col_1": [1]}, index=["t1"])
+
+
+@pytest.fixture
+def tem_21():
+    return pd.DataFrame({"tem_col_1": [1, 2]}, index=["t1", "t2"])
+
+
+@pytest.fixture
+def tem_31():
+    return pd.DataFrame({"tem_col_1": [1, 2, 3]}, index=["t1", "t2", "t3"])
+
+
+@pytest.fixture
+def edata_333(X_numpy_33, R_numpy_333, obs_31, var_31, tem_31):
+    return EHRData(X=X_numpy_33, R=R_numpy_333, obs=obs_31, var=var_31, tem=tem_31)
+
+
+@pytest.fixture
+def adata_33(X_numpy_33, obs_31, var_31):
+    return ad.AnnData(X=X_numpy_33, obs=obs_31, var=var_31)
+
+
+@pytest.fixture
+def omop_connection_vanilla():
+    con = duckdb.connect()
+    setup_connection(path="tests/data/toy_omop/vanilla", backend_handle=con)
+    yield con
+    con.close()
+
+
+@pytest.fixture
+def omop_connection_capital_letters():
+    con = duckdb.connect()
+    setup_connection(path="tests/data/toy_omop/capital_letters", backend_handle=con)
+    yield con
+    con.close()
+
+
+@pytest.fixture
+def omop_connection_empty_observation():
+    con = duckdb.connect()
+    setup_connection(path="tests/data/toy_omop/empty_observation", backend_handle=con)
+    yield con
+    con.close()
+
+
+@pytest.fixture
+def omop_connection_multiple_units():
+    con = duckdb.connect()
+    setup_connection(path="tests/data/toy_omop/multiple_units", backend_handle=con)
+    yield con
+    con.close()
