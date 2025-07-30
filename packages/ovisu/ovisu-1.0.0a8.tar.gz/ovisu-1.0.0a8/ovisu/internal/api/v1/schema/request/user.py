aaa03b9aa98e2420dfd6667362c21e409c038686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:21 +0000

    Add package ovisu/1.0.0a8/ovisu-1.0.0a8.tar.gz

---
+++ packages/ovisu/ovisu-1.0.0a8.tar.gz/ovisu-1.0.0a8/ovisu/internal/api/v1/schema/request/user.py
@@ -0,0 +1,12 @@
+
+from pydantic import Field
+
+from ovisu.internal.schema.user import UserBase
+
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=6)
+
+
+class UserLogin(UserBase):
+    password: str
