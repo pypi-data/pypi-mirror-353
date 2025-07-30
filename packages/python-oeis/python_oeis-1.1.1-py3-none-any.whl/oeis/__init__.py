72cb5a269cd34e7e5ef372628b65ac2c130e3f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:54 +0000

    Add package python-oeis/1.1.1/python_oeis-1.1.1-py3-none-any.whl

---
+++ packages/python-oeis/python_oeis-1.1.1-py3-none-any.whl/oeis/__init__.py
@@ -0,0 +1,19 @@
+"""
+OEIS API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic API wrapper for OEIS.
+
+:copyright: (c) 2025 Omkaar
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = "oeis"
+__author__ = "Omkaar"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025 Omkaar"
+__version__ = "1.1.1"
+
+
+from .sequence import *
