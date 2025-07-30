a4cb369c49fde3b9df1322eb54f49b1d56f93f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:34 +0000

    Add package stag/2.1.2/stag-2.1.2-cp38-cp38-win_amd64.whl

---
+++ packages/stag/stag-2.1.2-cp38-cp38-win_amd64.whl/stag/stag_lib/stag.h
@@ -0,0 +1,15 @@
+/*
+   This file is provided as part of the STAG library and released under the GPL
+   license.
+*/
+#ifndef STAG_TEST_STAG_H
+#define STAG_TEST_STAG_H
+
+namespace stag {
+  // Provide metadata about the library version
+  static const unsigned VERSION_MAJOR = 2;
+  static const unsigned VERSION_MINOR = 1;
+  static const unsigned VERSION_PATCH = 0;
+}
+
+#endif //STAG_TEST_STAG_H
