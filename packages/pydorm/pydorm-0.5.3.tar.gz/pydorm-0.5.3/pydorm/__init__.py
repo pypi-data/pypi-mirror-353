1f5771e5a23cde1fa7cd7f36367f69668d7a0150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:34 +0000

    Add package pydorm/0.5.3/pydorm-0.5.3.tar.gz

---
+++ packages/pydorm/pydorm-0.5.3.tar.gz/pydorm-0.5.3/pydorm/__init__.py
@@ -0,0 +1,27 @@
+from ._dorm import dorm
+from ._entity import entity, vo
+from ._initializer import init
+from ._insert import insert, insert_bulk, upsert, upsert_bulk
+from ._query import query, raw_query
+from ._transaction import begin, commit, rollback
+from ._update import update
+
+__author__ = 'melon'
+__version__ = '0.5.3'
+
+__all__ = [
+    'init',
+    'dorm',
+    'query',
+    'update',
+    'insert',
+    'insert_bulk',
+    'upsert',
+    'upsert_bulk',
+    'raw_query',
+    'entity',
+    'vo',
+    'begin',
+    'commit',
+    'rollback',
+]
