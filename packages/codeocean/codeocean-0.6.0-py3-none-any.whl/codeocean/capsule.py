ad9d12df92e60732d9ac593368832f1daf1ba16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:54 +0000

    Add package codeocean/0.6.0/codeocean-0.6.0-py3-none-any.whl

---
+++ packages/codeocean/codeocean-0.6.0-py3-none-any.whl/codeocean/capsule.py
@@ -0,0 +1,136 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+from typing import Optional, Iterator
+from requests_toolbelt.sessions import BaseUrlSession
+
+from codeocean.components import Ownership, SortOrder, SearchFilter, Permissions
+from codeocean.computation import Computation
+from codeocean.data_asset import DataAssetAttachParams, DataAssetAttachResults
+from codeocean.enum import StrEnum
+
+
+class CapsuleStatus(StrEnum):
+    NonRelease = "non_release"
+    Release = "release"
+
+
+class CapsuleSortBy(StrEnum):
+    Created = "created"
+    LastAccessed = "last_accessed"
+    Name = "name"
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class OriginalCapsuleInfo:
+    id: Optional[str] = None
+    major_version: Optional[int] = None
+    minor_version: Optional[int] = None
+    name: Optional[str] = None
+    created: Optional[int] = None
+    public: Optional[bool] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Capsule:
+    id: str
+    created: int
+    name: str
+    status: CapsuleStatus
+    owner: str
+    slug: str
+    article: Optional[dict] = None
+    cloned_from_url: Optional[str] = None
+    description: Optional[str] = None
+    field: Optional[str] = None
+    tags: Optional[list[str]] = None
+    original_capsule: Optional[OriginalCapsuleInfo] = None
+    release_capsule: Optional[str] = None
+    submission: Optional[dict] = None
+    versions: Optional[list[dict]] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class CapsuleSearchParams:
+    query: Optional[str] = None
+    next_token: Optional[str] = None
+    offset: Optional[int] = None
+    limit: Optional[int] = None
+    sort_field: Optional[CapsuleSortBy] = None
+    sort_order: Optional[SortOrder] = None
+    ownership: Optional[Ownership] = None
+    status: Optional[CapsuleStatus] = None
+    favorite: Optional[bool] = None
+    archived: Optional[bool] = None
+    filters: Optional[list[SearchFilter]] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class CapsuleSearchResults:
+    has_more: bool
+    results: list[Capsule]
+    next_token: Optional[str] = None
+
+
+@dataclass
+class Capsules:
+
+    client: BaseUrlSession
+
+    def get_capsule(self, capsule_id: str) -> Capsule:
+        res = self.client.get(f"capsules/{capsule_id}")
+
+        return Capsule.from_dict(res.json())
+
+    def list_computations(self, capsule_id: str) -> list[Computation]:
+        res = self.client.get(f"capsules/{capsule_id}/computations")
+
+        return [Computation.from_dict(c) for c in res.json()]
+
+    def update_permissions(self, capsule_id: str, permissions: Permissions):
+        self.client.post(
+            f"capsules/{capsule_id}/permissions",
+            json=permissions.to_dict(),
+        )
+
+    def attach_data_assets(
+            self,
+            capsule_id: str,
+            attach_params: list[DataAssetAttachParams]) -> list[DataAssetAttachResults]:
+        res = self.client.post(
+            f"capsules/{capsule_id}/data_assets",
+            json=[j.to_dict() for j in attach_params],
+        )
+
+        return [DataAssetAttachResults.from_dict(c) for c in res.json()]
+
+    def detach_data_assets(self, capsule_id: str, data_assets: list[str]):
+        self.client.delete(
+            f"capsules/{capsule_id}/data_assets/",
+            json=data_assets,
+        )
+
+    def search_capsules(self, search_params: CapsuleSearchParams) -> CapsuleSearchResults:
+        res = self.client.post("capsules/search", json=search_params.to_dict())
+
+        return CapsuleSearchResults.from_dict(res.json())
+
+    def search_capsules_iterator(self, search_params: CapsuleSearchParams) -> Iterator[Capsules]:
+        params = search_params.to_dict()
+        while True:
+            response = self.search_capsules(
+                search_params=CapsuleSearchParams(**params)
+            )
+
+            for result in response.results:
+                yield result
+
+            if not response.has_more:
+                return
+
+            params["next_token"] = response.next_token
---
+++ packages/codeocean/codeocean-0.6.0-py3-none-any.whl/codeocean/computation.py
@@ -0,0 +1,180 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+from requests_toolbelt.sessions import BaseUrlSession
+from typing import Optional
+from time import sleep, time
+
+from codeocean.enum import StrEnum
+from codeocean.folder import Folder, DownloadFileURL
+
+
+class ComputationState(StrEnum):
+    Initializing = "initializing"
+    Running = "running"
+    Finalizing = "finalizing"
+    Completed = "completed"
+    Failed = "failed"
+
+
+class ComputationEndStatus(StrEnum):
+    Succeeded = "succeeded"
+    Failed = "failed"
+    Stopped = "stopped"
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Param:
+    name: Optional[str] = None
+    param_name: Optional[str] = None
+    value: Optional[str] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class PipelineProcess:
+    name: str
+    capsule_id: str
+    version: Optional[int] = None
+    public: Optional[bool] = None
+    parameters: Optional[list[Param]] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class InputDataAsset:
+    id: str
+    mount: Optional[str] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Computation:
+    id: str
+    created: int
+    name: str
+    run_time: int
+    state: ComputationState
+    cloud_workstation: Optional[bool] = None
+    data_assets: Optional[list[InputDataAsset]] = None
+    parameters: Optional[list[Param]] = None
+    nextflow_profile: Optional[str] = None
+    processes: Optional[list[PipelineProcess]] = None
+    end_status: Optional[ComputationEndStatus] = None
+    exit_code: Optional[int] = None
+    has_results: Optional[bool] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class DataAssetsRunParam:
+    id: str
+    mount: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class NamedRunParam:
+    param_name: str
+    value: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class PipelineProcessParams:
+    name: str
+    parameters: Optional[list[str]] = None
+    named_parameters: Optional[list[NamedRunParam]] = None
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class RunParams:
+    capsule_id: Optional[str] = None
+    pipeline_id: Optional[str] = None
+    version: Optional[int] = None
+    resume_run_id: Optional[str] = None
+    nextflow_profile: Optional[str] = None
+    data_assets: Optional[list[DataAssetsRunParam]] = None
+    parameters: Optional[list[str]] = None
+    named_parameters: Optional[list[NamedRunParam]] = None
+    processes: Optional[list[PipelineProcessParams]] = None
+
+
+@dataclass
+class Computations:
+
+    client: BaseUrlSession
+
+    def get_computation(self, computation_id: str) -> Computation:
+        res = self.client.get(f"computations/{computation_id}")
+
+        return Computation.from_dict(res.json())
+
+    def run_capsule(self, run_params: RunParams) -> Computation:
+        res = self.client.post("computations", json=run_params.to_dict())
+
+        return Computation.from_dict(res.json())
+
+    def wait_until_completed(
+        self,
+        computation: Computation,
+        polling_interval: float = 5,
+        timeout: Optional[float] = None,
+    ) -> Computation:
+        """
+        Polls the given computation until it reaches the 'Completed' or 'Failed' state.
+
+        - `polling_interval` and `timeout` are in seconds
+        """
+        if polling_interval < 5:
+            raise ValueError(
+                f"Polling interval {polling_interval} should be greater than or equal to 5"
+            )
+        if timeout is not None and timeout < polling_interval:
+            raise ValueError(
+                f"Timeout {timeout} should be greater than or equal to polling interval {polling_interval}"
+            )
+        if timeout is not None and timeout < 0:
+            raise ValueError(
+                f"Timeout {timeout} should be greater than or equal to 0 (seconds), or None"
+            )
+        t0 = time()
+        while True:
+            comp = self.get_computation(computation.id)
+
+            if comp.state in [ComputationState.Completed, ComputationState.Failed]:
+                return comp
+
+            if timeout is not None and (time() - t0) > timeout:
+                raise TimeoutError(f"Computation {computation.id} did not complete within {timeout} seconds")
+
+            sleep(polling_interval)
+
+    def list_computation_results(self, computation_id: str, path: str = "") -> Folder:
+        data = {
+            "path": path,
+        }
+
+        res = self.client.post(f"computations/{computation_id}/results", json=data)
+
+        return Folder.from_dict(res.json())
+
+    def get_result_file_download_url(self, computation_id: str, path: str) -> DownloadFileURL:
+        res = self.client.get(
+            f"computations/{computation_id}/results/download_url",
+            params={"path": path},
+        )
+
+        return DownloadFileURL.from_dict(res.json())
+
+    def delete_computation(self, computation_id: str):
+        self.client.delete(f"computations/{computation_id}")
+
+    def rename_computation(self, computation_id: str, name: str):
+        self.client.patch(
+            f"computations/{computation_id}",
+            params={"name": name}
+            )
