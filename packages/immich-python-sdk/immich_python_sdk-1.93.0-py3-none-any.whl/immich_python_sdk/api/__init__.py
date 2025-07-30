7eac1643c4f152793cf093f16cd6b2a68c0ad419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:00 +0000

    Add package immich-python-sdk/1.93.0/immich_python_sdk-1.93.0-py3-none-any.whl

---
+++ packages/immich-python-sdk/immich_python_sdk-1.93.0-py3-none-any.whl/immich_python_sdk/api/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+
+# import apis into api package
+from immich_python_sdk.api.api_key_api import APIKeyApi
+from immich_python_sdk.api.activity_api import ActivityApi
+from immich_python_sdk.api.album_api import AlbumApi
+from immich_python_sdk.api.asset_api import AssetApi
+from immich_python_sdk.api.audit_api import AuditApi
+from immich_python_sdk.api.authentication_api import AuthenticationApi
+from immich_python_sdk.api.face_api import FaceApi
+from immich_python_sdk.api.job_api import JobApi
+from immich_python_sdk.api.library_api import LibraryApi
+from immich_python_sdk.api.o_auth_api import OAuthApi
+from immich_python_sdk.api.partner_api import PartnerApi
+from immich_python_sdk.api.person_api import PersonApi
+from immich_python_sdk.api.search_api import SearchApi
+from immich_python_sdk.api.server_info_api import ServerInfoApi
+from immich_python_sdk.api.shared_link_api import SharedLinkApi
+from immich_python_sdk.api.system_config_api import SystemConfigApi
+from immich_python_sdk.api.tag_api import TagApi
+from immich_python_sdk.api.user_api import UserApi
+
