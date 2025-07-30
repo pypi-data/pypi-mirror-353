6a5c5c9e13e387bbea984b1822ee9661fd2b02bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:49 +0000

    Add package cyberarkgetpassword/0.1.0/cyberarkgetpassword-0.1.0.tar.gz

---
+++ packages/cyberarkgetpassword/cyberarkgetpassword-0.1.0.tar.gz/cyberarkgetpassword-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cyberarkgetpassword',
+    version='0.1.0',
+    description='Utility to fetch CyberArk passwords',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
