b4b1dd2b576478c607ac63808894b66c620e2bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:31 +0000

    Add package fenics-plotly/2025.0.0/fenics_plotly-2025.0.0.tar.gz

---
+++ packages/fenics-plotly/fenics_plotly-2025.0.0.tar.gz/fenics_plotly-2025.0.0/fenics_plotly/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for FEniCS-Plotly."""
+
+__author__ = """Henrik Finsberg"""
+__email__ = "henriknf@simula.no"
+__version__ = "2025.0.0"
+
+from .fenics_plotly import plot, set_renderer
+
+__all__ = ["plot", "set_renderer"]
