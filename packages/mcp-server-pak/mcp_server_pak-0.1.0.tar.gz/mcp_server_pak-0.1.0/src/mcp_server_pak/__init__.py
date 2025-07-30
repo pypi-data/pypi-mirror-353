b80311ec2cdc5872a6786786a71eabd5141b9f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:16 +0000

    Add package mcp-server-pak/0.1.0/mcp_server_pak-0.1.0.tar.gz

---
+++ packages/mcp-server-pak/mcp_server_pak-0.1.0.tar.gz/mcp_server_pak-0.1.0/src/mcp_server_pak/__init__.py
@@ -0,0 +1,27 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool()
+def getWindowSize():
+    """ 获取窗口大小 """
+    return 100, 100
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+def main() -> None:
+    mcp.run(transport='sse')
+
