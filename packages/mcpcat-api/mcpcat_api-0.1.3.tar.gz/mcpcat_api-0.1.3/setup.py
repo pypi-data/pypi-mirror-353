4cc98ee4b27c2f0ac5fad847b0d175688beefdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:45 +0000

    Add package mcpcat-api/0.1.3/mcpcat_api-0.1.3.tar.gz

---
+++ packages/mcpcat-api/mcpcat_api-0.1.3.tar.gz/mcpcat_api-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mcpcat_api",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "urllib3>=1.25.3",
+        "python-dateutil",
+        "pydantic>=2.0.0,<3",
+    ],
+)
