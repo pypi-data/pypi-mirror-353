fa192f5f4acf1aa5a1cc65531466170b4b1204c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:36 +0000

    Add package py-pd-stepper/0.0.1/py_pd_stepper-0.0.1.tar.gz

---
+++ packages/py-pd-stepper/py_pd_stepper-0.0.1.tar.gz/py_pd_stepper-0.0.1/src/pd_stepper/pd_stepper.py
@@ -0,0 +1,10 @@
+from .controller_serial import ControllerSerial
+from .serial_port import SerialPort
+
+class PDStepper:
+    __serial_port: SerialPort
+    controller: ControllerSerial
+    def __init__(self, port: str) -> None:
+        self.__serial_port = SerialPort(port)
+        self.__serial_port.open_serial()
+        self.controller = ControllerSerial(self.__serial_port)
