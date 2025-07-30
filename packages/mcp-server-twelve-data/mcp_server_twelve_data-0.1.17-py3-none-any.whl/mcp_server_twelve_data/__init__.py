d2536f46aa31bcdc8d65054a8ff087ab51241f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:27 +0000

    Add package mcp-server-twelve-data/0.1.17/mcp_server_twelve_data-0.1.17-py3-none-any.whl

---
+++ packages/mcp-server-twelve-data/mcp_server_twelve_data-0.1.17-py3-none-any.whl/mcp_server_twelve_data/__init__.py
@@ -0,0 +1,43 @@
+from typing import Literal
+
+import click
+import logging
+import sys
+
+from pygments.lexer import default
+
+from .server import serve
+
+
+@click.command()
+@click.option("-v", "--verbose", count=True)
+@click.option("-t", "--transport", default="stdio", help="stdio, streamable-http")
+@click.option("-k", "--apikey", default="", help="if set, then override a query parameter")
+@click.option(
+    "-n", "--number-of-tools", default=35,
+    help="limit number of tools to prevent problems with mcp clients, max n value is 193, default is 35"
+)
+def main(
+    verbose: bool,
+    transport: Literal["stdio", "sse", "streamable-http"] = "stdio",
+    apikey: str = "",
+    number_of_tools: int = 30,
+) -> None:
+    """MCP Git Server - Git functionality for MCP"""
+    logging_level = logging.WARN
+    if verbose == 1:
+        logging_level = logging.INFO
+    elif verbose >= 2:
+        logging_level = logging.DEBUG
+
+    logging.basicConfig(level=logging_level, stream=sys.stderr)
+    serve(
+        api_base="https://api.twelvedata.com",
+        transport=transport,
+        apikey=apikey,
+        number_of_tools=number_of_tools
+    )
+
+
+if __name__ == "__main__":
+    main()
