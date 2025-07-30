4b6200e7fed5636a1f02f62f5a6b284e9daa9819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:17:53 +0000

    Add package atlasctf-21-prod-29/9.9/atlasctf_21_prod_29-9.9.tar.gz

---
+++ packages/atlasctf-21-prod-29/atlasctf_21_prod_29-9.9.tar.gz/atlasctf_21_prod_29-9.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="atlasctf_21_prod_29",
+    version="9.9",
+    packages=find_packages(),
+    description="Fake internal package",
+)
+
