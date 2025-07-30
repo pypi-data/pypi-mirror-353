e59c60690bc8a62bde632ac90cce591ad8b9baa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:13 +0000

    Add package bibtexautocomplete/1.4.2/bibtexautocomplete-1.4.2.tar.gz

---
+++ packages/bibtexautocomplete/bibtexautocomplete-1.4.2.tar.gz/bibtexautocomplete-1.4.2/bibtexautocomplete/utils/functions.py
@@ -0,0 +1,34 @@
+from typing import Callable, Iterable, List, Optional, Set, Tuple, TypeVar
+
+T = TypeVar("T")
+Q = TypeVar("Q")
+
+
+def list_unduplicate(lst: List[T]) -> Tuple[List[T], Set[T]]:
+    """Unduplicates a list, preserving order
+    Returns of set of duplicated elements"""
+    unique = list()
+    dups = set()
+    for x in lst:
+        if x in unique:
+            dups.add(x)
+        else:
+            unique.append(x)
+    return unique, dups
+
+
+def list_sort_using(to_sort: Iterable[Q], reference: List[T], map: Callable[[Q], T]) -> List[Q]:
+    """Sorts to_sort based on the order in reference, using map for conversion"""
+    order = {q: i for i, q in enumerate(reference)}
+    return sorted(to_sort, key=lambda t: order[map(t)])
+
+
+def split_iso_date(date: str) -> Tuple[Optional[str], Optional[str]]:
+    """Extract year and month from a YYYY-MM-DD or YYYY-MM date string"""
+    year = None
+    month = None
+    if len(date) >= 4 and date[:4].isnumeric() and 1000 <= int(date[:4]) <= 3000:
+        year = date[:4]
+        if len(date) >= 7 and date[5:7].isnumeric() and 1 <= int(date[5:7]) <= 12:
+            month = date[5:7]
+    return year, month
