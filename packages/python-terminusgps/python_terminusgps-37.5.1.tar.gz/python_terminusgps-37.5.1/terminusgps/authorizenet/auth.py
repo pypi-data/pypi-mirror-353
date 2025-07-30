651820d9b5aaf4148eb3a9f3f6ea04aefbe5b67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:12 +0000

    Add package python-terminusgps/37.5.1/python_terminusgps-37.5.1.tar.gz

---
+++ packages/python-terminusgps/python_terminusgps-37.5.1.tar.gz/python_terminusgps-37.5.1/terminusgps/authorizenet/auth.py
@@ -0,0 +1,44 @@
+from authorizenet.apicontractsv1 import merchantAuthenticationType
+from authorizenet.constants import constants
+from django.conf import settings
+
+from terminusgps.django import settings as default_settings
+
+if not settings.configured:
+    settings.configure(default_settings)
+
+
+def get_merchant_auth() -> merchantAuthenticationType:
+    """
+    Returns the current merchant authentication information for Authorizenet API requests.
+
+    :returns: A merchant authentication object.
+    :rtype: :py:obj:`~authorizenet.apicontractsv1.merchantAuthenticationType`
+
+    """
+    return merchantAuthenticationType(
+        name=str(settings.MERCHANT_AUTH_LOGIN_ID),
+        transactionKey=str(settings.MERCHANT_AUTH_TRANSACTION_KEY),
+    )
+
+
+def get_environment() -> str:
+    """
+    Returns the current environment for Authorizenet API requests.
+
+    :returns: An Authorizenet API environment string.
+    :rtype: :py:obj:`str`
+
+    """
+    return constants.SANDBOX if settings.DEBUG else constants.PRODUCTION
+
+
+def get_validation_mode() -> str:
+    """
+    Returns the current validation mode for Authorizenet API requests.
+
+    :returns: An Authorizenet API validation string.
+    :rtype: :py:obj:`str`
+
+    """
+    return "testMode" if settings.DEBUG else "liveMode"
