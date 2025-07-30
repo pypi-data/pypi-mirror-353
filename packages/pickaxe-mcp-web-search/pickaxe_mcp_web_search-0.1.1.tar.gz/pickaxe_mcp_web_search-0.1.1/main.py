4c2dcb7b6467d32da64f96c3558e68bc8f53d4b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:11:42 +0000

    Add package pickaxe-mcp-web-search/0.1.1/pickaxe_mcp_web_search-0.1.1.tar.gz

---
+++ packages/pickaxe-mcp-web-search/pickaxe_mcp_web_search-0.1.1.tar.gz/pickaxe_mcp_web_search-0.1.1/main.py
@@ -0,0 +1,37 @@
+import os
+import json
+from fastmcp import FastMCP
+from openai import OpenAI
+
+mcp = FastMCP(
+    name="web-search",
+    instructions="You are a web search engine. Call search() to find relevant information based on the user's query.",
+)
+
+client = OpenAI(
+    api_key=os.environ["OPENAI_API_KEY"],
+)
+
+@mcp.tool()
+def search(query: str) -> str:
+    """
+    Perform a web search for the given query.
+    """
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=query,
+        tools=[{ "type": "web_search_preview" }]
+    )
+
+    contents = ""
+    for result in response.output:
+        if result.type == "message":
+            contents += "\n".join(item.text for item in result.content if item.type == "output_text")
+
+    return f"""Search results for '{query}': {json.dumps(contents, default=str, ensure_ascii=False)}"""
+
+def main():
+    mcp.run(transport="stdio")
+
+if __name__ == "__main__":
+    main()
