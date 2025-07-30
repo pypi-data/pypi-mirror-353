63ce1266890966233ddcd89d763062f024a072a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:02 +0000

    Add package flux-arcane-mage/0.1.0/flux_arcane_mage-0.1.0.tar.gz

---
+++ packages/flux-arcane-mage/flux_arcane_mage-0.1.0.tar.gz/flux_arcane_mage-0.1.0/src/flux_arcane_mage/screens/__init__.py
@@ -0,0 +1,6 @@
+from .provision_node import ProvisionNodeScreen as ProvisionNodeScreen
+from .welcome import WelcomeScreen as WelcomeScreen
+from .welcome_proxmox import WelcomeScreenProxmox as WelcomeScreenProxmox
+from .hypervisor import AddHypervisorScreen as AddHypervisorScreen
+from .provisioning_info import ProvisioningInfoScreen as ProvisioningInfoScreen
+from .confirm_screen import ConfirmScreen as ConfirmScreen
