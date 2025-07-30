dc63034c16fb75d1b06a7c364de5e42e7044ae8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:47 +0000

    Add package wirepy/0.1.8/wirepy-0.1.8.tar.gz

---
+++ packages/wirepy/wirepy-0.1.8.tar.gz/wirepy-0.1.8/wirepy/templates/app/core/config.py
@@ -0,0 +1,20 @@
+# app/core/config.py
+from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv
+
+# as config is not where main.py, so we have to declaer the path
+from pathlib import Path
+
+env_path = Path(".") / ".env"
+# print(env_path)
+load_dotenv(dotenv_path=env_path)
+
+class Settings(BaseSettings):
+    PROJECT_TITLE: str = "Pradipta Blog Apis"
+    PROJECT_VERSION: str = "0.1.0"
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+settings = Settings()
