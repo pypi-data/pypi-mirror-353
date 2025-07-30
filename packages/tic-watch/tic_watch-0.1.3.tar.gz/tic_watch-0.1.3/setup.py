d5fd507d867ed88e9b11f23f47b81c4e782d2f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:34 +0000

    Add package tic-watch/0.1.3/tic_watch-0.1.3.tar.gz

---
+++ packages/tic-watch/tic_watch-0.1.3.tar.gz/tic_watch-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+setup(
+    name="tic_watch",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "opentelemetry-api",
+        "opentelemetry-sdk",
+        "opentelemetry-instrumentation-fastapi",
+        "opentelemetry-instrumentation-asgi",
+        "azure-monitor-opentelemetry-exporter"
+    ],
+    author="TIC",
+    author_email="Andrews.Rajkumar@dexian.com",
+    description="TIC Watch package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: FastAPI",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
