ddb706d682ae1997866ad0d05ae8ae30a0efe4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:22 +0000

    Add package countertype-sp/0.1.2/countertype_sp-0.1.2.tar.gz

---
+++ packages/countertype-sp/countertype_sp-0.1.2.tar.gz/countertype_sp-0.1.2/countertypes/__init__.py
@@ -0,0 +1,71 @@
+# Suresh Polai Cretead a CounterType
+class counterTypes:
+    def __init__(self, value):
+        self.integer = 0
+        self.floating = 0
+        self.string = 0
+        self.boolean = 0
+        self.lst = 0
+        self.tpl = 0
+        self.dct = 0
+        self.sets = 0
+
+        self.lstOfInt = []
+        self.lstOfFloat = []
+        self.lstOfString = []
+        self.lstOfBool = []
+        self.lstOfLst = []
+        self.lstOfTuple = []
+        self.lstOfSet = []
+        self.lstOfDict = []
+
+        self.value = value
+
+        for i in self.value:
+            if isinstance(i, bool):
+                self.boolean += 1
+                self.lstOfBool.append(i)
+            elif isinstance(i, int):
+                self.integer += 1
+                self.lstOfInt.append(i)
+            elif isinstance(i, float):
+                self.floating += 1
+                self.lstOfFloat.append(i)
+            elif isinstance(i, str):
+                self.string += 1
+                self.lstOfString.append(i)
+            elif isinstance(i, list):
+                self.lst += 1
+                self.lstOfLst.append(i)
+            elif isinstance(i, tuple):
+                self.tpl += 1
+                self.lstOfTuple.append(i)
+            elif isinstance(i, set):
+                self.sets += 1
+                self.lstOfSet.append(i)
+            elif isinstance(i, dict):
+                self.dct += 1
+                self.lstOfDict.append(i)
+
+    def NonZeroTotal(self):
+        NewSet = ''
+        if self.integer:
+            NewSet += f'Int : {self.integer}\n'
+        if self.floating:
+            NewSet += f'Float : {self.floating}\n'
+        if self.string:
+            NewSet += f'Str : {self.string}\n'
+        if self.boolean:
+            NewSet += f'Bool : {self.boolean}\n'
+        if self.lst:
+            NewSet += f'List : {self.lst}\n'
+        if self.tpl:
+            NewSet += f'Tuple : {self.tpl}\n'
+        if self.sets:
+            NewSet += f'Set : {self.sets}\n'
+        if self.dct:
+            NewSet += f'Dict : {self.dct}\n'
+        return NewSet.strip()
+
+    def Total(self):
+        return f"Int : {self.integer}\nFloat : {self.floating}\nStr : {self.string}\nbool : {self.boolen}\nlist : {self.lst}\nTuple : {self.tpl}\nSet : {self.sets}\nDict : {self.dct}"
