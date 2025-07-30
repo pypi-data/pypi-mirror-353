ba75a536808a3ef89dc47d8f259e90389107519a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:37 +0000

    Add package mqtt-presence/0.2.6/mqtt_presence-0.2.6.tar.gz

---
+++ packages/mqtt-presence/mqtt_presence-0.2.6.tar.gz/mqtt_presence-0.2.6/mqtt_presence/parser.py
@@ -0,0 +1,38 @@
+import argparse
+
+def get_parser(desc: str):
+    # define Arguemnts
+    parser = argparse.ArgumentParser(desc)
+
+    # Optional argument for selecting the UI
+#    parser.add_argument(
+#        '--ui',
+#        choices=['webUI', 'console'],  # Available options
+#        default='webUI',  # Default value
+#        type=str,  # Argument type
+#        help="Select the UI: 'webUI' (default)."
+    #)
+
+    # Optional argument for selecting the config directory
+    parser.add_argument(
+        '--config',
+        type=str,  # Argument type
+        help="Set the config directory"
+    )
+
+    # Optional argument for selecting the log directory
+    parser.add_argument(
+        '--log',
+        type=str,  # Argument type
+        help="Set the log directory"
+    )
+
+
+    # Positional argument for selecting the UI (defaults to 'webUI')
+    #parser.add_argument('ui_positional',
+    #    nargs='?',  # Makes it optional
+    #    choices=['webUI', 'console', 'none'],
+    #    help="Select the UI (same as --ui option)."
+    #)
+
+    return parser
