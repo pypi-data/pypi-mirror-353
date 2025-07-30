446d86fe0983597e1134ee61e872247d59558e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:04 +0000

    Add package media-calc/0.1.1/media_calc-0.1.1.tar.gz

---
+++ packages/media-calc/media_calc-0.1.1.tar.gz/media_calc-0.1.1/media/__init__.py
@@ -0,0 +1,8 @@
+from .funciones import media
+__all__ = ['media']
+
+# Make the media function available directly
+__version__ = '0.1.0'
+media = media  # This makes the function available at the package level
+
+
