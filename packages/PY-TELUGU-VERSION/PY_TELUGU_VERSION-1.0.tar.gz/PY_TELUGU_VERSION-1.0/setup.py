c90b174742979a73b5158b0466e6e1c1c1da76fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:32 +0000

    Add package PY-TELUGU-VERSION/1.0/PY_TELUGU_VERSION-1.0.tar.gz

---
+++ packages/PY-TELUGU-VERSION/PY_TELUGU_VERSION-1.0.tar.gz/PY_TELUGU_VERSION-1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PY_TELUGU_VERSION",
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pythontel = PY_TELUGU_VERSION.auto_update:main',
+            'pythonrun = PY_TELUGU_VERSION.main:main',
+        ],
+    },
+    author="sunstromium",
+    description="literal telugu package for python programming",
+    long_description="-- — test version",
+    long_description_content_type="text/plain",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
