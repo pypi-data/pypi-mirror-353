95363e9ace24183a95a3d1563e0070e9cff16f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:11:21 +0000

    Add package cucim-cu12/25.6.0/cucim_cu12-25.6.0-cp310-cp310-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/cucim-cu12/cucim_cu12-25.6.0-cp310-cp310-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/cucim/_misc.py
@@ -0,0 +1,42 @@
+"""Misc utility functions that are not from SciPy, NumPy or scikit-image.
+
+"""
+import numpy as np
+
+
+def ndim(a):
+    """
+    Return the number of dimensions of an array.
+
+    Parameters
+    ----------
+    a : array_like
+        Input array.  If it is not already an ndarray, a conversion is
+        attempted.
+
+    Returns
+    -------
+    number_of_dimensions : int
+        The number of dimensions in `a`.  Scalars are zero-dimensional.
+
+    See Also
+    --------
+    ndarray.ndim : equivalent method
+    shape : dimensions of array
+    ndarray.shape : dimensions of array
+
+    Examples
+    --------
+    >>> from cucim.numpy import ndim
+    >>> ndim([[1,2,3],[4,5,6]])
+    2
+    >>> ndim(cupy.asarray([[1,2,3],[4,5,6]]))
+    2
+    >>> ndim(1)
+    0
+
+    """
+    try:
+        return a.ndim
+    except AttributeError:
+        return np.asarray(a).ndim
