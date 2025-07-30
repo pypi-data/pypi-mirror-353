d82399cdc4f53b4215cd4477dc5e6b29ec5dd0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:12 +0000

    Add package certbot-dns-inwx/3.0.3/certbot_dns_inwx-3.0.3-py3-none-any.whl

---
+++ packages/certbot-dns-inwx/certbot_dns_inwx-3.0.3-py3-none-any.whl/certbot_dns_inwx/__init__.py
@@ -0,0 +1,7 @@
+"""
+The `~certbot_dns_inwx.dns_inwx` plugin automates the process of
+completing a ``dns-01`` challenge (`~acme.challenges.DNS01`) by creating, and
+subsequently removing, TXT records using the INWX XML-RPC DNS API.
+
+For further documentation see the README file.
+"""
