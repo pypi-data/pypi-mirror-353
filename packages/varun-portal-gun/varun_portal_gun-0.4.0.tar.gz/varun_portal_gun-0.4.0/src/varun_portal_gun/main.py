5222138def184f91932802229e1468549df35c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:03 +0000

    Add package varun-portal-gun/0.4.0/varun_portal_gun-0.4.0.tar.gz

---
+++ packages/varun-portal-gun/varun_portal_gun-0.4.0.tar.gz/varun_portal_gun-0.4.0/src/varun_portal_gun/main.py
@@ -0,0 +1,12 @@
+import typer
+import os
+
+app = typer.Typer()
+
+
+@app.command()
+def predict(file_path: str):
+    print("Here are your input files: " + file_path)
+
+    # for filename in os.listdir(file_path):
+    #     print("Type: " + type(filename) + ", " + "Name: " + filename)
