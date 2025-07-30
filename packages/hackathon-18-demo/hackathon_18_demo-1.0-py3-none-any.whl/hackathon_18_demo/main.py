f9c2eff9d69b44c422a266af790669428fb4610f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:49 +0000

    Add package hackathon-18-demo/1.0/hackathon_18_demo-1.0-py3-none-any.whl

---
+++ packages/hackathon-18-demo/hackathon_18_demo-1.0-py3-none-any.whl/hackathon_18_demo/main.py
@@ -0,0 +1,32 @@
+""" 
+Copyright (c) 2025 MeteoSwiss, Hugues de Laroussilhe
+
+Distributed under the terms of the BSD 3-Clause License.
+
+SPDX-License-Identifier: BSD-3-Clause
+"""
+
+
+def dummy_func(input_value: float) -> int:
+    """
+    Dummy function.
+
+    Args:
+        input_value (float): argument
+
+    Returns:
+        int: an integer
+    """
+    return int(input_value)
+
+def new_dummy_func(input_value: float) -> float:
+    """
+    New Dummy function.
+
+    Args:
+        input_value (float): argument
+
+    Returns:
+        float: a float
+    """
+    return float(input_value)
