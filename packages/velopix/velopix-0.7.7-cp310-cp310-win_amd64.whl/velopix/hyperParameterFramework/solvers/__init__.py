91b0102ff6eaaee792318e21dc2f5dc809ff6085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:43 +0000

    Add package velopix/0.7.7/velopix-0.7.7-cp310-cp310-win_amd64.whl

---
+++ packages/velopix/velopix-0.7.7-cp310-cp310-win_amd64.whl/velopix/hyperParameterFramework/solvers/__init__.py
@@ -0,0 +1,9 @@
+from ._Bayesian import Bayesian
+from ._GridSearch import GridSearch
+from ._ParticleSwarm import ParticleSwarm
+
+__all__ = [
+    "_Bayesian",
+    "_GridSearch",
+    "_ParticleSwarm"
+]
