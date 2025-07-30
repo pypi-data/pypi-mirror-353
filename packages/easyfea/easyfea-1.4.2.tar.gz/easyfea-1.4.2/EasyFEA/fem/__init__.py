a0c2578fbd266514600f2615c0c018e1281a3a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:05:47 +0000

    Add package easyfea/1.4.2/easyfea-1.4.2.tar.gz

---
+++ packages/easyfea/easyfea-1.4.2.tar.gz/easyfea-1.4.2/EasyFEA/fem/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2021-2025 Université Gustave Eiffel.
+# This file is part of the EasyFEA project.
+# EasyFEA is distributed under the terms of the GNU General Public License v3 or later, see LICENSE.txt and CREDITS.md for more information.
+
+from ._utils import ElemType, MatrixType, FeArray
+from ._mesh import Mesh, Mesh_Optim, Calc_projector
+from ._boundary_conditions import BoundaryCondition, LagrangeCondition
+from ._gmsh_interface import Mesher, gmsh
+from ._group_elems import _GroupElem, GroupElemFactory
