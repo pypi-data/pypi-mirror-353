fec0ee71fe04007f5b632f255f13cbdc251ae1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:35 +0000

    Add package D7s-Style/0.1.1/d7s_style-0.1.1.tar.gz

---
+++ packages/D7s-Style/d7s_style-0.1.1.tar.gz/d7s_style-0.1.1/README.md
@@ -0,0 +1,3 @@
+# D7s-Style
+#For Install in Linux and Termux:
+#     pip3 install D7s-Style
---
+++ packages/D7s-Style/d7s_style-0.1.1.tar.gz/d7s_style-0.1.1/src/D7s_Style/__init__.py
@@ -0,0 +1,8 @@
+
+#########################################
+from D7s_Style.D7s_Style import Loading
+from D7s_Style.D7s_Style import Animation
+from D7s_Style.D7s_Style import D7s_Style
+from D7s_Style.D7s_Style import My_Style
+#The Dark7Storm
+##########################################
