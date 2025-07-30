2cb7c6fc5b2c8e689f710bb8ae1d56b326564fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:34 +0000

    Add package opencv-stubs/0.0.12/opencv_stubs-0.0.12-py3-none-any.whl

---
+++ packages/opencv-stubs/opencv_stubs-0.0.12-py3-none-any.whl/cv2-stubs/__init__.pyi
@@ -0,0 +1,6 @@
+__version__ = "0.0.12"
+
+from .classes import *
+from .constants import *
+from .functions import *
+from .modules import *
---
+++ packages/opencv-stubs/opencv_stubs-0.0.12-py3-none-any.whl/cv2-stubs/modules/gapi/wip/gst.pyi
@@ -0,0 +1,9 @@
+import builtins
+from typing import Final
+
+class GStreamerPipeline: ...
+
+GSTREAMER_SOURCE_OUTPUT_TYPE_FRAME: Final[int]
+GSTREAMER_SOURCE_OUTPUT_TYPE_MAT: Final[int]
+GStreamerSource_OutputType_FRAME: Final[int]
+GStreamerSource_OutputType_MAT: Final[int]
