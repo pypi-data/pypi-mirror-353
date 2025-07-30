3f4bbd77c62478c2db435727bff44651e60a9ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:22 +0000

    Add package youtube.py3/1.0.10/youtube_py3-1.0.10-py3-none-any.whl

---
+++ packages/youtube.py3/youtube_py3-1.0.10-py3-none-any.whl/youtube_py3/__init__.py
@@ -0,0 +1,14 @@
+"""
+YouTube.py3 - YouTube Data API v3 Wrapper Library
+
+YouTube Data API v3を簡単に使用するためのPythonラッパーライブラリです。
+"""
+
+__version__ = "1.0.10"
+__author__ = "Chihalu"
+__email__ = "yanase.ui.prv@gmail.com"
+__license__ = "MIT"
+
+from .youtube_py3 import YouTubeAPI, YouTubeAPIError
+
+__all__ = ["YouTubeAPI", "YouTubeAPIError"]
