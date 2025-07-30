91b348adb6e8664d958c7cce95150ad00f3d2299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:06 +0000

    Add package majadigi/0.1.2/majadigi-0.1.2.tar.gz

---
+++ packages/majadigi/majadigi-0.1.2.tar.gz/majadigi-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='majadigi',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'psycopg2-binary',
+        'sqlalchemy',
+        'tabulate',
+        'scikit-learn'
+    ],
+    author='Wawu Tri Ambodo',
+    description='Library untuk cleansing dan transformasi data wilayah Jatim',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
