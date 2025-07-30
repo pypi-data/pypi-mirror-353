aa5392530a77a885e92f4167993e14ad5efbefd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:03 +0000

    Add package langfuse-pydantic-ai/0.2.1/langfuse_pydantic_ai-0.2.1-py3-none-any.whl

---
+++ packages/langfuse-pydantic-ai/langfuse_pydantic_ai-0.2.1-py3-none-any.whl/langfuse_pydantic_ai/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for langfuse-pydantic."""
+
+__author__ = "wh1isper"
+__email__ = "jizhongsheng957@gmail.com"
+__version__ = "0.2.1"
+
+
+from ._warpper import observed_agent, observed_model, use_observed_agent
