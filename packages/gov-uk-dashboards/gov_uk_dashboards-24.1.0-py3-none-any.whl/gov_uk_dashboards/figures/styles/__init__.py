74914e5ec1494c34ba42e3bf2e92e196ccb6fe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:13 +0000

    Add package gov-uk-dashboards/24.1.0/gov_uk_dashboards-24.1.0-py3-none-any.whl

---
+++ packages/gov-uk-dashboards/gov_uk_dashboards-24.1.0-py3-none-any.whl/gov_uk_dashboards/figures/styles/__init__.py
@@ -0,0 +1,7 @@
+"""Module containing classes and functions used for styling plotly figures.
+
+Contains:
+- LineStyle: Dataclass containing information on how to style a line on a line
+    chart.
+"""
+from .line_style import LineStyle
