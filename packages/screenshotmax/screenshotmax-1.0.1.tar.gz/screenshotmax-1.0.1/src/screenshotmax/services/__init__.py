22de339156f5aad20f790e7fb4108072f2685f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:59 +0000

    Add package screenshotmax/1.0.1/screenshotmax-1.0.1.tar.gz

---
+++ packages/screenshotmax/screenshotmax-1.0.1.tar.gz/screenshotmax-1.0.1/src/screenshotmax/services/__init__.py
@@ -0,0 +1,17 @@
+from .screenshot import ScreenshotService
+from .screencast import ScreencastService
+from .scrape import ScrapeService
+from .pdf import PDFService
+from .task import TaskService
+from .usage import UsageService
+from .device import DeviceService
+
+__all__ = [
+  "ScreenshotService",
+  "ScreencastService",
+  "ScrapeService",
+  "PDFService",
+  "TaskService",
+  "UsageService",
+  "DeviceService",
+]
