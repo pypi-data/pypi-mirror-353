d0b4e6eab59a139329ae3e3c34e03a72e5b555e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:29 +0000

    Add package inkhinge/0.1.1/inkhinge-0.1.1-py3-none-any.whl

---
+++ packages/inkhinge/inkhinge-0.1.1-py3-none-any.whl/inkhinge/__init__.py
@@ -0,0 +1,9 @@
+"""inkhinge - 一个简单的数学运算工具包"""
+
+__version__ = '0.1.1'
+__author__ = 'Fenglong Li  SiYang Li  KeHan Li'
+__email__ = '15340666394@163.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/yourusername/inkhinge'
+
+from .core import add_numbers, multiply_numbers
