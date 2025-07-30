f97ae44e5b66b84466d1581f9bf5b31544047a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:02 +0000

    Add package Computer-Vision-Tool/0.1.2/computer_vision_tool-0.1.2.tar.gz

---
+++ packages/Computer-Vision-Tool/computer_vision_tool-0.1.2.tar.gz/computer_vision_tool-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Computer_Vision_Tool",  # Tên thư viện trên PyPI
+    version="0.1.2",  # Phiên bản đầu tiên
+    packages=find_packages(),
+    install_requires=[],  # Các thư viện phụ thuộc nếu có
+    author="David_Nguyen",
+    description="Vision_tool",  
+)
