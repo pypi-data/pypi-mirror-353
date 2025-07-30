a3fdd5f4560bd1f092711354a51955543b0be68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:36 +0000

    Add package aioinject/1.2.0/aioinject-1.2.0.tar.gz

---
+++ packages/aioinject/aioinject-1.2.0.tar.gz/aioinject-1.2.0/tests/container/test_future_annotations.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from aioinject import Container, Scoped
+from aioinject.errors import CannotDetermineReturnTypeError
+
+
+async def test_deferred_dependencies() -> None:
+    if TYPE_CHECKING:
+        from decimal import Decimal
+
+    def some_deferred_type() -> Decimal:
+        from decimal import Decimal
+
+        return Decimal("1.0")
+
+    class DoubledDecimal:
+        def __init__(self, decimal: Decimal) -> None:
+            self.decimal = decimal * 2
+
+    container = Container()
+
+    def register_decimal_scoped() -> None:
+        from decimal import Decimal
+
+        container.register(Scoped(some_deferred_type, Decimal))
+
+    register_decimal_scoped()
+    container.register(Scoped(DoubledDecimal))
+    async with container.context() as ctx:
+        assert (await ctx.resolve(DoubledDecimal)).decimal == DoubledDecimal(
+            some_deferred_type(),
+        ).decimal
+
+
+def test_provider_fn_deferred_dep_misuse() -> None:
+    with pytest.raises(CannotDetermineReturnTypeError) as exc_info:
+        from tests.container.mod_tests import (
+            provider_fn_deferred_dep_misuse,  # noqa: F401
+        )
+    assert exc_info.match("Or it's type is not defined yet.")
