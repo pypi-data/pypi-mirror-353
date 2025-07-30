8013bd71dbbb71f8735cb575282dbffc129c7b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:05:44 +0000

    Add package omnikdatalogger/1.15.6/omnikdatalogger-1.15.6-py3-none-any.whl

---
+++ packages/omnikdatalogger/omnikdatalogger-1.15.6-py3-none-any.whl/omnik/plugin_client/__init__.py
@@ -0,0 +1,25 @@
+class BaseClient(type):
+    def __init__(cls, name, bases, attrs):
+        super(BaseClient, cls).__init__(name)
+        if not hasattr(cls, "client"):
+            cls.client = []
+        else:
+            cls.register(cls)  # Called when a client class is imported
+
+    def register(cls, client=None):
+        cls.client.append(client())
+
+
+class Client(object, metaclass=BaseClient):
+
+    config = None
+    logger = None
+    hass_api = None
+    use_timer = True
+    plant_id_list = []
+
+    def initialize(self):
+        return True
+
+    def terminate(self):
+        return True
