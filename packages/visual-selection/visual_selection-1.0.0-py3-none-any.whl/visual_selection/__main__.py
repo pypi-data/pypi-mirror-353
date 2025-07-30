e4b694f3adf65a1b4a2f476eb94fdb3e964742ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:24 +0000

    Add package visual-selection/1.0.0/visual_selection-1.0.0-py3-none-any.whl

---
+++ packages/visual-selection/visual_selection-1.0.0-py3-none-any.whl/visual_selection/__main__.py
@@ -0,0 +1,14 @@
+"""Main entry point for the application."""
+
+from .app import SelectionApp
+
+
+def run() -> None:
+    """Run the application."""
+    SelectionApp().run()
+
+
+if __name__ == "__main__":
+    run()
+
+### __main__.py ends here
