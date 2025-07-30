90ac26a744ba040df4bdc8a1768f77f7dcd6bd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:24 +0000

    Add package endstone/0.8.2/endstone-0.8.2-cp313-cp313-win_amd64.whl

---
+++ packages/endstone/endstone-0.8.2-cp313-cp313-win_amd64.whl/endstone/__init__.py
@@ -0,0 +1,29 @@
+from endstone._internal.endstone_python import (
+    ColorFormat,
+    GameMode,
+    Logger,
+    NamespacedKey,
+    OfflinePlayer,
+    Player,
+    EnchantmentRegistry,
+    Server,
+    Skin,
+)
+
+from endstone._internal.version import __version__
+
+__minecraft_version__ = "1.21.84"
+
+__all__ = [
+    "__version__",
+    "__minecraft_version__",
+    "ColorFormat",
+    "GameMode",
+    "Logger",
+    "NamespacedKey",
+    "OfflinePlayer",
+    "Player",
+    "EnchantmentRegistry",
+    "Server",
+    "Skin",
+]
