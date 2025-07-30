8186ab04dc13425d390ea52c9ddc9c0d7795e396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:16 +0000

    Add package riskgpt/0.1.2/riskgpt-0.1.2-py3-none-any.whl

---
+++ packages/riskgpt/riskgpt-0.1.2-py3-none-any.whl/riskgpt/chains/get_categories.py
@@ -0,0 +1,54 @@
+from langchain_core.output_parsers import PydanticOutputParser
+
+from riskgpt.config.settings import RiskGPTSettings
+from riskgpt.models.schemas import CategoryRequest, CategoryResponse
+from riskgpt.registry.chain_registry import register
+from riskgpt.utils.prompt_loader import load_prompt
+
+from .base import BaseChain
+
+
+@register("get_categories")
+def get_categories_chain(request: CategoryRequest) -> CategoryResponse:
+    settings = RiskGPTSettings()
+    prompt_data = load_prompt("get_categories")
+
+    parser = PydanticOutputParser(pydantic_object=CategoryResponse)
+    chain = BaseChain(
+        prompt_template=prompt_data["template"],
+        parser=parser,
+        settings=settings,
+        prompt_name="get_categories",
+    )
+
+    inputs = request.model_dump()
+    inputs["domain_section"] = (
+        f"Domain knowledge: {request.domain_knowledge}"
+        if request.domain_knowledge
+        else ""
+    )
+
+    return chain.invoke(inputs)
+
+
+async def async_get_categories_chain(request: CategoryRequest) -> CategoryResponse:
+    """Asynchronous wrapper around :func:`get_categories_chain`."""
+    settings = RiskGPTSettings()
+    prompt_data = load_prompt("get_categories")
+
+    parser = PydanticOutputParser(pydantic_object=CategoryResponse)
+    chain = BaseChain(
+        prompt_template=prompt_data["template"],
+        parser=parser,
+        settings=settings,
+        prompt_name="get_categories",
+    )
+
+    inputs = request.model_dump()
+    inputs["domain_section"] = (
+        f"Domain knowledge: {request.domain_knowledge}"
+        if request.domain_knowledge
+        else ""
+    )
+
+    return await chain.invoke_async(inputs)
