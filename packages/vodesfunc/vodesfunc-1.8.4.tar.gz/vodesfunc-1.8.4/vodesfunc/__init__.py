7508d35b921de849529eae9262558a079a9cc181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:05:51 +0000

    Add package vodesfunc/1.8.4/vodesfunc-1.8.4.tar.gz

---
+++ packages/vodesfunc/vodesfunc-1.8.4.tar.gz/vodesfunc-1.8.4/vodesfunc/__init__.py
@@ -0,0 +1,17 @@
+"""
+Oh god no
+"""
+
+# flake8: noqa
+
+from . import aa, misc, noise, scale, denoise, rescale, util, spikefinder, rescale_ext
+from .aa import *
+from .misc import *
+from .noise import *
+from .scale import *
+from .rescale import *
+from .denoise import *
+from .util import *
+from .spikefinder import *
+
+from .rescale_ext.mixed_rescale import *
