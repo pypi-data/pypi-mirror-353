b4911d24debd87f3aa68d107bee1354a31645bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:16 +0000

    Add package py-reqlint/1.3/py_reqlint-1.3.tar.gz

---
+++ packages/py-reqlint/py_reqlint-1.3.tar.gz/py_reqlint-1.3/reqlint/__init__.py
@@ -0,0 +1,1 @@
+from .reqlint import ReqLint
---
+++ packages/py-reqlint/py_reqlint-1.3.tar.gz/py_reqlint-1.3/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="py-reqlint",
+    version="1.3",
+    author="Innate Essence",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
