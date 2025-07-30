c5ca55da262f175f850ecd562f023943bd57a244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:41 +0000

    Add package inkhinge/0.1.2/inkhinge-0.1.2.tar.gz

---
+++ packages/inkhinge/inkhinge-0.1.2.tar.gz/inkhinge-0.1.2/inkhinge/__init__.py
@@ -0,0 +1,9 @@
+"""inkhinge - 一个简单的数学运算工具包"""
+
+__version__ = '0.1.2'
+__author__ = 'Fenglong Li,SiYang Li,KeHan Li'
+__email__ = '15340666394@163.com'
+__license__ = 'MIT'
+__url__ = 'https://github.com/1412universe/inkhinge'
+
+from .core import add_numbers, multiply_numbers
