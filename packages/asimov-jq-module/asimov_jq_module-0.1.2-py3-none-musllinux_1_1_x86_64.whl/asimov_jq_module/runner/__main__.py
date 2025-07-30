84b7e04b92c12e477137c67dfe4e5d56717d9805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:47 +0000

    Add package asimov-jq-module/0.1.2/asimov_jq_module-0.1.2-py3-none-musllinux_1_1_x86_64.whl

---
+++ packages/asimov-jq-module/asimov_jq_module-0.1.2-py3-none-musllinux_1_1_x86_64.whl/asimov_jq_module/runner/__main__.py
@@ -0,0 +1,6 @@
+import os, sys
+argv = [os.path.join(os.path.dirname(__file__), "asimov-jq-runner"), *sys.argv[1:]]
+if os.name == 'posix':
+    os.execv(argv[0], argv)
+else:
+    import subprocess; sys.exit(subprocess.call(argv))
