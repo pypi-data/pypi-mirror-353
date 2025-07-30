621ac24a9dac9e27da8d5b2f1cee61f6dfe0de75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:54 +0000

    Add package terrayml/0.93/terrayml-0.93-py3-none-any.whl

---
+++ packages/terrayml/terrayml-0.93-py3-none-any.whl/terrayml/modules/vpc/security_group.tf
@@ -0,0 +1,27 @@
+locals {
+  security_groups = flatten([
+      for vpc_key, vpc in var.vpc_list: [
+        for security_group_key, security_group in vpc.security_groups: {
+          vpc_key = vpc_key
+          vpc_name = vpc.vpc_name
+          vpc_id = aws_vpc.this[vpc.vpc_name].id
+
+          security_group_key = security_group_key
+          security_group_name = security_group.security_group_name
+        }
+      ]
+    ])
+
+}
+
+resource "aws_security_group" "this" {
+    for_each = tomap({
+        for security_group in local.security_groups: "${security_group.vpc_name}.${security_group.security_group_name}" => security_group
+    })
+    
+    name   = each.value.security_group_name
+    vpc_id = each.value.vpc_id
+    tags = merge(var.common.default_tags, {
+        Name = "${each.value.security_group_name}-${var.common.environment}"
+    })
+}
