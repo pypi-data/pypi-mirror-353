3832866036615cb18fd68910f8c127a0e8ea0943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:01 +0000

    Add package PyWSGIRef/1.0.1/pywsgiref-1.0.1.tar.gz

---
+++ packages/PyWSGIRef/pywsgiref-1.0.1.tar.gz/pywsgiref-1.0.1/src/PyWSGIRef/defaults.py
@@ -0,0 +1,14 @@
+HELLO_WORLD = """<!DOCTYPE html>
+<html lang"de">
+    <body>
+        <h1>Hallo Welt!</h1>
+    </body>
+<html>"""
+
+ERROR = """<!DOCTYPE html>
+<html lang"de">
+    <body>
+        <h1>Fehler!</h1>
+        <p>Es ist ein Fehler aufgetreten.</p>
+    </body>
+</html>"""
