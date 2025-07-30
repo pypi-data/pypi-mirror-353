3e8e25d6620d719b7dde0e2190497d654deb8144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:48 +0000

    Add package eciespy/0.4.5/eciespy-0.4.5-py3-none-any.whl

---
+++ packages/eciespy/eciespy-0.4.5-py3-none-any.whl/ecies/config.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Literal
+
+from .consts import (
+    COMPRESSED_PUBLIC_KEY_SIZE,
+    CURVE25519_PUBLIC_KEY_SIZE,
+    UNCOMPRESSED_PUBLIC_KEY_SIZE,
+)
+
+EllipticCurve = Literal["secp256k1", "x25519"]
+SymmetricAlgorithm = Literal["aes-256-gcm", "xchacha20"]
+NonceLength = Literal[12, 16]  # only for aes-256-gcm, xchacha20 will always be 24
+
+
+@dataclass()
+class Config:
+    elliptic_curve: EllipticCurve = "secp256k1"
+    is_ephemeral_key_compressed: bool = False
+    is_hkdf_key_compressed: bool = False
+    symmetric_algorithm: SymmetricAlgorithm = "aes-256-gcm"
+    symmetric_nonce_length: NonceLength = 16
+
+    @property
+    def ephemeral_key_size(self):
+        if self.elliptic_curve == "secp256k1":
+            return (
+                COMPRESSED_PUBLIC_KEY_SIZE
+                if self.is_ephemeral_key_compressed
+                else UNCOMPRESSED_PUBLIC_KEY_SIZE
+            )
+        elif self.elliptic_curve == "x25519":
+            return CURVE25519_PUBLIC_KEY_SIZE
+        else:
+            raise NotImplementedError
+
+
+ECIES_CONFIG = Config()
---
+++ packages/eciespy/eciespy-0.4.5-py3-none-any.whl/ecies/utils/eth.py
@@ -0,0 +1,51 @@
+from coincurve.utils import get_valid_secret
+from Crypto.Hash import keccak
+from typing_extensions import deprecated
+
+
+@deprecated(
+    "Use `eth_keys.keys.PrivateKey(coincurve.utils.get_valid_secret())` instead"
+)
+def generate_eth_key():
+    """
+    Note: `eth-keys` needs to be installed in advance.
+
+    Generate a random `eth_keys.keys.PrivateKey`
+
+    Returns
+    -------
+    eth_keys.keys.PrivateKey
+        An ethereum flavored secp256k1 key
+
+    """
+    from eth_keys import keys  # type:ignore
+
+    return keys.PrivateKey(get_valid_secret())
+
+
+# for cli only
+def to_eth_address(pk_bytes: bytes) -> str:
+    if len(pk_bytes) != 64:
+        raise NotImplementedError
+    return encode_checksum(keccak256(pk_bytes)[-20:].hex())
+
+
+# private below
+def encode_checksum(raw_address: str) -> str:
+    # https://github.com/ethereum/ercs/blob/master/ERCS/erc-55.md
+    address = raw_address.lower().replace("0x", "")
+    address_hash = keccak256(address.encode()).hex()
+
+    res = []
+    for a, h in zip(address, address_hash):
+        if int(h, 16) >= 8:
+            res.append(a.upper())
+        else:
+            res.append(a)
+
+    return "0x" + "".join(res)
+
+
+def keccak256(data: bytes) -> bytes:
+    h = keccak.new(data=data, digest_bits=256)
+    return h.digest()
