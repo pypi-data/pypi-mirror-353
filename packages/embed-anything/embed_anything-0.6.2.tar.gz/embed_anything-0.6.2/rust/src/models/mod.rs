f8fbe77fc34be6eac6655cecf51335947138eeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:36 +0000

    Add package embed-anything/0.6.2/embed_anything-0.6.2.tar.gz

---
+++ packages/embed-anything/embed_anything-0.6.2.tar.gz/embed_anything-0.6.2/rust/src/models/mod.rs
@@ -0,0 +1,11 @@
+pub mod bert;
+pub mod clip;
+pub mod colpali;
+pub mod gemma;
+pub mod jina_bert;
+pub mod modernbert;
+pub mod paligemma;
+pub mod siglip;
+pub mod with_tracing;
+pub mod qwen3;
+pub mod quantized_qwen3;
