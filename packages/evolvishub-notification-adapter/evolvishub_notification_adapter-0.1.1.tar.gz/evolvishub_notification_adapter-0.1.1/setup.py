618bacd38899bc50809df9e4efd85b658c1ba898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:05:40 +0000

    Add package evolvishub-notification-adapter/0.1.1/evolvishub_notification_adapter-0.1.1.tar.gz

---
+++ packages/evolvishub-notification-adapter/evolvishub_notification_adapter-0.1.1.tar.gz/evolvishub_notification_adapter-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="evolvishub-notification-adapter",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "aiosqlite>=0.19.0",
+        "pyyaml>=6.0.1",
+    ],
+    python_requires=">=3.7",
+    author="Evolvis AI",
+    author_email="info@evolvis.ai",
+    description="A flexible notification system adapter for Evolvishub applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/evolvis/evolvishub-notification-adapter",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    extras_require={
+        "test": [
+            "pytest>=7.0.0",
+            "pytest-asyncio>=0.21.0",
+        ],
+    },
+) 
