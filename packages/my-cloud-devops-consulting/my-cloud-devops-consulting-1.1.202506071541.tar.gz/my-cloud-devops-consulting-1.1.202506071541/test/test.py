eb10cb617a8279aaaf7a68a7f16f66f291ae4512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:01 +0000

    Add package my-cloud-devops-consulting/1.1.202506071541/my-cloud-devops-consulting-1.1.202506071541.tar.gz

---
+++ packages/my-cloud-devops-consulting/my-cloud-devops-consulting-1.1.202506071541.tar.gz/my-cloud-devops-consulting-1.1.202506071541/test/test.py
@@ -0,0 +1,8 @@
+monthy_income = int( input (" What is your monthly Income ? " )) # $5000
+spent_on_rent = int( input (" How much spent on rent ? " ))       #  $1000
+groceries  =  int( input (" How much spent on groceries ? " ))    # $500
+money_left_over = monthy_income - spent_on_rent - groceries 
+print(monthy_income)
+print(spent_on_rent)
+print(groceries)
+print(money_left_over)
