ff7ce055631706d0f0e93616ce2e390b1a3caee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:52 +0000

    Add package sql-to-code-local/0.0.38/sql_to_code_local-0.0.38.tar.gz

---
+++ packages/sql-to-code-local/sql_to_code_local-0.0.38.tar.gz/sql_to_code_local-0.0.38/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+PACAKGE_NAME = "sql-to-code-local"  # TODO sql2code-local?
+package_dir = PACAKGE_NAME.replace("-", "_")
+
+setuptools.setup(
+    name=PACAKGE_NAME,  # https://pypi.org/project/sql-to-code-local
+    version='0.0.38',  # update each time
+    author="Circles",
+    author_email="info@circlez.ai",
+    description="PyPI Package for Circles sql-to-code-local Local Python",
+    long_description="PyPI Package for Circles sql-to-code-local Local Python",
+    long_description_content_type='text/markdown',
+    url="https://github.com/circles-zone/sql2code-local-python-package",
+
+    # old
+    # packages=[package_dir],
+    # package_dir={package_dir: f'{package_dir}/src'},
+    # package_data={package_dir: ['*.py']},
+
+    # new
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "logger-local>=0.0.71",  # https://pypi.org/project/logger-local/
+        # TODO >= 0.1.1
+        "database-mysql-local==0.0.459",  # https://pypi.org/project/database-infrastructure-local/
+        "python-sdk-local>=0.0.147",  # https://pypi.org/project/python-sdk-local/
+    ]
+)
