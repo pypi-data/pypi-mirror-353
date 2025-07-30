5aa43de4a832f76e8de1ae54b4e419140b5a9962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:46 +0000

    Add package django-mcpx/0.1.2/django_mcpx-0.1.2.tar.gz

---
+++ packages/django-mcpx/django_mcpx-0.1.2.tar.gz/django_mcpx-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-mcpx",
+    version="0.1.2",
+    author="synw",
+    author_email="synwe@yahoo.com",
+    description="A Django package for MCP authentication",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/synw/django-mcpx",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+    ],
+    python_requires=">=3.6",
+    install_requires=["fastmcp"],
+)
