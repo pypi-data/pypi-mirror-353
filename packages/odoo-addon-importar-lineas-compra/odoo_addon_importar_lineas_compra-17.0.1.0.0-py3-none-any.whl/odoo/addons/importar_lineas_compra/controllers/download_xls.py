be2ee555660056302460eb84fbc746c810fddeca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:40 +0000

    Add package odoo-addon-importar-lineas-compra/17.0.1.0.0/odoo_addon_importar_lineas_compra-17.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo-addon-importar-lineas-compra/odoo_addon_importar_lineas_compra-17.0.1.0.0-py3-none-any.whl/odoo/addons/importar_lineas_compra/controllers/download_xls.py
@@ -0,0 +1,36 @@
+# Copyright 2022 - Komun.org Álex Berbel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.web.controllers.main import content_disposition
+import base64
+import os, os.path
+import csv
+from os import listdir
+import sys
+
+class Download_xls(http.Controller):
+    
+    @http.route('/web/binary/download_document', type='http', auth="public")
+    def download_document(self,model,id, **kw):
+
+        Model = request.env[model]
+        res = Model.browse(int(id))
+
+        if res.sample_option == 'xls':
+            invoice_xls = request.env['ir.attachment'].search([('name','=','purchase_order_line.xls')])
+            filecontent = invoice_xls.datas
+            filename = 'Demo_importar_compras.xls'
+            filecontent = base64.b64decode(filecontent)
+
+        elif res.sample_option == 'csv':
+            invoice_xls = request.env['ir.attachment'].search([('name','=','purchase_order_line.csv')])
+            filecontent = invoice_xls.datas
+            filename = 'Demo_importar_compras.csv'
+            filecontent = base64.b64decode(filecontent)
+
+        return request.make_response(filecontent,
+            [('Content-Type', 'application/octet-stream'),
+            ('Content-Disposition', content_disposition(filename))])
