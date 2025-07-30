9b0deba1229398aa5c54176d63c29a05ccbc96f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:19 +0000

    Add package python-kacl/0.6.5/python_kacl-0.6.5.tar.gz

---
+++ packages/python-kacl/python_kacl-0.6.5.tar.gz/python_kacl-0.6.5/kacl/__init__.py
@@ -0,0 +1,32 @@
+# Version of the python-kacl package
+__version__ = "0.6.5"
+
+from kacl.document import KACLDocument
+from kacl.serializer import KACLMarkdownSerializer
+
+
+def load(file):
+    """
+    Parse the first YAML document in a stream
+    and produce the corresponding Python object.
+    """
+    doc = None
+    with open(file, "r") as f:
+        document = f.read()
+        try:
+            doc = KACLDocument.parse(document)
+        finally:
+            f.close()
+    return doc
+
+
+def parse(text):
+    return KACLDocument.parse(text)
+
+
+def dump(document):
+    return KACLMarkdownSerializer().serialize(document)
+
+
+def new():
+    return KACLDocument.init()
