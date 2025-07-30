8da8df394e340b9d85fdf5ce4811a7bc5ed9b8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:54 +0000

    Add package batch-executor/0.1.2/batch_executor-0.1.2.tar.gz

---
+++ packages/batch-executor/batch_executor-0.1.2.tar.gz/batch_executor-0.1.2/src/batch_executor/cli.py
@@ -0,0 +1,18 @@
+"""Console script for batch_executor."""
+import batch_executor
+
+import typer
+from rich.console import Console
+
+app = typer.Typer()
+console = Console()
+
+@app.command()
+def main():
+    """Console script for batch_executor."""
+    console.print("Replace this message by putting your code into "
+               "batch_executor.cli.main")
+    console.print("See Typer documentation at https://typer.tiangolo.com/")
+
+if __name__ == "__main__":
+    app()
