1479bab1d30abf767e260108bafc2d6e4732ab59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:49 +0000

    Add package cemcp/0.0.8/cemcp-0.0.8-py3-none-any.whl

---
+++ packages/cemcp/cemcp-0.0.8-py3-none-any.whl/cemcp/cemcp.py
@@ -0,0 +1,35 @@
+# from fastmcp import FastMCP
+#
+# mcp = FastMCP("cemcp")
+#
+# @mcp.tool
+# def greet1(name: str) -> str:
+#     """
+#     返回小明是谁
+#     """
+#     return f"Hello, {name}!"
+#
+# def mcp_run():
+#     mcp.run()
+#
+# if __name__ == "__main__":
+#     mcp_run()
+
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool(name='Add two numbers', description='Add two numbers')
+def add1(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+def mcp_run():
+    mcp.run(transport='stdio')
+
+if __name__ == '__main__':
+   mcp_run()
