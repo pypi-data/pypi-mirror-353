b68bc9b61d79cfd462f7afa9a0efee7cca62bd03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:02 +0000

    Add package flux-arcane-mage/0.1.0/flux_arcane_mage-0.1.0-py3-none-any.whl

---
+++ packages/flux-arcane-mage/flux_arcane_mage-0.1.0-py3-none-any.whl/flux_arcane_mage/password.py
@@ -0,0 +1,28 @@
+from pyescrypt import Mode, WrongPassword, WrongPasswordConfiguration, Yescrypt
+from dataclasses import dataclass, field
+import secrets
+
+
+@dataclass
+class HashedPassword:
+    password: str
+    hasher: Yescrypt = field(
+        default_factory=lambda: Yescrypt(n=2**12, r=32, p=1, mode=Mode.MCF)
+    )
+    salt: bytes = field(default_factory=lambda: secrets.token_bytes(16))
+
+    @property
+    def encoded_pass(self) -> bytes:
+        return self.password.encode()
+
+    def hash(self) -> str:
+        hashed = self.hasher.digest(password=self.encoded_pass, salt=self.salt)
+        return hashed.decode()
+
+    def validate(self, hashed: bytes) -> bool:
+        try:
+            self.hasher.compare(self.encoded_pass, hashed)
+        except (WrongPasswordConfiguration, WrongPassword):
+            return False
+
+        return True
