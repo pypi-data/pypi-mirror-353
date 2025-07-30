1c2a72f3e0c23b1b5e1d3c80ac1d475b5e6d40fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:57 +0000

    Add package simulate-damage/0.1.3/simulate_damage-0.1.3.tar.gz

---
+++ packages/simulate-damage/simulate_damage-0.1.3.tar.gz/simulate_damage-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simulate_damage",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=["biopython"],
+    entry_points={
+        "console_scripts": [
+            "simulate_damage=simulate_damage.simulate_damage:run"
+        ]
+    },
+    author="Andrew Tedder",
+    author_email="a.tedder@bradford.ac.uk",
+    description="Simulate ancient DNA deamination damage on paired-end FASTQ files",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/simulate_damage",  # Optional, if using GitHub
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.7',
+)
