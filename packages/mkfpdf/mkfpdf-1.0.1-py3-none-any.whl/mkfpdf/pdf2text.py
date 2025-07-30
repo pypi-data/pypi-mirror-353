6a4d2427a48fc32bf4685a6f017b0a8846d591c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:13 +0000

    Add package mkfpdf/1.0.1/mkfpdf-1.0.1-py3-none-any.whl

---
+++ packages/mkfpdf/mkfpdf-1.0.1-py3-none-any.whl/mkfpdf/pdf2text.py
@@ -0,0 +1,15 @@
+""" This module provides functions to convert a PDF to text. """
+
+
+def convert2(path):
+    """ 
+    Convert the given PDF to text.
+
+    parameters:
+    path(str): The path to a PDF file.
+
+    Return: 
+    str: The content of the PDF file as text. 
+    """
+
+    print("pdf2text")
