511222ab33f81ba394cc0b1179b1e21285ef20df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:26 +0000

    Add package navground/0.6.0/navground-0.6.0-cp313-cp313-manylinux_2_34_x86_64.whl

---
+++ packages/navground/navground-0.6.0-cp313-cp313-manylinux_2_34_x86_64.whl/navground/core/__main__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import sys
+
+from . import main
+
+if __name__ == '__main__':
+    # `None` is accepted by sys.exit and equivalent to 0
+    sys.exit(main.main())  # type: ignore[func-returns-value]
