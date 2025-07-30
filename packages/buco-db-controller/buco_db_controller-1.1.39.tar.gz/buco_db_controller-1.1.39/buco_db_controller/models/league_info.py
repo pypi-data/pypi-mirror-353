3dac0ffdd9b363c4e12bcfbd0e7508db28a1c195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:30 +0000

    Add package buco-db-controller/1.1.39/buco_db_controller-1.1.39.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-1.1.39.tar.gz/buco_db_controller-1.1.39/buco_db_controller/models/league_info.py
@@ -0,0 +1,15 @@
+
+class LeagueInfo:
+    def __init__(self, league_id: int, name: str, country: str = None):
+        self.league_id: int = league_id
+        self.name: str = name
+        self.country: str = country
+
+    @classmethod
+    def from_dict(cls, response: dict) -> 'LeagueInfo':
+        data = response['data']
+        return cls(
+            league_id=data[-1]['league']['id'],
+            name=data[-1]['league']['name'],
+            country=data[-1]['country']['name']
+        )
