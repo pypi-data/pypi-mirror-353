e01be562847e1da6d90e1379a2909a30b4476b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:14 +0000

    Add package roarm-sdk/0.1.0/roarm_sdk-0.1.0.tar.gz

---
+++ packages/roarm-sdk/roarm_sdk-0.1.0.tar.gz/roarm_sdk-0.1.0/roarm_sdk/__init__.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+import datetime
+from roarm_sdk.roarm import roarm
+from roarm_sdk.generate import CommandGenerator
+from roarm_sdk import utils
+
+__all__ = [   
+    "roarm",
+    "CommandGenerator",
+    "utils"
+]
+
+__version__ = "0.1.0" 
+__author__ = "waveshareteam"
+__email__ = "2849678712@qq.com"
+__git_url__ = "https://github.com/waveshareteam/waveshare_roarm_sdk.git"
+__copyright__ = "".format(
+    datetime.datetime.now().year
+)
+
