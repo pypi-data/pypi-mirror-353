4d9883465391cd0f7fea593f34ed4ec7ea7416d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:18 +0000

    Add package a-max-product-limited/0.1.0/a_max_product_limited-0.1.0-py3-none-any.whl

---
+++ packages/a-max-product-limited/a_max_product_limited-0.1.0-py3-none-any.whl/a-max-product-limited/product1.py
@@ -0,0 +1,2 @@
+def product1():
+    return "This is product1, MaxRecruit: This AI-powered recruitment module streamlines the hiring process by automating candidate sourcing, screening, and onboarding. It offers features like proactive workforce planning, integration with multiple recruitment channels, and seamless interview management, ensuring organizations attract and onboard top talent efficiently ."
