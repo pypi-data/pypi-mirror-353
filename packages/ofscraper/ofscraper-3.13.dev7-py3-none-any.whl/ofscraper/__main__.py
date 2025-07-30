92b0947f2dda6d01fc79525dfd948db65b8a40ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:38 +0000

    Add package ofscraper/3.13.dev7/ofscraper-3.13.dev7-py3-none-any.whl

---
+++ packages/ofscraper/ofscraper-3.13.dev7-py3-none-any.whl/ofscraper/__main__.py
@@ -0,0 +1,15 @@
+#!/root/OF-Scraper/.venv/bin/python
+import multiprocessing
+
+import ofscraper.main.open.load as load
+import ofscraper.utils.system.system as system
+
+
+def main():
+    if system.get_parent():
+        load.main()
+
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    main()
---
+++ packages/ofscraper/ofscraper-3.13.dev7-py3-none-any.whl/ofscraper/__version__.py
@@ -0,0 +1,34 @@
+r"""
+        _____                                               
+  _____/ ____\______ ________________    ____   ___________ 
+ /  _ \   __\/  ___// ___\_  __ \__  \  /  _ \_/ __ \_  __ \
+(  <_> )  |  \___ \\  \___|  | \// __ \(  <_> )  ___/|  | \/
+ \____/|__| /____  >\___  >__|  (____  /\____/ \___  >__|   
+                 \/     \/           \/            \/       
+"""
+
+__title__ = "ofscraper"
+__author__ = "datawhores"
+__author_email__ = "datawhores@riseup.net"
+__description__ = (
+    "A command-line program to quickly download,like or unlike posts, and more"
+)
+__url__ = "https://github.com/datawhores/OF-Scraper"
+__license__ = "GNU General Public License v3 or later (GPLv3+)"
+__copyright__ = "Copyright 2023"
+
+
+try:
+    from dunamai import Version
+
+    __hardcoded__ = None
+    __version__ = __hardcoded__ or Version.from_git(
+        pattern="(?P<base>\d+\.\d+(\.((\d+\.\w+)|\w+)|))"
+    ).serialize(format="{base}+{branch}.{commit}", metadata=False)
+    # if __version__ == "0.0.0":
+    #     print(__version__)
+    # raise Exception("pipx error")
+except:
+    import importlib.metadata
+
+    __version__ = importlib.metadata.version("ofscraper")
