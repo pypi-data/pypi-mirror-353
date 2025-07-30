76f06c04ccda4ef66cf8e29f014630d3643c209a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:45 +0000

    Add package docker-tools-plus/0.3.1/docker_tools_plus-0.3.1.tar.gz

---
+++ packages/docker-tools-plus/docker_tools_plus-0.3.1.tar.gz/docker_tools_plus-0.3.1/docker_tools_plus/main.py
@@ -0,0 +1,6 @@
+
+
+if __name__ == '__main__':
+
+    print("This module is not meant to be run directly. Use the CLI commands instead.")
+    print("Run 'docker-tools --help' to see available commands.")
