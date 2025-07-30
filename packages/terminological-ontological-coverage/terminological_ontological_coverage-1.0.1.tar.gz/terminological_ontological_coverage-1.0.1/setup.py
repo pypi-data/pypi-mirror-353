bcfb9ab954f9b9858a952ac3b2dc14fafb4115db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:35 +0000

    Add package terminological-ontological-coverage/1.0.1/terminological_ontological_coverage-1.0.1.tar.gz

---
+++ packages/terminological-ontological-coverage/terminological_ontological_coverage-1.0.1.tar.gz/terminological_ontological_coverage-1.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Load the README file.
+current_directory = pathlib.Path(__file__).parent
+long_description = (current_directory / "README.md").read_text()
+
+setup(
+    name="terminological_ontological_coverage",
+    version="1.0.1",
+    packages=find_packages(),
+    package_data={                                           
+        'terminological_ontological_coverage': ['*.txt'],  
+    },                                                       
+    include_package_data=True,                              
+    install_requires=[
+        "PyMuPDF",   # This installs the `fitz` package
+        "rdflib",
+        "rake-nltk",
+        "keybert",
+        "pandas",
+        "argparse",
+        "transformers"
+    ],
+    entry_points={
+        'console_scripts': [
+            'keyword-comparison=terminological_ontological_coverage.keyword_comparison:main',
+        ]
+    },
+    author="Anna Sofia Lippolis",
+    description="A package for keyword and ontology comparison.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="keywords ontology comparison",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
