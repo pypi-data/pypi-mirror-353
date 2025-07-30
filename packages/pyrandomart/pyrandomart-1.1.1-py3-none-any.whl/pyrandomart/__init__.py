3a9fd8cdce28b8082a55e1ff15e0057a4611761d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:24 +0000

    Add package pyrandomart/1.1.1/pyrandomart-1.1.1-py3-none-any.whl

---
+++ packages/pyrandomart/pyrandomart-1.1.1-py3-none-any.whl/pyrandomart/__init__.py
@@ -0,0 +1,8 @@
+from pyrandomart.pyrandomart import randomart
+from pyrandomart.version import version as __version__
+from pyrandomart.version import version_tuple as __version_tuple__
+
+del pyrandomart
+del version
+
+
