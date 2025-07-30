8c000475477553aa91ee606c5119444ee20d8fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:27 +0000

    Add package endstone/0.8.2/endstone-0.8.2-cp39-cp39-manylinux_2_31_x86_64.whl

---
+++ packages/endstone/endstone-0.8.2-cp39-cp39-manylinux_2_31_x86_64.whl/endstone/inventory.py
@@ -0,0 +1,3 @@
+from endstone._internal.endstone_python import Inventory, ItemFactory, ItemMeta, ItemStack, MapMeta, PlayerInventory
+
+__all__ = ["ItemStack", "ItemFactory", "ItemMeta", "Inventory", "MapMeta", "PlayerInventory"]
