3c728e43d963b731455ad2beecd1a09303b96e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:32 +0000

    Add package vidtoolz-trim/0.1.2/vidtoolz_trim-0.1.2.tar.gz

---
+++ packages/vidtoolz-trim/vidtoolz_trim-0.1.2.tar.gz/vidtoolz_trim-0.1.2/tests/test_vidtoolz_trim.py
@@ -0,0 +1,23 @@
+import pytest
+import vidtoolz_trim as w
+
+from argparse import Namespace, ArgumentParser
+
+
+def test_create_parser():
+    subparser = ArgumentParser().add_subparsers()
+    parser = w.create_parser(subparser)
+
+    assert parser is not None
+
+    result = parser.parse_args(["hello", "-st", "0", "-et", "1"])
+    assert result.input == "hello"
+    assert result.starttime == "0"
+    assert result.endtime == "1"
+    assert result.output is None
+
+
+def test_plugin(capsys):
+    w.trim_plugin.hello(None)
+    captured = capsys.readouterr()
+    assert "Hello! This is an example ``vidtoolz`` plugin." in captured.out
